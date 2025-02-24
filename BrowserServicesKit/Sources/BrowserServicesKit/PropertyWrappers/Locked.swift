e41785a40e8115f9814d4d5d1a6f18ddca0b9e87
Sabrina Tardio <stardio@duckduckgo.com>
2025-02-24 14:57:25 +0100

    add a lign to improve readability

--- BrowserServicesKit/Sources/BrowserServicesKit/PropertyWrappers/Locked.swift
+++ BrowserServicesKit/Sources/BrowserServicesKit/PropertyWrappers/Locked.swift
@@ -29,6 +29,7 @@ import Foundation
 ///   The lock is applied only during the getter and setter. Compound operations—such as in-place mutations on a collection (e.g. `allowlist.append(newEntry)` or using `+=` on a scalar)—are not atomic.
 ///
 /// - For mutable collections or compound operations, use dedicated methods that lock the entire operation or a thread-safe container.
+///
 @propertyWrapper
 final class Locked<Value> {
     private var value: Value

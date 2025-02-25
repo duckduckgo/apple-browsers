8a08a709295937dabed22d18062446b4054c39d2
Anh Do <ado@duckduckgo.com>
2025-02-25 16:01:47 -0500

    Skip rollout

--- macOS/DuckDuckGo/Updates/UpdateController.swift
+++ macOS/DuckDuckGo/Updates/UpdateController.swift
@@ -284,6 +284,7 @@ final class UpdateController: NSObject, UpdateControllerProtocol {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
             try? self?.configureUpdater(needsUpdateCheck: true)
             self?.checkForUpdateRespectingRollout()
+            self?.checkForUpdateSkippingRollout()
         }
     }
 

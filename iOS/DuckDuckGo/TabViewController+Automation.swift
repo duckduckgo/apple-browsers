f9fed856ecfff7ebf1a9ae5a3db08b99accd9af0
Jonathan Kingston <jkingston@duckduckgo.com>
2025-03-04 12:53:43 +0000

    Removing CI debugging

--- iOS/DuckDuckGo/TabViewController+Automation.swift
+++ iOS/DuckDuckGo/TabViewController+Automation.swift
@@ -23,28 +23,8 @@ extension TabViewController {
     public func executeScript(_ javaScriptString: String,
                               args: [String: Any] = [:]) async -> Result<Any?, any Error> {
         do {
-            var modifiedJavaScriptString = javaScriptString
-            // DEBUG
-            if javaScriptString.contains("window.__wptrunner_url") {
-                modifiedJavaScriptString = """
-                function getDocumentHTML() {
-                    return new Promise((resolve) => {
-                        if (document.readyState === "complete" || document.readyState === "interactive") {
-                        // Document is already ready, resolve immediately
-                        resolve(document.documentElement.innerHTML);
-                        } else {
-                        // Wait for the DOM to be fully loaded
-                        document.addEventListener("DOMContentLoaded", () => {
-                            resolve(document.documentElement.innerHTML);
-                        });
-                        }
-                    });
-                    }
-                return getDocumentHTML();
-                """
-            }
             var result = try await webView.callAsyncJavaScript(
-                modifiedJavaScriptString,
+                javaScriptString,
                 arguments: args,
                 in: nil,
                 contentWorld: .page

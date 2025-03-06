3c0648102740d00d8a43d6cb28268a42df86704b
Daniel Bernal <dbernal@duckduckgo.com>
2025-03-06 19:54:03 +0100

    Fix file header

--- iOS/DuckDuckGo/DuckPlayer/NativeUI/Views/DuckPlayerViewUtils.swift
+++ iOS/DuckDuckGo/DuckPlayer/NativeUI/Views/DuckPlayerViewUtils.swift
@@ -2,7 +2,7 @@
 //  DuckPlayerViewUtils.swift
 //  DuckDuckGo
 //
-//  Copyright 2025 DuckDuckGo. All rights reserved.
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -13,7 +13,9 @@
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific langu
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 import SwiftUI
 

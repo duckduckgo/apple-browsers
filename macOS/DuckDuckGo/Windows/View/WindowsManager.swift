cdfc92cdc3de3907b8ca083f43c14b27a4962e6a
Tomas Strba <tom@duckduckgo.com>
2025-03-10 12:37:56 +0100

    Popup edge case resolved

--- macOS/DuckDuckGo/Windows/View/WindowsManager.swift
+++ macOS/DuckDuckGo/Windows/View/WindowsManager.swift
@@ -117,8 +117,7 @@ final class WindowsManager {
 
         let tabCollectionViewModel: TabCollectionViewModel = {
             if popUp {
-                //TODO: Don't show pinned tabs
-                return .init(tabCollection: tabCollection, burnerMode: tab.burnerMode)
+                return .init(tabCollection: tabCollection, pinnedTabsManagerProvider: nil, burnerMode: tab.burnerMode)
             }
             return .init(tabCollection: tabCollection, burnerMode: tab.burnerMode)
         }()

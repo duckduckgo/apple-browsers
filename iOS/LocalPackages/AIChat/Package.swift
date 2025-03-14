659718dcdd5b9773ddb97faeae2164889142a27a
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-03-14 19:24:12 +0100

    Update DRK branch

--- iOS/LocalPackages/AIChat/Package.swift
+++ iOS/LocalPackages/AIChat/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "4.0.0")
+        .package(url: "https://github.com/duckduckgo/DesignResourcesKit", branch: "mariusz/add-alternative-colors")
     ],
     targets: [
         .target(

1ef5be9439b74b230bab673be3576587cd877bb0
Chris Brind <brindy@duckduckgo.com>
2025-03-14 11:46:06 +0000

    fix tab switcher test and be more explicit about the tab style state

--- iOS/DuckDuckGo/TabSwitcherViewController+MultiSelect.swift
+++ iOS/DuckDuckGo/TabSwitcherViewController+MultiSelect.swift
@@ -422,6 +422,7 @@ extension TabSwitcherViewController {
 extension TabSwitcherViewController {
 
     func refreshBarButtons() {
+        barsHandler.tabSwitcherStyleButton.accessibilityLabel = tabsStyle.accessibilityLabel
         barsHandler.tabSwitcherStyleButton.primaryAction = action(image: tabsStyle.rawValue, { [weak self] in
             guard let self else { return }
             self.onTabStyleChange()
@@ -441,6 +442,7 @@ extension TabSwitcherViewController {
             self?.addNewTab()
         })
 
+        barsHandler.fireButton.accessibilityLabel = "Close all tabs and clear data"
         barsHandler.fireButton.primaryAction = action(image: "FireLeftPadded") { [weak self] in
             self?.burn(sender: self!.barsHandler.fireButton)
         }

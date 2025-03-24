ffd4a24f63827e11989d2f16b2f1febe71be8a22
Anh Do <ado@duckduckgo.com>
2025-03-24 10:27:09 -0400

    Resolve merge conflict

--- macOS/DuckDuckGo/DBP/DBPHomeViewController.swift
+++ macOS/DuckDuckGo/DBP/DBPHomeViewController.swift
@@ -32,7 +32,7 @@ public extension Notification.Name {
 final class DBPHomeViewController: NSViewController {
     private var presentedWindowController: NSWindowController?
     private let dataBrokerProtectionManager: DataBrokerProtectionManager
-    private let vpnBypassService: VPNBypassServiceProvider
+    private let vpnBypassService: VPNBypassFeatureProvider
     private let pixelHandler: EventMapping<DataBrokerProtectionMacOSPixels> = DataBrokerProtectionMacOSPixelsHandler()
     private var currentChildViewController: NSViewController?
     private var observer: NSObjectProtocol?

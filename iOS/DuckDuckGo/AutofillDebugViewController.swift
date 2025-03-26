ba98b15828b70e84854c8457c759e77262889516
amddg44 <amallon@duckduckgo.com>
2025-03-26 16:14:48 +0100

    Update mocks + lint warning

--- iOS/DuckDuckGo/AutofillDebugViewController.swift
+++ iOS/DuckDuckGo/AutofillDebugViewController.swift
@@ -117,6 +117,7 @@ class AutofillDebugViewController: UITableViewController {
             }
         }
     }
+    // swiftlint:enable cyclomatic_complexity
 
     private func promptForNumberOfLoginsToAdd() {
         let alertController = UIAlertController(title: "Enter number of Logins to add for autofill.me", message: nil, preferredStyle: .alert)

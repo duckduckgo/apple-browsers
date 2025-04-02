702cb76a8f8b163335d7b2c1459a91d12b91d810
Federico Cappelli <kap.posta@gmail.com>
2025-04-02 10:49:42 +0100

    privacyProAuthV2 ff is now remoteReleasable

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -205,7 +205,7 @@ extension FeatureFlag: FeatureFlagDescribing {
             return .remoteReleasable(.subfeature(PrivacyProSubfeature.privacyProOnboardingCTAMarch25))
 
         case .privacyProAuthV2:
-            return .remoteDevelopment(.subfeature(PrivacyProSubfeature.privacyProAuthV2))
+            return .remoteReleasable(.subfeature(PrivacyProSubfeature.privacyProAuthV2))
 
         case .onboardingSetAsDefaultBrowser:
             return .remoteReleasable(.subfeature(OnboardingSubfeature.setAsDefaultBrowserExperiment))

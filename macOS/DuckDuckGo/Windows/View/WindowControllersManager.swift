4ec773640796cbaca81775600167c801119b46c5
Alexey Martemyanov <mallexxx@gmail.com>
2025-04-10 19:56:06 +0600

    Bookmarks Bar: support middle-click and cmd+clicking background window

--- macOS/DuckDuckGo/Windows/View/WindowControllersManager.swift
+++ macOS/DuckDuckGo/Windows/View/WindowControllersManager.swift
@@ -188,7 +188,7 @@ extension WindowControllersManager {
         open(historyEntry.url, source: .historyEntry, target: nil, event: event)
     }
 
-    /// Helper method for opening with an event
+    /// Helper method for opening URL with an event respecting its Key Modifiers
     func open(_ url: URL, source: Tab.TabContent.URLSource, target window: NSWindow?, event: NSEvent?) {
         // get clicked window or last key window if menu item selected
         let windowController = mainWindowController(for: window ?? event?.window) ?? lastKeyMainWindowController
@@ -241,9 +241,6 @@ extension WindowControllersManager {
     ///   - source: The origin of the URL being opened, which can indicate whether it is from a bookmark, history record, external link, etc.
     ///   - newTab: A Boolean value indicating whether to create a new tab instead of reusing an existing one.
     ///             The default is `false`.
-    ///   - selected: An optional Boolean value that determines whether the new tab should be selected (active) or opened in the background.
-    ///               If `nil`, the new tab activation setting value will be followed (`TabsPreferences.shared.switchToNewTabWhenOpened`).
-    ///               The default is `true`.
     func show(url: URL?, tabId: String? = nil, source: Tab.TabContent.URLSource, newTab: Bool = false, selected: Bool? = true) {
         let nonPopupMainWindowControllers = mainWindowControllers.filter { $0.window?.isPopUpWindow == false }
         if source == .bookmark {

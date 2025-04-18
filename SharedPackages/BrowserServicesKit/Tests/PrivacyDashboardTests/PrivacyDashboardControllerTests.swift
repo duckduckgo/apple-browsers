94fb621785e4f6b7ff0828097e775bd2cfe1f0d5
Sam Symons <sam@samsymons.com>
2025-04-19 10:17:06 +1200

    Update the Privacy Dashboard hack.

--- SharedPackages/BrowserServicesKit/Tests/PrivacyDashboardTests/PrivacyDashboardControllerTests.swift
+++ SharedPackages/BrowserServicesKit/Tests/PrivacyDashboardTests/PrivacyDashboardControllerTests.swift
@@ -289,7 +289,9 @@ class MockWebView: WKWebView {
             capturedJavaScriptString = javaScriptString
             expectation.fulfill()
         }
-        return try await super.evaluateJavaScript(javaScriptString) as Void? as Any
+
+        let superResult: Any? = try await super.evaluateJavaScript(javaScriptString)
+        return superResult as Any
     }
 
 }

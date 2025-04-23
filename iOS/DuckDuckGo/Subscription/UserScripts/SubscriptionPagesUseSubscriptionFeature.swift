2f26615d28f02a61eee1c9ee971cf62371e7954a
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-04-23 19:45:29 +0200

    Some additional changes

--- iOS/DuckDuckGo/Subscription/UserScripts/SubscriptionPagesUseSubscriptionFeature.swift
+++ iOS/DuckDuckGo/Subscription/UserScripts/SubscriptionPagesUseSubscriptionFeature.swift
@@ -370,14 +370,10 @@ final class DefaultSubscriptionPagesUseSubscriptionFeature: SubscriptionPagesUse
         if shouldPerformFreeTrialPostPurchaseActions {
             subscriptionParameters = completeSubscriptionFreeTrialParameters
             fireFreeTrialSubscriptionPurchasePixel(for: subscriptionSelection.id)
+        } else if let feExperiment = subscriptionSelection.experiment {
+            subscriptionParameters = feExperiment.asParameters()
         }
 
-        if let feExperiment = subscriptionSelection.experiment {
-            subscriptionParameters = {
-                let parameters = subscriptionParameters ?? [String: String]()
-                return parameters.merging(feExperiment.asParameters()) { $1 }
-            }()
-        }
 
         // Privacy Pro Promotion Experiment Pixels
         firePrivacyProPromotionSubscriptionPurchasePixel(for: subscriptionSelection.id)
@@ -892,7 +888,20 @@ final class DefaultSubscriptionPagesUseSubscriptionFeatureV2: SubscriptionPagesU
         resetSubscriptionFlow()
 
         struct SubscriptionSelection: Decodable {
+            struct Experiment: Codable {
+                let name: String
+                let cohort: String
+
+                func asParameters() -> [String: String] {
+                    [
+                        "experimentName": name,
+                        "experimentCohort": cohort,
+                    ]
+                }
+            }
+
             let id: String
+            let experiment: Experiment?
         }
 
         let message = original
@@ -954,18 +963,21 @@ final class DefaultSubscriptionPagesUseSubscriptionFeatureV2: SubscriptionPagesU
             return nil
         }
 
+        var subscriptionParameters: [String: String]?
+
         // Free Trials Experiment Parameters & Pixels
-        var freeTrialParameters: [String: String]?
         if shouldPerformFreeTrialPostPurchaseActions {
-            freeTrialParameters = completeSubscriptionFreeTrialParameters
+            subscriptionParameters = completeSubscriptionFreeTrialParameters
             fireFreeTrialSubscriptionPurchasePixel(for: subscriptionSelection.id)
+        } else if let feExperiment = subscriptionSelection.experiment {
+            subscriptionParameters = feExperiment.asParameters()
         }
 
         // Privacy Pro Promotion Experiment Pixels
         firePrivacyProPromotionSubscriptionPurchasePixel(for: subscriptionSelection.id)
 
         switch await appStorePurchaseFlow.completeSubscriptionPurchase(with: purchaseTransactionJWS,
-                                                                       additionalParams: freeTrialParameters) {
+                                                                       additionalParams: subscriptionParameters) {
         case .success:
             Logger.subscription.log("Subscription purchase completed successfully")
             DailyPixel.fireDailyAndCount(pixel: .privacyProPurchaseSuccess,

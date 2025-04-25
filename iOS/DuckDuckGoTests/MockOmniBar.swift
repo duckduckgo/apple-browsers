30738ff518e2cc28c6529537706cd4d52d1b682f
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-04-25 09:31:57 +0200

    Satisfy Swiftlint

--- iOS/DuckDuckGoTests/MockOmniBar.swift
+++ iOS/DuckDuckGoTests/MockOmniBar.swift
@@ -60,17 +60,17 @@ final class MockOmniBar: OmniBar {
     func resetPrivacyIcon(for url: URL?) { }
     func cancelAllAnimations() { }
     func completeAnimationForDaxDialog() { }
-
-    final class MockOmniBarView: UIView, OmniBarView {        
-
+    
+    final class MockOmniBarView: UIView, OmniBarView {
+        
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
-
+        
         init() {
             super.init(frame: .zero)
         }
-
+        
         var text: String?
         static let expectedHeight: CGFloat = 52
         var expectedHeight: CGFloat = MockOmniBarView.expectedHeight
@@ -92,12 +92,12 @@ final class MockOmniBar: OmniBar {
         var leftIconContainerView: UIView! = UIView()
         var customIconView: UIImageView = UIImageView()
         var clearButton: UIButton! = UIButton()
-
+        
         func showSeparator() { }
         func hideSeparator() { }
         func moveSeparatorToTop() { }
         func moveSeparatorToBottom() { }
-
+        
         var progressView: DuckDuckGo.ProgressView?
         var privacyIconView: UIView?
         var searchContainerWidth: CGFloat = 0
@@ -119,11 +119,11 @@ final class MockOmniBar: OmniBar {
         var onDismissPressed: (() -> Void)?
         var onSettingsLongPress: (() -> Void)?
         var onAccessoryLongPress: (() -> Void)?
-
+        
         static func create() -> Self {
             Self.init()
         }
-
+        
         var isPrivacyInfoContainerHidden: Bool = true
         var isClearButtonHidden: Bool = true
         var isMenuButtonHidden: Bool = true
@@ -138,6 +138,6 @@ final class MockOmniBar: OmniBar {
         var isAccessoryButtonHidden: Bool = true
         var isSearchLoupeHidden: Bool = true
         var isDismissButtonHidden: Bool = true
-
+        
     }
 }

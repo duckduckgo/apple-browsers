ac57a275f3d64ce6ca6811f8fc99966c577069f6
Michal Smaga <msmaga@duckduckgo.com>
2025-04-29 13:38:45 +0200

    Create preference views for PIR and ITR

---
+++ macOS/LocalPackages/SubscriptionUI/Sources/SubscriptionUI/Preferences/PreferencesIdentityTheftRestorationModel.swift
@@ -0,0 +1,62 @@
+//
+//  PreferencesIdentityTheftRestorationModel.swift
+//
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import AppKit
+import Subscription
+
+public final class PreferencesIdentityTheftRestorationModel: ObservableObject {
+
+    private let subscriptionManager: SubscriptionManager
+    private var accountManager: AccountManager {
+        subscriptionManager.accountManager
+    }
+    private let openURLHandler: (URL) -> Void
+    public let userEventHandler: (PreferencesSubscriptionModel.UserEvent) -> Void
+
+    public init(openURLHandler: @escaping (URL) -> Void,
+                userEventHandler: @escaping (PreferencesSubscriptionModel.UserEvent) -> Void,
+                subscriptionManager: SubscriptionManager) {
+        self.subscriptionManager = subscriptionManager
+        self.openURLHandler = openURLHandler
+        self.userEventHandler = userEventHandler
+    }
+
+    deinit {
+
+    }
+
+    @MainActor
+    func didAppear() {
+//        userEventHandler(.openSubscriptionSettingsClick)
+    }
+
+//    @MainActor
+//    func openPersonalInformationRemoval() {
+//        userEventHandler(.openDB)
+//    }
+
+    @MainActor
+    func openIdentityTheftRestoration() {
+        userEventHandler(.openITR)
+    }
+
+    @MainActor
+    func openFAQ() {
+        openURLHandler(subscriptionManager.url(for: .faq))
+    }
+}

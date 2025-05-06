1b563a81375c7fd0ff03fd7d61c5b852cd400756
Elle Sullivan <ellesailenesullivan@gmail.com>
2025-05-06 16:53:59 +0100

    Fix file header

--- iOS/DuckDuckGo/DataBrokerProtectionDebugViewController.swift
+++ iOS/DuckDuckGo/DataBrokerProtectionDebugViewController.swift
@@ -1,8 +1,8 @@
 //
-//  NetworkProtectionDebugViewController.swift
+//  DataBrokerProtectionDebugViewController.swift
 //  DuckDuckGo
 //
-//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.

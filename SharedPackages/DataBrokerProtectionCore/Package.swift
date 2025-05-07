51d4b973ef3ab21b4438ddf4a7288cd50fbd2d7b
Anh Do <ado@duckduckgo.com>
2025-05-07 13:47:42 -0400

    Add FeatureFlags as dependency to DBPCore

--- SharedPackages/DataBrokerProtectionCore/Package.swift
+++ SharedPackages/DataBrokerProtectionCore/Package.swift
@@ -34,6 +34,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../BrowserServicesKit"),
+        .package(path: "../FeatureFlags"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", exact: "0.9.19"),
     ],
     targets: [
@@ -43,6 +44,7 @@ let package = Package(
                 .product(name: "BrowserServicesKit", package: "BrowserServicesKit"),
                 .product(name: "PixelKit", package: "BrowserServicesKit"),
                 .product(name: "Persistence", package: "BrowserServicesKit"),
+                .product(name: "FeatureFlags", package: "FeatureFlags"),
                 "ZIPFoundation",
             ],
             resources: [.copy("BundleResources")],

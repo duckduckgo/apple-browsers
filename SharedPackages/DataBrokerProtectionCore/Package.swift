91ee7fa498a2e8fecdb49368edf255fe948181b3
Anh Do <ado@duckduckgo.com>
2025-05-08 11:54:51 -0400

    Update Package.swift

--- SharedPackages/DataBrokerProtectionCore/Package.swift
+++ SharedPackages/DataBrokerProtectionCore/Package.swift
@@ -43,7 +43,6 @@ let package = Package(
                 .product(name: "BrowserServicesKit", package: "BrowserServicesKit"),
                 .product(name: "PixelKit", package: "BrowserServicesKit"),
                 .product(name: "Persistence", package: "BrowserServicesKit"),
-                .product(name: "FeatureFlags", package: "FeatureFlags"),
                 "ZIPFoundation",
             ],
             resources: [.copy("BundleResources")],

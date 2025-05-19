11e49e85cb2eb71b15a22720f5f38e6342754801
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-05-19 14:30:44 +0200

    Add comment about preventing hang

--- iOS/DuckDuckGo/NewTabPageView.swift
+++ iOS/DuckDuckGo/NewTabPageView.swift
@@ -74,7 +74,9 @@ private extension NewTabPageView {
     private var sectionsView: some View {
         GeometryReader { proxy in
             NewTabPageShadowScrollView(shadowColor: .init(designSystemColor: .shadowPrimary), setUpScrollView: {
+                // This setting prevents from going into redraw loop for the hosted SUI view when opening a keyboard covering part of the hosted content.
                 $0.contentInsetAdjustmentBehavior = .never
+                
                 $0.backgroundColor = UIColor(designSystemColor: .background)
                 $0.alwaysBounceVertical = true
                 $0.keyboardDismissMode = .onDrag

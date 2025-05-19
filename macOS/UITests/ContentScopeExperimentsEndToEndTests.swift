44bba20e122aef2329f21f8c6e6e0f097dab03b2
Sabrina Tardio <stardio@duckduckgo.com>
2025-05-19 14:53:52 +0200

    fix test

--- macOS/UITests/ContentScopeExperimentsEndToEndTests.swift
+++ macOS/UITests/ContentScopeExperimentsEndToEndTests.swift
@@ -39,7 +39,9 @@ final class ContentScopeExperimentsEndToEndTests: XCTestCase {
         let textField = app.dialogs["alert"].children(matching: .textField).element
         XCTAssertTrue(textField.waitForExistence(timeout: 3), "Custom config alert did not appear.")
         textField.typeURL(configURL, pressingEnter: false)
-        app.buttons["action-button-1"].click()
+        let button = app.buttons["action-button-1"]
+        XCTAssertTrue(button.waitForExistence(timeout: 3), "The action button did not appear.")
+        button.click()
 
         // Step 2: Load test page
         let testPageUrl = URL(string: "https://privacy-test-pages.site/content-scope-scripts/infra/pages/conditional-matching-experiments.html")!

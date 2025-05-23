e41e6a36067059847e32ce9c759ccb2647cf4520
Tomas Strba <tom@duckduckgo.com>
2025-05-23 12:25:41 +0200

    'quit without clearing' session restoration with duplicate windows fixed

--- macOS/DuckDuckGo/Application/AppDelegate.swift
+++ macOS/DuckDuckGo/Application/AppDelegate.swift
@@ -546,6 +546,8 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             stateRestorationManager.applicationDidFinishLaunching()
         }
 
+        setUpAutoClearHandler()
+
         BWManager.shared.initCommunication()
 
         if WindowsManager.windows.first(where: { $0 is MainWindow }) == nil,
@@ -599,8 +601,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         DataBrokerProtectionAppEvents(featureGatekeeper: pirGatekeeper).applicationDidFinishLaunching()
 
         TipKitAppEventHandler(featureFlagger: featureFlagger).appDidFinishLaunching()
-
-        setUpAutoClearHandler()
 
         setUpAutofillPixelReporter()
 
@@ -954,6 +954,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
                 NSApplication.shared.reply(toApplicationShouldTerminate: true)
             }
         }
+        self.autoClearHandler.restoreTabsIfNeeded()
     }
 
     private func setUpAutofillPixelReporter() {

88d3b9f9bebc3c2d719edb829add963c5ce0fe1d
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-05-28 10:07:37 +0200

    Satisfy linter

--- SharedPackages/Onboarding/Package.swift
+++ SharedPackages/Onboarding/Package.swift
@@ -1,5 +1,8 @@
-//  swift-tools-version: 5.9
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
 //  Package.swift
+//
 //  DuckDuckGo
 //
 //  Copyright © 2025 DuckDuckGo. All rights reserved.

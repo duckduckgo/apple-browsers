0882236356c575317c6f893f877d71653d8c7622
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-05-28 16:02:42 +0200

    Use different icons for review and release DMGs

--- macOS/scripts/archive.sh
+++ macOS/scripts/archive.sh
@@ -42,11 +42,13 @@ read_command_line_arguments() {
 
 	case "$1" in
 		review)
+			release_type="review"
 			app_name="DuckDuckGo Review"
 			scheme="macOS Browser Review"
 			configuration="Review"
 			;;
 		release)
+			release_type="release"
 			app_name="DuckDuckGo"
 			scheme="macOS Browser"
 			configuration="Release"
@@ -286,7 +288,7 @@ create_dmg() {
 
 	local dmg_dir="${workdir}/dmg"
 	local dmg_background="${cwd}/assets/dmg-background.png"
-	local dmg_icon="${cwd}/assets/dmg.icns"
+	local dmg_icon="${cwd}/assets/dmg-${release_type}.icns"
 	dmg_output_path="${workdir}/duckduckgo-${version_identifier}.dmg"
 
 	rm -rf "${dmg_dir}" "${dmg_output_path}"

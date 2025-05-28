990e2bd759737ffdaa7abfe0754e568a9c9e9271
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-05-28 09:25:02 +0200

    Prevent white stripes when dismissing keyboard

--- iOS/DuckDuckGo/NewTabPageShadowScrollView.swift
+++ iOS/DuckDuckGo/NewTabPageShadowScrollView.swift
@@ -115,7 +115,7 @@ struct NewTabPageShadowScrollView<Content: View>: UIViewControllerRepresentable 
         let bottomShadowView = makeShadowView(isTop: false)
         
         [topShadowView, bottomShadowView].forEach {
-            $0.backgroundColor = .white
+            $0.backgroundColor = parentView.backgroundColor
             $0.translatesAutoresizingMaskIntoConstraints = false
             parentView.addSubview($0)
         }

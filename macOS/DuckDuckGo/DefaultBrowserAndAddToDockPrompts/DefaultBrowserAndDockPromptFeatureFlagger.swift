19e47ee04ca94b6928d25c2c131b153d220052fd
Alessandro Boron <aboron@duckduckgo.com>
2025-05-29 17:43:44 +1000

    Add subfeature settings parsing for scheduled SAD/ATT prompts

--- macOS/DuckDuckGo/DefaultBrowserAndAddToDockPrompts/DefaultBrowserAndDockPromptFeatureFlagger.swift
+++ macOS/DuckDuckGo/DefaultBrowserAndAddToDockPrompts/DefaultBrowserAndDockPromptFeatureFlagger.swift
@@ -18,6 +18,7 @@
 
 import Foundation
 import BrowserServicesKit
+import struct Common.CodableHelper
 import FeatureFlags
 
 public protocol DefaultBrowserAndDockPromptFeatureFlagProvider {
@@ -35,32 +36,22 @@ public protocol DefaultBrowserAndDockPromptFeatureFlagsSettingsProvider {
     var bannerRepeatIntervalDays: Int { get }
 }
 
-/// An enum representing the different settings for Set Default Browser (SAD) and Add to Dock (ATT) feature flag.
-public enum DefaultBrowserAndDockPromptFeatureSettings: String {
-    /// The setting for the number of days to wait after app installation before showing the first popover. Default to 14 days.
-    case firstPopoverDelayDays
-    /// The setting for the number of days to wait after the popover has been shown before displaying the banner. Default to 14 days.
-    case bannerAfterPopoverDelayDays
-    /// The settings for the number of days between subsequent displays of the banner. Default to 14 days.
-    case bannerRepeatIntervalDays
-
-    public var defaultValue: Int {
-        switch self {
-        case .firstPopoverDelayDays: return 14
-        case .bannerAfterPopoverDelayDays: return 14
-        case .bannerRepeatIntervalDays: return 14
-        }
-    }
-}
-
 typealias DefaultBrowserAndDockPromptFeatureFlagger = DefaultBrowserAndDockPromptFeatureFlagProvider & DefaultBrowserAndDockPromptFeatureFlagsSettingsProvider
 
 final class DefaultBrowserAndDockPromptFeatureFlag {
     private let privacyConfigManager: PrivacyConfigurationManaging
     private let featureFlagger: FeatureFlagger
 
-    private var remoteSettings: PrivacyConfigurationData.PrivacyFeature.FeatureSettings {
-        privacyConfigManager.privacyConfig.settings(for: .setAsDefaultAndAddToDock)
+    private var remoteSubfeatureSettings: ScheduledDefaultBrowserAndDockPromptSettings {
+        guard
+            let subFeatureJSON = privacyConfigManager.privacyConfig.settings(for: SetAsDefaultAndAddToDockSubfeature.scheduledSetDefaultBrowserAndAddToDockPrompts),
+            let data = subFeatureJSON.data(using: .utf8),
+            let decodedSettings: ScheduledDefaultBrowserAndDockPromptSettings = CodableHelper.decode(jsonData: data)
+        else {
+            // Return default values if cannot
+            return ScheduledDefaultBrowserAndDockPromptSettings()
+        }
+        return decodedSettings
     }
 
     public init(privacyConfigManager: PrivacyConfigurationManaging, featureFlagger: FeatureFlagger) {
@@ -74,7 +65,7 @@ final class DefaultBrowserAndDockPromptFeatureFlag {
 extension DefaultBrowserAndDockPromptFeatureFlag: DefaultBrowserAndDockPromptFeatureFlagProvider {
 
     public var isDefaultBrowserAndDockPromptFeatureEnabled: Bool {
-        featureFlagger.isFeatureOn(for: FeatureFlag.popoverVsBannerExperiment)
+        featureFlagger.isFeatureOn(for: FeatureFlag.scheduledSetDefaultBrowserAndAddToDockPrompts)
     }
 
 }
@@ -84,19 +75,27 @@ extension DefaultBrowserAndDockPromptFeatureFlag: DefaultBrowserAndDockPromptFea
 extension DefaultBrowserAndDockPromptFeatureFlag: DefaultBrowserAndDockPromptFeatureFlagsSettingsProvider {
 
     public var firstPopoverDelayDays: Int {
-        getSettings(.firstPopoverDelayDays)
+        remoteSubfeatureSettings.firstPopoverDelayDays
     }
 
     public var bannerAfterPopoverDelayDays: Int {
-        getSettings(.bannerAfterPopoverDelayDays)
+        remoteSubfeatureSettings.bannerAfterPopoverDelayDays
     }
 
     public var bannerRepeatIntervalDays: Int {
-        getSettings(.bannerRepeatIntervalDays)
+        remoteSubfeatureSettings.bannerRepeatIntervalDays
     }
 
-    private func getSettings(_ value: DefaultBrowserAndDockPromptFeatureSettings) -> Int {
-        remoteSettings[value.rawValue] as? Int ?? value.defaultValue
-    }
+}
 
+// MARK: - ScheduledDefaultBrowserAndDockPromptSettings
+
+/// An struct representing the different settings for Set Default Browser (SAD) and Add to Dock (ATT) feature flag.
+struct ScheduledDefaultBrowserAndDockPromptSettings: Codable {
+    /// The setting for the number of days to wait after app installation before showing the first popover. Default to 14 days.
+    private(set) var firstPopoverDelayDays: Int = 14
+    /// The setting for the number of days to wait after the popover has been shown before displaying the banner. Default to 14 days.
+    private(set) var bannerAfterPopoverDelayDays: Int = 14
+    /// The settings for the number of days between subsequent displays of the banner. Default to 14 days.
+    private(set) var bannerRepeatIntervalDays: Int = 14
 }

7d6f32a0f93c1f85b22470dacd16d2acb898ea15
Graeme Arthur <garthur@duckduckgo.com>
2025-05-30 16:36:20 +0200

    Make sure pixels are all properly defined
    7e3d863c8e1449b0e30d789a4e94f5210d476b94

--- macOS/DuckDuckGo/Sync/SyncPixels.swift
+++ macOS/DuckDuckGo/Sync/SyncPixels.swift
@@ -49,3 +49,66 @@ enum SyncSwitchAccountPixelKitEvent: PixelKitEventV2 {
         NonStandardEvent(self)
     }
 }
+
+enum SyncSetupPixelKitEvent: PixelKitEventV2 {
+    enum Source: String {
+        case exchange
+        case connect
+    }
+
+    enum ParameterKey {
+        static let source = "source"
+    }
+
+    case syncSetupBarcodeScreenShown(Source)
+    case syncSetupBarcodeScannerSuccess(Source)
+    case syncSetupBarcodeScannerFailed(Source)
+    case syncSetupBarcodeCodeCopied(Source)
+    case syncSetupManualCodeEntryScreenShown(Source)
+    case syncSetupManualCodeEntered(Source)
+    case syncSetupManualCodeEnteredFailed(Source)
+    case syncSetupEndedAbandoned(Source)
+    case syncSetupEndedSuccessful(Source)
+
+    var name: String {
+        switch self {
+        case .syncSetupBarcodeScreenShown: return "sync_setup_barcode_screen_shown"
+        case .syncSetupBarcodeScannerSuccess: return "sync_setup_barcode_scanner_success"
+        case .syncSetupBarcodeScannerFailed: return "sync_setup_barcode_scanner_failed"
+        case .syncSetupBarcodeCodeCopied: return "sync_setup_barcode_code_copied"
+        case .syncSetupManualCodeEntryScreenShown: return "sync_setup_manual_code_entry_screen_shown"
+        case .syncSetupManualCodeEntered: return "sync_setup_manual_code_entered"
+        case .syncSetupManualCodeEnteredFailed: return "sync_setup_manual_code_entered_failed"
+        case .syncSetupEndedAbandoned: return "sync_setup_ended_abandoned"
+        case .syncSetupEndedSuccessful: return "sync_setup_ended_successful"
+        }
+    }
+
+    var parameters: [String: String]? {
+        return [ParameterKey.source: source.rawValue]
+    }
+
+    var error: (any Error)? {
+        nil
+    }
+
+    var withoutMacPrefix: NonStandardEvent {
+        NonStandardEvent(self)
+    }
+
+    private var source: Source {
+        switch self {
+        case
+            .syncSetupBarcodeScreenShown(let source),
+            .syncSetupBarcodeScannerSuccess(let source),
+            .syncSetupBarcodeScannerFailed(let source),
+            .syncSetupBarcodeCodeCopied(let source),
+            .syncSetupManualCodeEntryScreenShown(let source),
+            .syncSetupManualCodeEntered(let source),
+            .syncSetupManualCodeEnteredFailed(let source),
+            .syncSetupEndedAbandoned(let source),
+            .syncSetupEndedSuccessful(let source):
+            return source
+        }
+    }
+}

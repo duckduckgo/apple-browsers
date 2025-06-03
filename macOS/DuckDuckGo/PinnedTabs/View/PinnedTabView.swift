30780e20de2aca0f0f3801f1b6b97a3418b9f86d
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2025-06-03 11:59:45 -0300

    Decrease width of inside of pinned tab when hovering in legacy UI

--- macOS/DuckDuckGo/PinnedTabs/View/PinnedTabView.swift
+++ macOS/DuckDuckGo/PinnedTabs/View/PinnedTabView.swift
@@ -249,20 +249,11 @@ struct PinnedTabInnerView: View {
         ZStack {
             Rectangle()
                 .foregroundColor(foregroundColor)
-                .frame(width: shouldApplyNewHoverState ? width - 8 : width,
+                .frame(width: shouldApplyNewHoverState ? width - 8 : showSShaped ? width : width-2,
                        height: shouldApplyNewHoverState ? height - 8 : height)
                 .cornerRadius(
                     shouldApplyNewHoverState ? 6 : PinnedTabView.Const.cornerRadius,
                     corners: shouldApplyNewHoverState ? [.topLeft, .topRight, .bottomLeft, .bottomRight] : [.topLeft, .topRight]
-                )
-                .clipShape(
-                    CustomRoundedCornersShape(
-                        inset: 0,
-                        tl: shouldApplyNewHoverState ? 6 : PinnedTabView.Const.cornerRadius,
-                        tr: shouldApplyNewHoverState ? 6 : PinnedTabView.Const.cornerRadius,
-                        bl: shouldApplyNewHoverState ? 6 : 0,
-                        br: shouldApplyNewHoverState ? 6 : 0
-                    )
                 )
 
             if drawSeparator {

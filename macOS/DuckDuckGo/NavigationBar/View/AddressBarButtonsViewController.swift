0aba04047652916273e447e09a3a4359d480ddbb
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2025-06-03 15:15:15 -0300

    New icons and animation (privacy shield with checkmark)

--- macOS/DuckDuckGo/NavigationBar/View/AddressBarButtonsViewController.swift
+++ macOS/DuckDuckGo/NavigationBar/View/AddressBarButtonsViewController.swift
@@ -77,6 +77,7 @@ final class AddressBarButtonsViewController: NSViewController {
     var shieldAnimationView: LottieAnimationView!
     var shieldDotAnimationView: LottieAnimationView!
     @IBOutlet weak var privacyShieldLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var animationWrapperViewLeadingConstraint: NSLayoutConstraint!
 
     @IBOutlet weak var aiChatDivider: NSImageView!
     @IBOutlet weak var aiChatStackTrailingViewConstraint: NSLayoutConstraint!
@@ -236,6 +237,7 @@ final class AddressBarButtonsViewController: NSViewController {
         guard visualStyle.addressBarStyleProvider.shouldAddPaddingToAddressBarButtons else { return }
 
         imageButtonLeadingConstraint.constant = isFocused ? 2 : 1
+        animationWrapperViewLeadingConstraint.constant = 1
 
         if let superview = privacyEntryPointButton.superview {
             privacyEntryPointButton.translatesAutoresizingMaskIntoConstraints = false

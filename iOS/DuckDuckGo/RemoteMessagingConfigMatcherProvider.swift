fd589af065f4354ced126a816f83257f540df780
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-06-05 10:22:38 +0200

    Prevent showing remote message before visual updates are applied in runtime

--- iOS/DuckDuckGo/RemoteMessagingConfigMatcherProvider.swift
+++ iOS/DuckDuckGo/RemoteMessagingConfigMatcherProvider.swift
@@ -115,7 +115,21 @@ final class RemoteMessagingConfigMatcherProvider: RemoteMessagingConfigMatcherPr
         let shownMessageIds = store.fetchShownRemoteMessageIDs()
 
         let enabledFeatureFlags: [String] = FeatureFlag.allCases.filter { flag in
-            flag.cohortType == nil && featureFlagger.isFeatureOn(for: flag)
+            guard flag.cohortType == nil else { return false }
+
+            let isFlagEnabled: Bool
+            switch flag {
+            case .visualUpdates:
+                // Use value established at launch instead of live feature flag.
+                // This prevents showing remote message that's based on this flag prematurely,
+                // i.e. before `visualUpdates` flag becomes effective after restart.
+                // See https://app.asana.com/1/137249556945/project/1206226850447395/task/1210454132810101
+                isFlagEnabled = ThemeManager.shared.properties.isExperimentalThemingEnabled
+            default:
+                isFlagEnabled = featureFlagger.isFeatureOn(for: flag)
+            }
+
+            return isFlagEnabled
         }.map(\.rawValue)
 
         return RemoteMessagingConfigMatcher(

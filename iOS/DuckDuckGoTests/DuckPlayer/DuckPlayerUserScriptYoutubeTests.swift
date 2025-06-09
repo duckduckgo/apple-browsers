37b346158e9addc633bb658487c8716783bb35a5
Daniel Bernal <dbernal@duckduckgo.com>
2025-06-09 15:07:58 +0200

    Update Mock

--- iOS/DuckDuckGoTests/DuckPlayer/DuckPlayerUserScriptYoutubeTests.swift
+++ iOS/DuckDuckGoTests/DuckPlayer/DuckPlayerUserScriptYoutubeTests.swift
@@ -40,8 +40,8 @@ class DuckPlayerUserScriptYoutubeTests: XCTestCase {
         let mockSettings = MockDuckPlayerSettings(
             appSettings: AppSettingsMock(),
             privacyConfigManager: MockPrivacyConfigurationManager(),
-            internalUserDecider: MockDuckPlayerInternalUserDecider(),
-            featureFlagger: MockDuckPlayerFeatureFlagger()
+            featureFlagger: MockDuckPlayerFeatureFlagger(),
+            internalUserDecider: MockDuckPlayerInternalUserDecider()
         )
         
         let mockFeatureFlagger = MockDuckPlayerFeatureFlagger()

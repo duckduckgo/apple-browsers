3b143ea7221330f7e64c86bd7272a7a9501965e3
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-06-11 17:34:38 +0200

    Fixes swiftlint warnings

--- macOS/UnitTests/Common/OSVersion/SupportedOSCheckerTests.swift
+++ macOS/UnitTests/Common/OSVersion/SupportedOSCheckerTests.swift
@@ -1,7 +1,7 @@
 //
 //  SupportedOSCheckerTests.swift
 //
-//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-//SupportedOSChecker
+//
 
 import XCTest
 import FeatureFlags
@@ -196,7 +196,7 @@ final class SupportedOSCheckerTests: XCTestCase {
         }
         XCTAssertEqual(version, "12.3")
     }
-    
+
     func testWhenWillSoonDropBigSurSupportFeatureFlagIsOnThenShowsUpcomingWarning() {
         // Given
         let mockFeatureFlagger = FeatureFlaggerMock()

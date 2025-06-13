d4a6fa38db6145a439fe101a7b9fc90586cd8429
Elle Sullivan <ellesailenesullivan@gmail.com>
2025-06-13 18:24:04 +0100

    Add supporting methods to DBPUICommunicationModel

--- SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCore/UIWeb/DBPUICommunicationModel.swift
+++ SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCore/UIWeb/DBPUICommunicationModel.swift
@@ -77,7 +77,7 @@ public struct DBPUIStandardResponse: Codable {
 }
 
 /// Message Object representing a user profile name
-public struct DBPUIUserProfileName: Codable {
+public struct DBPUIUserProfileName: Codable, Equatable {
     public let first: String
     public let middle: String?
     public let last: String
@@ -89,10 +89,14 @@ public struct DBPUIUserProfileName: Codable {
         self.last = last
         self.suffix = suffix
     }
+
+    public func requiredComponentsAreBlank() -> Bool {
+        return first.isBlank || last.isBlank
+    }
 }
 
 /// Message Object representing a user profile address
-public struct DBPUIUserProfileAddress: Codable {
+public struct DBPUIUserProfileAddress: Codable, Equatable {
     public let street: String?
     public let city: String
     public let state: String
@@ -103,6 +107,10 @@ public struct DBPUIUserProfileAddress: Codable {
         self.city = city
         self.state = state
         self.zipCode = zipCode
+    }
+
+    public func requiredComponentsAreBlank() -> Bool {
+        return city.isBlank || state.isBlank
     }
 }
 
@@ -126,6 +134,14 @@ public struct DBPUIUserProfile: Codable {
         self.names = names
         self.birthYear = birthYear
         self.addresses = addresses
+    }
+}
+
+public extension DBPUIUserProfile {
+    init(fromDataBrokerProtectionProfile profile: DataBrokerProtectionProfile) {
+        names = profile.names.map { DBPUIUserProfileName(first: $0.firstName, middle: $0.middleName, last: $0.lastName, suffix: $0.suffix) }
+        addresses = profile.addresses.map { DBPUIUserProfileAddress(street: $0.street, city: $0.city, state: $0.state, zipCode: $0.zipCode) }
+        birthYear = profile.birthYear
     }
 }
 
@@ -366,6 +382,17 @@ public struct DBPUIScanAndOptOutMaintenanceState: DBPUISendableMessage {
     }
 }
 
+public extension DBPUIScanAndOptOutMaintenanceState {
+    static func emptyMaintenanceState() -> Self {
+        return .init(
+            inProgressOptOuts: [],
+            completedOptOuts: [],
+            scanSchedule: DBPUIScanSchedule(lastScan: DBPUIScanDate(date: 0, dataBrokers: []), nextScan: DBPUIScanDate(date: 0, dataBrokers: [])),
+            scanHistory: DBPUIScanHistory(sitesScanned: 0)
+        )
+    }
+}
+
 public struct DBPUIOptOutMatch: DBPUISendableMessage {
     public let dataBroker: DBPUIDataBroker
     public let matches: Int
@@ -436,6 +463,12 @@ public struct DBPUIInitialScanState: DBPUISendableMessage {
     public init(resultsFound: [DBPUIDataBrokerProfileMatch], scanProgress: DBPUIScanProgress) {
         self.resultsFound = resultsFound
         self.scanProgress = scanProgress
+    }
+}
+
+extension DBPUIInitialScanState {
+    public static func emptyInitialScanState() -> DBPUIInitialScanState {
+        DBPUIInitialScanState(resultsFound: [], scanProgress: DBPUIScanProgress(currentScans: 0, totalScans: 0, scannedBrokers: []))
     }
 }
 

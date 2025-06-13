67c7bd9a31c5fdf7d6e263bdeceb7ec552b375fc
Graeme Arthur <garthur@duckduckgo.com>
2025-06-13 18:57:43 +0200

    Missing fill event?

--- iOS/DuckDuckGo/AutofillLoginPromptViewController.swift
+++ iOS/DuckDuckGo/AutofillLoginPromptViewController.swift
@@ -117,6 +117,8 @@ extension AutofillLoginPromptViewController: AutofillLoginPromptViewModelDelegat
             Pixel.fire(pixel: .autofillLoginsFillLoginInlineManualConfirmed, withAdditionalParameters: parameters)
         }
 
+        NotificationCenter.default.post(name: .autofillFillEvent, object: nil)
+
         onAccountSelected(account)
 
         if AppDependencyProvider.shared.autofillLoginSession.isSessionValid {

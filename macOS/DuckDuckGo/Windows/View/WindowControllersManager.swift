34b6f336c419c8dff789e816c8647eaa246fe169
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-06-16 12:58:55 +0200

    Remove openAIChat default parameter for hasPrompt

--- macOS/DuckDuckGo/Windows/View/WindowControllersManager.swift
+++ macOS/DuckDuckGo/Windows/View/WindowControllersManager.swift
@@ -204,7 +204,7 @@ extension WindowControllersManager {
     ///   - linkOpenBehavior: Specifies where to open the URL. Defaults to `.currentTab`.
     ///   - hasPrompt: If `true` and the current tab is an AI chat, reloads the tab. Ignored if `target` is `.newTabSelected`
     ///                or `.newTabUnselected`.
-    func openAIChat(_ url: URL, with linkOpenBehavior: LinkOpenBehavior = .currentTab, hasPrompt: Bool = false) {
+    func openAIChat(_ url: URL, with linkOpenBehavior: LinkOpenBehavior = .currentTab, hasPrompt: Bool) {
 
         let tabCollectionViewModel = mainWindowController?.mainViewController.tabCollectionViewModel
 

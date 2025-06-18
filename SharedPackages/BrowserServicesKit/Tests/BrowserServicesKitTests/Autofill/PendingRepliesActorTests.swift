60701bfbba450ed58d6d8eaaad8db8ad5fc323d0
amddg44 <amallon@duckduckgo.com>
2025-06-18 23:38:04 +0200

    Linter

--- SharedPackages/BrowserServicesKit/Tests/BrowserServicesKitTests/Autofill/PendingRepliesActorTests.swift
+++ SharedPackages/BrowserServicesKit/Tests/BrowserServicesKitTests/Autofill/PendingRepliesActorTests.swift
@@ -1,8 +1,19 @@
 //
 //  PendingRepliesActorTests.swift
-//  BrowserServicesKit
 //
-//  Created by Anya Mallon on 18/06/2025.
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import XCTest
@@ -72,26 +83,31 @@ final class PendingRepliesActorTests: XCTestCase {
         waitForExpectations(timeout: 1.0)
     }
 
-    func testWhenRegisteringTwoHandlersForSameType_ThenSecondReceivesResponse() {
-        let realExp = expectation(description: #function)
+    func testWhenRegisteringTwoHandlersForSameType_ThenFirstIsCancelledAndSecondReceivesResponse() {
+        let cancelExp = expectation(description: #function + "_cancel")
+        let realExp   = expectation(description: #function + "_real")
 
         Task {
-            // 1) register handler1 (it will be cancelled by the next registration)
-            await actor.register({ _ in
-                XCTFail("First handler should have been cancelled")
+            // 1) First handler: should be cancelled by the next register call.
+            await actor.register({ response in
+                // decode NoActionResponse to verify .none action
+                let data = response?.data(using: .utf8)
+                let result = try? JSONDecoder().decode(AutofillUserScript.NoActionResponse.self, from: data!)
+                XCTAssertEqual(result?.success.action, AutofillUserScript.NoActionResponse.NoActionType.none)
+                cancelExp.fulfill()
             }, for: "dupType")
 
-            // 2) register handler2
+            // 2) Second handler: the one that should receive the real payload
             await actor.register({ response in
                 XCTAssertEqual(response, "real")
                 realExp.fulfill()
             }, for: "dupType")
 
-            // 3) send: only handler2 should fire
+            // 3) Send: only the second handler fires with “real”
             await actor.send(response: "real", for: "dupType")
         }
 
-        waitForExpectations(timeout: 1.0)
+        wait(for: [cancelExp, realExp], timeout: 1.0)
     }
 
     func testWhenCancelAll_ThenAllHandlersAreCancelled() {

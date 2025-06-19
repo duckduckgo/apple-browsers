aefc3037dd841dcaafbfbd33b8f6986ac580357c
Elle Sullivan <ellesailenesullivan@gmail.com>
2025-06-19 14:00:31 +0100

    Add tests for element sorting

--- SharedPackages/DataBrokerProtectionCore/Tests/DataBrokerProtectionCoreTests/BrokerProfileQueryDataTests.swift
+++ SharedPackages/DataBrokerProtectionCore/Tests/DataBrokerProtectionCoreTests/BrokerProfileQueryDataTests.swift
@@ -53,14 +53,14 @@ final class BrokerProfileQueryDataTests: XCTestCase {
 
     // namesOfBrokersScannedIncludingMirrorSites tests
 
-    func testNamesOfBrokersScannedIncludingMirrorSites_whenNoLastRunDate_thenReturnsEmptyArray() {
+    func test_namesOfBrokersScannedIncludingMirrorSites_whenNoLastRunDate_thenReturnsEmptyArray() {
         let queryData = BrokerProfileQueryData.mock(lastRunDate: nil)
 
         let result = queryData.namesOfBrokersScannedIncludingMirrorSites()
         XCTAssertTrue(result.isEmpty)
     }
 
-    func testNamesOfBrokersScannedIncludingMirrorSites_whenNoMirrorSites_ThenReturnsOnlyMainBrokerName() {
+    func test_namesOfBrokersScannedIncludingMirrorSites_whenNoMirrorSites_ThenReturnsOnlyMainBrokerName() {
         let scanEvent = HistoryEvent.mockScanEvent(with: Date())
         let queryData = BrokerProfileQueryData.mock(dataBrokerName: "testBroker", lastRunDate: Date(), scanHistoryEvents: [scanEvent], mirrorSites: [])
 
@@ -68,7 +68,7 @@ final class BrokerProfileQueryDataTests: XCTestCase {
         XCTAssertEqual(result, ["testBroker"])
     }
 
-    func testNamesOfBrokersScannedIncludingMirrorSites_whenMirrorSitesExistAndExtant_ThenReturnsAllNames() {
+    func test_namesOfBrokersScannedIncludingMirrorSites_whenMirrorSitesExistAndExtant_ThenReturnsAllNames() {
         let currentDate = Date()
         let scanEvent = HistoryEvent.mockScanEvent(with: currentDate)
         let mirrorSite1 = MirrorSite(name: "mirror1", url: "mirror1.url", addedAt: Date(timeInterval: -1000, since: currentDate), removedAt: nil)
@@ -81,7 +81,7 @@ final class BrokerProfileQueryDataTests: XCTestCase {
         XCTAssertEqual(result, ["testBroker", "mirror1", "mirror2", "mirror3"])
     }
 
-    func testNamesOfBrokersScannedIncludingMirrorSites_whenMirrorSitesExistButNotAllWereExtantAtScanTime_ThenReturnsOnlyScannedNames() {
+    func test_namesOfBrokersScannedIncludingMirrorSites_whenMirrorSitesExistButNotAllWereExtantAtScanTime_ThenReturnsOnlyScannedNames() {
         let currentDate = Date()
         let scanEvent = HistoryEvent.mockScanEvent(with: currentDate)
         let mirrorSite1 = MirrorSite(name: "mirror1", url: "mirror1.url", addedAt: Date(timeInterval: -1000, since: currentDate), removedAt: nil) // This one existed
@@ -96,26 +96,52 @@ final class BrokerProfileQueryDataTests: XCTestCase {
 
     // [BrokerProfileQueryData] tests
 
+    // elementsSortedByScanLastRunDateWhereScansRanBetween tests
+
+    func test_elementsSortedByScanLastRunDateWhereScansRanBetween_whenNoDatesBetweenDates_ThenReturnsEmptyArray() {
+        let queryData1 = BrokerProfileQueryData.mock(dataBrokerName: "1", lastRunDate: Date(timeIntervalSince1970: 0))
+        let queryData2 = BrokerProfileQueryData.mock(dataBrokerName: "2", lastRunDate: Date(timeIntervalSince1970: 1000))
+        let queryData3 = BrokerProfileQueryData.mock(dataBrokerName: "3", lastRunDate: Date(timeIntervalSince1970: 500))
+        let array = [queryData1, queryData2, queryData3]
+
+        let result = array.elementsSortedByScanLastRunDateWhereScansRanBetween(earlierDate: Date(timeIntervalSince1970: 2000), laterDate: Date(timeIntervalSince1970: 20000))
+        XCTAssertEqual(result.count, 0)
+    }
+
+    func test_elementsSortedByScanLastRunDateWhereScansRanBetween_whenSomeDatesBetweenDates_ThenReturnsSortedArray() {
+        let queryData1 = BrokerProfileQueryData.mock(dataBrokerName: "1", lastRunDate: Date(timeIntervalSince1970: 0))
+        let queryData2 = BrokerProfileQueryData.mock(dataBrokerName: "2", lastRunDate: Date(timeIntervalSince1970: 1000))
+        let queryData3 = BrokerProfileQueryData.mock(dataBrokerName: "3", lastRunDate: Date(timeIntervalSince1970: 500))
+        let array = [queryData1, queryData2, queryData3]
+
+        let result = array.elementsSortedByScanLastRunDateWhereScansRanBetween(earlierDate: Date(timeIntervalSince1970: 400), laterDate: Date(timeIntervalSince1970: 20000))
+        XCTAssertEqual(result.map { $0.dataBroker.name }, ["3", "2"])
+    }
+
+    // elementsSortedByScanPreferredRunDateWhereDateIsBetween tests
+
+    func test_elementsSortedByScanPreferredRunDateWhereDateIsBetween_whenNoDatesBetweenDates_ThenReturnsEmptyArray() {
+        let queryData1 = BrokerProfileQueryData.mock(dataBrokerName: "1", preferredRunDate: Date(timeIntervalSince1970: 0))
+        let queryData2 = BrokerProfileQueryData.mock(dataBrokerName: "2", preferredRunDate: Date(timeIntervalSince1970: 1000))
+        let queryData3 = BrokerProfileQueryData.mock(dataBrokerName: "3", preferredRunDate: Date(timeIntervalSince1970: 500))
+        let array = [queryData1, queryData2, queryData3]
+
+        let result = array.elementsSortedByScanPreferredRunDateWhereDateIsBetween(earlierDate: Date(timeIntervalSince1970: 2000), laterDate: Date(timeIntervalSince1970: 20000))
+        XCTAssertEqual(result.count, 0)
+    }
+
+    func test_elementsSortedByScanPreferredRunDateWhereDateIsBetween_whenSomeDatesBetweenDates_ThenReturnsSortedArray() {
+        let queryData1 = BrokerProfileQueryData.mock(dataBrokerName: "1", preferredRunDate: Date(timeIntervalSince1970: 0))
+        let queryData2 = BrokerProfileQueryData.mock(dataBrokerName: "2", preferredRunDate: Date(timeIntervalSince1970: 1000))
+        let queryData3 = BrokerProfileQueryData.mock(dataBrokerName: "3", preferredRunDate: Date(timeIntervalSince1970: 500))
+        let array = [queryData1, queryData2, queryData3]
+
+        let result = array.elementsSortedByScanPreferredRunDateWhereDateIsBetween(earlierDate: Date(timeIntervalSince1970: 400), laterDate: Date(timeIntervalSince1970: 20000))
+        XCTAssertEqual(result.map { $0.dataBroker.name }, ["3", "2"])
+    }
+
     /*
-     func elementsSortedByScanLastRunDateWhereScansRanBetween(earlierDate: Date, laterDate: Date) -> [BrokerProfileQueryData] {
-         guard earlierDate < laterDate else {
-             assertionFailure()
-             return []
-         }
 
-         let unsortedElementsBetweenDates = self.filter {
-             $0.scanJobData.lastRunDate != nil &&
-             $0.scanJobData.lastRunDate! >= earlierDate &&
-             $0.scanJobData.lastRunDate! <= laterDate
-         }
-
-         let sortedElements = unsortedElementsBetweenDates.sorted {
-             $0.scanJobData.lastRunDate! < $1.scanJobData.lastRunDate!
-             // At this point they are guaranteed to have a lastRunDate due to the previous filter
-         }
-
-         return sortedElements
-     }
 
      func elementsSortedByScanPreferredRunDateWhereDateIsBetween(earlierDate: Date, laterDate: Date) -> [BrokerProfileQueryData] {
          guard earlierDate < laterDate else {

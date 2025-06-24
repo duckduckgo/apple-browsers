162d567dce4b4c2928d1a5a6271795f8a1b8e646
Pete <petes44@icloud.com>
2025-06-24 12:15:46 +0100

    Add isUserEligibleForFreeTrial property to DBPUIHandshakeUserData model, add plumbing to set the value.

--- SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCoreTestsUtils/Mocks.swift
+++ SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCoreTestsUtils/Mocks.swift
@@ -1604,6 +1604,7 @@ public final class MockAuthenticationManager: DataBrokerProtectionAuthentication
     public init() { }
 
     public var isUserAuthenticatedValue = false
+    public var isUserEligibleForFreeTrialValue = false
     public var accessTokenValue: String? = "fake token"
     public var shouldAskForInviteCodeValue = false
     public var redeemCodeCalled = false
@@ -1612,6 +1613,8 @@ public final class MockAuthenticationManager: DataBrokerProtectionAuthentication
     public var shouldThrowEntitlementError = false
 
     public var isUserAuthenticated: Bool { isUserAuthenticatedValue }
+    
+    public var isUserEligibleForFreeTrial: Bool { isUserEligibleForFreeTrialValue }
 
     public func accessToken() async -> String? { accessTokenValue }
 

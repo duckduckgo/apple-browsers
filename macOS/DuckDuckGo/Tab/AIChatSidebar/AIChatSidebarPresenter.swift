916915fbf70fdfa6058178c89a444a3ea0163511
Michal Smaga <msmaga@duckduckgo.com>
2025-06-27 13:18:40 +0200

    Lock sidebar toggling for the transition duration

--- macOS/DuckDuckGo/Tab/AIChatSidebar/AIChatSidebarPresenter.swift
+++ macOS/DuckDuckGo/Tab/AIChatSidebar/AIChatSidebarPresenter.swift
@@ -57,6 +57,7 @@ final class AIChatSidebarPresenter: AIChatSidebarPresenting {
     private let windowControllersManager: WindowControllersManagerProtocol
     private let sidebarPresenceWillChangeSubject = PassthroughSubject<AIChatSidebarPresenceChange, Never>()
 
+    private var isAnimatingSidebarTransition: Bool = false
     private var cancellables = Set<AnyCancellable>()
 
     init(
@@ -89,7 +90,8 @@ final class AIChatSidebarPresenter: AIChatSidebarPresenting {
 
     func toggleSidebar() {
         guard featureFlagger.isFeatureOn(.aiChatSidebar) else { return }
-        guard let currentTabID = sidebarHost.currentTabID else { return }
+        guard !isAnimatingSidebarTransition,
+              let currentTabID = sidebarHost.currentTabID else { return }
 
         let willShowSidebar = !sidebarProvider.isShowingSidebar(for: currentTabID)
 
@@ -102,6 +104,7 @@ final class AIChatSidebarPresenter: AIChatSidebarPresenting {
     }
 
     private func updateSidebarConstraints(for tabID: TabIdentifier, isShowingSidebar: Bool, withAnimation: Bool) {
+        isAnimatingSidebarTransition = true
         sidebarPresenceWillChangeSubject.send(.init(tabID: tabID, isShown: isShowingSidebar))
 
         if isShowingSidebar {
@@ -123,7 +126,10 @@ final class AIChatSidebarPresenter: AIChatSidebarPresenting {
                 context.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
                 sidebarHost.sidebarContainerLeadingConstraint?.animator().constant = newConstraintValue
             } completionHandler: { [weak self, tabID = sidebarHost.currentTabID] in
-                guard let self, let tabID, !isShowingSidebar else { return }
+                guard let self else { return }
+                self.isAnimatingSidebarTransition = false
+
+                guard let tabID, !isShowingSidebar else { return }
                 self.sidebarProvider.handleSidebarDidClose(for: tabID)
             }
         } else {
@@ -132,6 +138,7 @@ final class AIChatSidebarPresenter: AIChatSidebarPresenting {
             if let tabID = sidebarHost.currentTabID, !isShowingSidebar {
                 sidebarProvider.handleSidebarDidClose(for: tabID)
             }
+            self.isAnimatingSidebarTransition = false
         }
     }
 

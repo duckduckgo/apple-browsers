a082ec7dcba63d9ac4ae1e90ba55a22eab7083a8
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-07-03 19:04:52 +0200

    Rolls back changes, renames a file

--- iOS/Core/UserDefaultsPropertyWrapper.swift
+++ iOS/Core/UserDefaultsPropertyWrapper.swift
@@ -136,7 +136,6 @@ public struct UserDefaultsWrapper<T> {
         case syncCurrentBookmarksPausedError = "sync.current-bookmarks-paused-error"
         case syncCurrentCredentialsPausedError = "sync.current-credentials-paused-error"
 
-        case networkProtectionEntitlementsExpired = "com.duckduckgo.network-protection.entitlements.expired"
         case networkProtectionDebugOptionAlwaysOnDisabled = "com.duckduckgo.network-protection.always-on.disabled"
         case networkProtectionWaitlistTermsAndConditionsAccepted = "com.duckduckgo.ios.vpn.terms-and-conditions-accepted"
 

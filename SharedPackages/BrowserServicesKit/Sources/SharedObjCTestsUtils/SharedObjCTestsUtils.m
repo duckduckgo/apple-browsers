94d1ab477f937ff7ba40c1034da0024845b65910
Alexey Martemyanov <mallexxx@gmail.com>
2025-07-07 21:13:50 +0600

    rollback debug/info logging

--- SharedPackages/BrowserServicesKit/Sources/SharedObjCTestsUtils/SharedObjCTestsUtils.m
+++ SharedPackages/BrowserServicesKit/Sources/SharedObjCTestsUtils/SharedObjCTestsUtils.m
@@ -15,7 +15,7 @@
         self.dateFormatter = [[NSDateFormatter alloc] init];
         [self.dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS"];
 
-        os_log_info(self.logger, "🟢🟢🟢 Starting");
+        os_log(self.logger, "🟢🟢🟢 Starting");
     }
     return self;
 }
@@ -32,7 +32,7 @@
 
 - (void)testSuiteWillStart:(XCTestSuite *)testSuite {
     if (![testSuite.name hasSuffix:@".xctest"] && ![testSuite.name isEqualToString:@"All tests"]) {
-        os_log_info(self.logger, "Test Suite '%{public}@' started at %{public}@.", testSuite.name, [self timestampString]);
+        os_log(self.logger, "Test Suite '%{public}@' started at %{public}@.", testSuite.name, [self timestampString]);
     }
 }
 
@@ -42,18 +42,18 @@
         double totalTime = testSuite.testRun.totalDuration;
         double testTime = testSuite.testRun.testDuration;
         
-        os_log_info(self.logger, "🔵 Test Suite '%{public}@' %{public}@ at %{public}@.", testSuite.name, result, [self timestampString]);
-        os_log_info(self.logger, "\t Executed %lu tests, with %lu failures (%lu unexpected) in %.3f (%.3f) seconds",
-                    (unsigned long)testSuite.testRun.testCaseCount,
-                    (unsigned long)testSuite.testRun.failureCount,
-                    (unsigned long)testSuite.testRun.unexpectedExceptionCount,
-                    testTime,
-                    totalTime);
+        os_log(self.logger, "🔵 Test Suite '%{public}@' %{public}@ at %{public}@.", testSuite.name, result, [self timestampString]);
+        os_log(self.logger, "\t Executed %lu tests, with %lu failures (%lu unexpected) in %.3f (%.3f) seconds",
+               (unsigned long)testSuite.testRun.testCaseCount,
+               (unsigned long)testSuite.testRun.failureCount,
+               (unsigned long)testSuite.testRun.unexpectedExceptionCount,
+               testTime,
+               totalTime);
     }
 }
 
 - (void)testCaseWillStart:(XCTestCase *)testCase {
-    os_log_info(self.logger, "➡️ Test Case '%{public}@' started.", testCase.name);
+    os_log(self.logger, "➡️ Test Case '%{public}@' started.", testCase.name);
 }
 
 - (void)testCaseDidFinish:(XCTestCase *)testCase {
@@ -61,7 +61,7 @@
     NSString *marker = (testCase.testRun.hasSucceeded) ? @"🟢" : @"🔴";
     double duration = testCase.testRun.totalDuration;
     
-    os_log_info(self.logger, "%{public}@ Test Case '%{public}@' %{public}@ (%.3f seconds).", marker, testCase.name, result, duration);
+    os_log(self.logger, "%{public}@ Test Case '%{public}@' %{public}@ (%.3f seconds).", marker, testCase.name, result, duration);
 }
 
 - (void)testCase:(XCTestCase *)testCase didFailWithDescription:(NSString *)description inFile:(NSString *)filePath atLine:(NSUInteger)lineNumber {

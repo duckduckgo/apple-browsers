af4d0b1dc6a59274b41c55e3dfd7a475f8d6d36c
amddg44 <amallon@duckduckgo.com>
2025-07-07 11:31:31 +0200

    Update import shortcuts images

--- macOS/DuckDuckGo/DataImport/View/DataImportShortcutsView.swift
+++ macOS/DuckDuckGo/DataImport/View/DataImportShortcutsView.swift
@@ -18,6 +18,7 @@
 
 import SwiftUI
 import BrowserServicesKit
+import DesignResourcesKitIcons
 
 struct DataImportShortcutsView: ModalView {
 
@@ -38,7 +39,8 @@ struct DataImportShortcutsView: ModalView {
         VStack(alignment: .leading, spacing: 8) {
             VStack(spacing: 0) {
                 if let dataTypes = model.dataTypes, dataTypes.contains(.bookmarks), OnboardingActionsManager.isOnboardingFinished {
-                    importShortcutsRow(image: Image(.bookmarksFavoritesColor24),
+
+                    importShortcutsRow(image: NSApp.delegateTyped.visualStyle.isNewStyle ? Image(nsImage: DesignSystemImages.Color.Size24.bookmarkFavorite) : Image(.bookmarksFavoritesColor24),
                                        title: UserText.importShortcutsBookmarksTitle,
                                        subtitle: UserText.importShortcutsBookmarksSubtitle,
                                        isOn: $model.showBookmarksBarStatus)
@@ -49,7 +51,7 @@ struct DataImportShortcutsView: ModalView {
                         .padding(.leading)
                 }
 
-                importShortcutsRow(image: Image(.keyColor24),
+                importShortcutsRow(image: NSApp.delegateTyped.visualStyle.isNewStyle ? Image(nsImage: DesignSystemImages.Color.Size24.key) : Image(.keyColor24),
                                    title: UserText.importShortcutsPasswordsTitle,
                                    subtitle: UserText.importShortcutsPasswordsSubtitle,
                                    isOn: $model.showPasswordsPinnedStatus)

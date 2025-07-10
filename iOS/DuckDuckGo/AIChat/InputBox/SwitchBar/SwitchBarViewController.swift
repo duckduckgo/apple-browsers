f176a7d410a6e7b76c0a216449bb466d63254f81
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-07-10 18:25:19 +0200

    Fix back button size

--- iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/SwitchBarViewController.swift
+++ iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/SwitchBarViewController.swift
@@ -30,6 +30,7 @@ class SwitchBarViewController: UIViewController {
         static let textEntryViewTopPadding: CGFloat = 16
         static let textEntryViewSidePadding: CGFloat = 16
         static let backButtonHorizontalPadding: CGFloat = 16
+        static let backButtonSize: CGFloat = 44
     }
 
     private var segmentedPickerHostingController: UIHostingController<PickerWrapper>?
@@ -183,7 +184,9 @@ class SwitchBarViewController: UIViewController {
             textEntryViewController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).withPriority(.init(999)),
 
             backButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.backButtonHorizontalPadding),
-            backButton.centerYAnchor.constraint(equalTo: segmentedPickerView.centerYAnchor)
+            backButton.centerYAnchor.constraint(equalTo: segmentedPickerView.centerYAnchor),
+            backButton.heightAnchor.constraint(equalToConstant: Constants.backButtonSize),
+            backButton.widthAnchor.constraint(equalToConstant: Constants.backButtonSize)
         ])
     }
 

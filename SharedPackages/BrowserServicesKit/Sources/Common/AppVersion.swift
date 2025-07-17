bcf0a70aeed3ddc1584600910d0638d3f0acd9a8
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-07-17 19:38:33 +0200

    Set prerelease label

--- SharedPackages/BrowserServicesKit/Sources/Common/AppVersion.swift
+++ SharedPackages/BrowserServicesKit/Sources/Common/AppVersion.swift
@@ -52,8 +52,8 @@ public struct AppVersion {
         return bundle.object(forInfoDictionaryKey: Bundle.Key.alphaBuildSuffix) as? String ?? ""
     }
 
-    public var commitSHA: String {
-        return bundle.object(forInfoDictionaryKey: Bundle.Key.commitSHA) as? String ?? ""
+    public var commitSHA: String? {
+        return bundle.object(forInfoDictionaryKey: Bundle.Key.commitSHA) as? String
     }
 
     public var versionAndBuildNumber: String {

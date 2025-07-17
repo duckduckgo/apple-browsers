68f19d46da105c7a3a4b7b1fec5b19ba09cb92e0
Sam Symons <sam@samsymons.com>
2025-07-16 18:24:40 -0700

    Force a test failure

--- iOS/FingerprintingUITests/FingerprintUITest.swift
+++ iOS/FingerprintingUITests/FingerprintUITest.swift
@@ -94,6 +94,8 @@ class FingerprintUITest: XCTestCase {
     }
 
     func test() throws {
+        XCTFail("Forced fail to test out a PR")
+
         let app = XCUIApplication()
 
         _ = app.buttons["Browsing Menu"].waitForExistence(timeout: 25)

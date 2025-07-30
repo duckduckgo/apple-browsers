92861d4e1734c9082e69b435253af3a8ef5bdb59
Rachel McRoberts <rmcroberts@duckduckgo.com>
2025-07-30 14:39:11 +0100

    More precise fix for tab context menu: prevent glass view from intercepting events

--- macOS/DuckDuckGo/MainWindow/MainWindowController.swift
+++ macOS/DuckDuckGo/MainWindow/MainWindowController.swift
@@ -221,18 +221,11 @@ final class MainWindowController: NSWindowController {
         let tabBarViewController = mainViewController.tabBarViewController
         tabBarViewController.view.removeFromSuperview()
 
-        let toolbarView = newParentView.subviews.first(where: { $0.className == "NSToolbarView" })
         if toTitlebarView {
             // Prevent 1px line from appearing below Tab Bar when hiding and subsequently showing it in fullscreen mode
             // https://app.asana.com/1/137249556945/project/1201048563534612/task/1209999815083499?focus=true
             newParentView.layer?.masksToBounds = true
             newParentView.addSubview(tabBarViewController.view)
-
-            // In macOS 26, the toolbar contains a glass view that intercepts context clicks on the tab bar
-            // Since that view is currently unused, we can hide it for now to allow clicks through
-            if #available(macOS 26.0, *), let glassView = toolbarView?.subviews.first(where: { $0.className == "NSGlassContainerView" }) {
-                glassView.isHidden = true
-            }
         } else {
             newParentView.addSubview(tabBarViewController.view, positioned: .below, relativeTo: mainViewController.fireViewController.view)
         }
@@ -240,7 +233,7 @@ final class MainWindowController: NSWindowController {
         // enable Tab Bar appear as an AX child of the Window
         // this won‘t work by-default since we‘re adding it to the NSToolbar view
         // providing its own a11y implementation: see TabBarViewController
-        if let toolbarView {
+        if let toolbarView = newParentView.subviews.first(where: { $0.className == "NSToolbarView" }) {
             toolbarView.setAccessibilityEnabled(false)
             toolbarView.setAccessibilityElement(false)
             tabBarViewController.view.setAccessibilityParent(window)
@@ -543,5 +536,17 @@ fileprivate extension NavigationBarViewController {
 extension NSWindow {
     var isFullScreen: Bool {
         return self.styleMask.contains(.fullScreen) && self.isMiniaturized == false
+    }
+}
+
+@available(macOS 26.0, *)
+extension NSGlassEffectContainerView {
+    open override func hitTest(_ point: NSPoint) -> NSView? {
+        // Prevent glass view in the toolbar from intercepting events.
+        // This works around an issue in macOS 26 where the glass view prevents the context menu from appearing in the tab bar.
+        if superview?.className == "NSToolbarView" {
+            return nil
+        }
+        return super.hitTest(point)
     }
 }

bacc453e08219f5a1c181e3955e6634cc9921994
Sam Symons <sam@samsymons.com>
2025-08-09 11:39:13 -0700

    Include the global unicast route if needed.

--- SharedPackages/VPN/Sources/VPN/Routing/VPNRoutingTableResolver.swift
+++ SharedPackages/VPN/Sources/VPN/Routing/VPNRoutingTableResolver.swift
@@ -31,12 +31,15 @@ struct VPNRoutingTableResolver {
 
     private let dnsServers: [DNSServer]
     private let excludeLocalNetworks: Bool
+    private let includeIPv6Routes: Bool
 
     init(dnsServers: [DNSServer],
-         excludeLocalNetworks: Bool) {
+         excludeLocalNetworks: Bool,
+         includeIPv6Routes: Bool = true) {
 
         self.dnsServers = dnsServers
         self.excludeLocalNetworks = excludeLocalNetworks
+        self.includeIPv6Routes = includeIPv6Routes
     }
 
     var excludedRoutes: [IPAddressRange] {
@@ -54,6 +57,10 @@ struct VPNRoutingTableResolver {
 
         if !excludeLocalNetworks {
             routes += VPNRoutingRange.localNetworkRange
+        }
+
+        if includeIPv6Routes {
+            routes += [IPAddressRange(from: "2000::/3")!]
         }
 
         return routes

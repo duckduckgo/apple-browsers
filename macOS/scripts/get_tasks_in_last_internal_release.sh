df7694bf39af3310aa6f5a01669e756fe2fcdf6c
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-08-11 15:25:25 +0200

    Fix typo

--- macOS/scripts/get_tasks_in_last_internal_release.sh
+++ macOS/scripts/get_tasks_in_last_internal_release.sh
@@ -30,7 +30,7 @@ get_task_id() {
 	local url="$1"
 	local task_id
 	task_id="$(perl -pe 's|.*https://app.asana.com/0/[0-9]+/([0-9]+)(?:/f)?.*|\1|; \
-                s|.*https://app.asana.com/1/[0-9]+(?:/[0-9a-z/]*)?/task/([0-9]+)(:?/[0-9a-z/]*)?(?:\?focus=true)?.*|\1|; \
+                s|.*https://app.asana.com/1/[0-9]+(?:/[0-9a-z/]*)?/task/([0-9]+)(?:/[0-9a-z/]*)?(?:\?focus=true)?.*|\1|; \
                 s|.*https://app.asana.com/1/[0-9]+/inbox/[0-9]+/item/([0-9]+)/story/([0-9]+)(?:\?focus=true)?.*|\1|' <<< "$url")"
 	if [[ -n "$task_id" ]]; then
 		local http_code

004e7cf935b50882bb7e9d48dca396cf92f566b1
Alessandro Boron <aboron@duckduckgo.com>
2025-08-12 13:47:58 +1000

    Ship review hack

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -349,7 +349,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .scheduledSetDefaultBrowserPrompts:
             return .remoteReleasable(.subfeature(SetAsDefaultAndAddToDockSubfeature.scheduledDefaultBrowserPrompts))
         case .scheduledSetDefaultBrowserPromptsForInactiveUsers:
-            return .remoteReleasable(.subfeature(SetAsDefaultAndAddToDockSubfeature.scheduledDefaultBrowserPromptsInactiveUser))
+            return .internalOnly()
         case .subscriptionRebranding:
             return .remoteReleasable(.subfeature(PrivacyProSubfeature.subscriptionRebranding))
         case .supportsAlternateStripePaymentFlow:

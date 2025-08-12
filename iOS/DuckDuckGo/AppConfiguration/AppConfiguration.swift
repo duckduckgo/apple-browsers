34dc8c19eb1784e03130bcbc3892466fe819a5ea
Chris Brind <brindy@duckduckgo.com>
2025-08-12 15:41:00 +0100

    swiftlint

--- iOS/DuckDuckGo/AppConfiguration/AppConfiguration.swift
+++ iOS/DuckDuckGo/AppConfiguration/AppConfiguration.swift
@@ -62,7 +62,7 @@ struct AppConfiguration {
     /// Perform AI Chat settings migration, and needs to happen before AIChatSettings is created
     ///  and the widgets needs to be reloaded after.
     /// Moves settings from `UserDefaults.standard` to the shared container.
-    private func migrateAIChatSettings() throws {        
+    private func migrateAIChatSettings() throws {
         let sharedUserDefaults = UserDefaults(suiteName: Global.appConfigurationGroupName) ?? UserDefaults()
         AIChatSettingsMigration.migrate(from: UserDefaults.standard, to: sharedUserDefaults)
     }

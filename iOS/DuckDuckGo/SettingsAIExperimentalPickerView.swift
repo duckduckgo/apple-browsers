8df8ab8ea3ae1c6c3d481b29936292d045ceb316
Fernando Bunn <bunn@idevzilla.com>
2025-08-15 12:58:10 -0300

    Fix Color

--- iOS/DuckDuckGo/SettingsAIExperimentalPickerView.swift
+++ iOS/DuckDuckGo/SettingsAIExperimentalPickerView.swift
@@ -100,7 +100,7 @@ struct SettingsAIExperimentalPickerView: View {
         Image(uiImage: DesignSystemImages.Glyphs.Size24.shapeCircle)
             .resizable()
             .renderingMode(.template)
-            .foregroundStyle(Color(designSystemColor: .textPrimary))
+            .foregroundStyle(Color(designSystemColor: .textSecondary))
 
     }
 }

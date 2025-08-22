ee3bde6696980e0cc6cf5675081ff0ad8e0926f4
Fernando Bunn <bunn@idevzilla.com>
2025-08-22 13:48:39 -0300

    Change animation

--- SharedPackages/UIComponents/Sources/UIComponents/iOS/ImageSegmentedPickerView.swift
+++ SharedPackages/UIComponents/Sources/UIComponents/iOS/ImageSegmentedPickerView.swift
@@ -158,7 +158,6 @@ public struct ImageSegmentedPickerView: View {
                     .frame(width: geo.size.width / CGFloat(viewModel.items.count), height: Constants.innerHeight)
                     .offset(x: currentOffset)
                     .shadow(color: Color(designSystemColor: .shadowPrimary), radius: 0.5, x: 0, y: 0.5)
-                    .animation(viewModel.scrollProgress == nil ? .easeInOut(duration: 0.2) : nil, value: currentOffset)
 
                 HStack(spacing: 0) {
                     ForEach(Array(viewModel.items.enumerated()), id: \.element.id) { index, item in

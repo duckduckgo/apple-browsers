ef9e248b7fb36bfcd2b52afee42fff0585b065b6
Bartek Waresiak <bartek@duckduckgo.com>
2025-08-29 15:16:23 +0200

    Remove account before updating it to apply access changes

--- SharedPackages/BrowserServicesKit/Sources/DDGSync/DDGSync.swift
+++ SharedPackages/BrowserServicesKit/Sources/DDGSync/DDGSync.swift
@@ -323,6 +323,9 @@ public class DDGSync: DDGSyncing {
         let syncQueue = SyncQueue(dataProviders: providers, dependencies: dependencies)
         try syncQueue.prepareDataModelsForSync(needsRemoteDataFetch: account.state == .addingNewDevice)
 
+        //Remove account from keychain to re-apply attributes changes
+        try? dependencies.secureStore.removeAccount()
+
         if account.state != .active {
             let activatedAccount = account.updatingState(.active)
             try dependencies.secureStore.persistAccount(activatedAccount)

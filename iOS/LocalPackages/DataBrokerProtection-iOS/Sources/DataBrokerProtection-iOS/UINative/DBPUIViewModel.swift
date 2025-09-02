27643c4d03359348315354c41e1e12227ff44a80
Rachel McRoberts <rmcroberts@duckduckgo.com>
2025-09-02 19:05:27 +0100

    Fix optional in iOS

--- iOS/LocalPackages/DataBrokerProtection-iOS/Sources/DataBrokerProtection-iOS/UINative/DBPUIViewModel.swift
+++ iOS/LocalPackages/DataBrokerProtection-iOS/Sources/DataBrokerProtection-iOS/UINative/DBPUIViewModel.swift
@@ -84,7 +84,7 @@ public final class DBPUIViewModel {
             configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
 
             if let dbpUIContentController = configuration.userContentController as? DBPUIUserContentController {
-                communicationLayer = dbpUIContentController.dbpUIUserScripts.dbpUICommunicationLayer
+                communicationLayer = dbpUIContentController.dbpUIUserScripts?.dbpUICommunicationLayer
             }
 
             return configuration

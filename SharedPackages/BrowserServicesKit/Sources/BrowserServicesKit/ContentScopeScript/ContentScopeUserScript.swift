69c7f9b47a8275129834b04081be04f9e38529c8
Jonathan Kingston <jkingston@duckduckgo.com>
2025-09-10 09:37:15 +0100

    Add debug state to C-S-S in debug builds

--- SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/ContentScopeScript/ContentScopeUserScript.swift
+++ SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/ContentScopeScript/ContentScopeUserScript.swift
@@ -45,7 +45,7 @@ public struct ContentScopeExperimentData: Encodable, Equatable {
 
 public final class ContentScopeProperties: Encodable {
     public let globalPrivacyControlValue: Bool
-    public let debug: Bool = false
+    public var debug: Bool = false
     public let sessionKey: String
     public let messageSecret: String
     public let languageCode: String
@@ -58,6 +58,9 @@ public final class ContentScopeProperties: Encodable {
                 messageSecret: String,
                 featureToggles: ContentScopeFeatureToggles,
                 currentCohorts: [ContentScopeExperimentData] = []) {
+        #if DEBUG
+        self.debug = true
+        #endif
         self.globalPrivacyControlValue = gpcEnabled
         self.sessionKey = sessionKey
         self.messageSecret = messageSecret

db780cea1ac721e6dd446bef59a5a69a61284761
Rachel McRoberts <rmcroberts@duckduckgo.com>
2025-09-10 11:22:41 +0100

    Add pixels

--- macOS/DuckDuckGo/Statistics/GeneralPixel.swift
+++ macOS/DuckDuckGo/Statistics/GeneralPixel.swift
@@ -522,6 +522,11 @@ enum GeneralPixel: PixelKitEvent {
     // Enhanced statistics
     case usageSegments
 
+    // Encryption key access
+    case encryptionKeystoreReadKeySucceeded
+    case encryptionKeystoreReadKeyFailed
+    case encryptionKeystoreWriteKeyFailed
+
     var name: String {
         switch self {
         case .crash(let appIdentifier):
@@ -1230,6 +1235,11 @@ enum GeneralPixel: PixelKitEvent {
 
             // Enhanced statistics
         case .usageSegments: return "retention_segments"
+
+            // Encryption key
+        case .encryptionKeystoreReadKeySucceeded: return "m_mac_encryption_keystore_read_key_succeeded"
+        case .encryptionKeystoreReadKeyFailed: return "m_mac_encryption_keystore_read_key_failed"
+        case .encryptionKeystoreWriteKeyFailed: return "m_mac_encryption_keystore_write_key_failed"
 
         }
     }

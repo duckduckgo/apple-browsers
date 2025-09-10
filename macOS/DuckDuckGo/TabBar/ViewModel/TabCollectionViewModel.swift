e031de85c4a0caeeb5f2ee6867934288bd49a946
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2025-09-10 12:11:25 -0300

    TabCollectionViewModel: select displayable tab now also accounts for Pinned Tabs

--- macOS/DuckDuckGo/TabBar/ViewModel/TabCollectionViewModel.swift
+++ macOS/DuckDuckGo/TabBar/ViewModel/TabCollectionViewModel.swift
@@ -292,17 +292,16 @@ final class TabCollectionViewModel: NSObject {
             return true
         }
 
-        guard let index = tabCollection.tabs.firstIndex(where: { $0.content.matchesDisplayableTab(content) })
+        guard let index = indexInAllTabs(where: { $0.content.matchesDisplayableTab(content) }),
+              let tab = tab(at: index)
         else {
             return false
         }
 
-        if selectUnpinnedTab(at: index) {
-            tabCollection.tabs[index].setContent(content)
-            delegate?.tabCollectionViewModel(self, didSelectAt: index)
-            return true
-        }
-        return false
+        select(at: index)
+        tab.setContent(content)
+
+        return true
     }
 
     func selectNext() {

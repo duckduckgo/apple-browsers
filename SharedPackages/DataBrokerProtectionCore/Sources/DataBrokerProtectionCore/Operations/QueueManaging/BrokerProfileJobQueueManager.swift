2b8f7ec457829d95cab1f540f00413c3bcc590f1
Anh Do <ado@duckduckgo.com>
2025-09-11 12:50:26 -0400

    Fix linting

--- SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCore/Operations/QueueManaging/BrokerProfileJobQueueManager.swift
+++ SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCore/Operations/QueueManaging/BrokerProfileJobQueueManager.swift
@@ -195,7 +195,7 @@ public final class BrokerProfileJobQueueManager: BrokerProfileJobQueueManaging {
 
     public func addEmailConfirmationJobs(showWebView: Bool, jobDependencies: BrokerProfileJobDependencyProviding) {
         guard jobDependencies.featureFlagger.isEmailConfirmationDecouplingFeatureOn else { return }
-        
+
         do {
             let emailConfirmationDependencies = EmailConfirmationJobDependencies(from: jobDependencies)
             let emailJobs = try emailConfirmationJobProvider.createEmailConfirmationJobs(

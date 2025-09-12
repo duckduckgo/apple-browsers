5e08ebc71536fe35b50bb58c9a77caa7378bf3a1
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2025-09-12 14:00:35 -0300

    Delete testing code

--- macOS/DuckDuckGo/Application/AppDelegate.swift
+++ macOS/DuckDuckGo/Application/AppDelegate.swift
@@ -864,10 +864,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         }
 
         Task {
-            try? await LatestReleaseChecker().getLatestReleaseAvailable(for: .macOSAppStore)
-        }
-
-        Task {
             await subscriptionManagerV1?.loadInitialData()
             await subscriptionManagerV2?.loadInitialData()
 

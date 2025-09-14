0c3f53d70d5a96455e743bad945baa94908d1bd7
Sam Symons <sam@samsymons.com>
2025-09-14 14:05:13 -0700

    Revert error domain change

--- SharedPackages/BrowserServicesKit/Sources/PixelKit/WidePixel/WidePixelError.swift
+++ SharedPackages/BrowserServicesKit/Sources/PixelKit/WidePixel/WidePixelError.swift
@@ -48,7 +48,7 @@ public enum WidePixelError: DDGError, LocalizedError {
         return description
     }
 
-    public static var errorDomain: String { "WidePixelError" }
+    public static var errorDomain: String { "com.duckduckgo.widePixel" }
 
     public var errorCode: Int {
         switch self {

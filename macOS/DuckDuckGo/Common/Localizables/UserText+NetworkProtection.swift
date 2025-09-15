ddee56172d717ab13c5d999e1374beb2e0a71980
Sabrina Tardio <stardio@duckduckgo.com>
2025-09-15 17:07:16 +0200

    remove rebranding flag on macOS

--- macOS/DuckDuckGo/Common/Localizables/UserText+NetworkProtection.swift
+++ macOS/DuckDuckGo/Common/Localizables/UserText+NetworkProtection.swift
@@ -80,12 +80,7 @@ extension UserText {
 
     // MARK: - Feedback Form
 
-    static func feedbackFormTitle(isSubscriptionRebrandingEnabled: Bool) -> String {
-        if isSubscriptionRebrandingEnabled {
-            return NSLocalizedString("feedback-form.title", value: "Help Improve the DuckDuckGo Subscription", comment: "Title for each screen of the feedback form")
-        }
-        return NSLocalizedString("feedback-form.title.deprecated", value: "Help Improve Privacy Pro", comment: "Title for each screen of the feedback form")
-    }
+    static let feedbackFormTitle = NSLocalizedString("feedback-form.title", value: "Help Improve the DuckDuckGo Subscription", comment: "Title for each screen of the feedback form")
 
     static let generalFeedbackFormCategorySelect = NSLocalizedString("general.feedback-form.category.select-feature", value: "Select a category", comment: "Title for the feature selection state of the general feedback form")
 

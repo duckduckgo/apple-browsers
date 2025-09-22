fb660826a7029479e4ff761c550e25d8b407d6ab
James Frost <jfrost@duckduckgo.com>
2025-09-22 18:17:37 +0100

    Add hang reporting feature flag (browser config subfeature, internal only)

--- SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/PrivacyConfig/Features/PrivacyFeature.swift
+++ SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/PrivacyConfig/Features/PrivacyFeature.swift
@@ -117,6 +117,9 @@ public enum MacOSBrowserConfigSubfeature: String, PrivacySubfeature {
 
     /// https://app.asana.com/1/137249556945/project/1206580121312550/task/1209808389662317?focus=true
     case willSoonDropBigSurSupport
+
+    /// Hang reporting feature flag
+    case hangReporting
 }
 
 public enum iOSBrowserConfigSubfeature: String, PrivacySubfeature {

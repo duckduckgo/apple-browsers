7635317ed35c3cb4231a6c21fe76d84cbd2383bd
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2025-09-25 23:57:46 -0300

    Relocates new color palettes

--- SharedPackages/DesignResourcesKit/Sources/DesignResourcesKit/Colors/ColorPalletes/ColorPalette.swift
+++ SharedPackages/DesignResourcesKit/Sources/DesignResourcesKit/Colors/ColorPalletes/ColorPalette.swift
@@ -29,6 +29,9 @@ public enum ColorPalette {
     case coolGray
     case desert
     case figma
+    case green
+    case orange
+    case rose
 #else
     case `default`
 #endif
@@ -44,6 +47,12 @@ public enum ColorPalette {
             return DesertColorPalette.self
         case .figma:
             return SharedFigmaColorPalette.self
+        case .green:
+            return GreenColorPalette.self
+        case .orange:
+            return OrangeColorPalette.self
+        case .rose:
+            return RoseColorPalette.self
         }
     }
 #endif

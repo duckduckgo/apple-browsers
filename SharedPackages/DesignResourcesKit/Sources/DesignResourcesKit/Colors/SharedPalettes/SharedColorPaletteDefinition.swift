6009b9fca5e52584064ac6f8b8f3e8bdd12729ca
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2025-09-25 21:17:08 -0300

    Addressing Feedback. Part II

---
+++ SharedPackages/DesignResourcesKit/Sources/DesignResourcesKit/Colors/SharedPalettes/SharedColorPaletteDefinition.swift
@@ -0,0 +1,29 @@
+//
+//  SharedColorPaletteDefinition.swift
+//
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+#if os(macOS)
+
+protocol SharedColorPaletteDefinition {
+
+    /// Gets dynamic color proxy for a specific semantic color based on the JSON import
+    static func dynamicColor(for designSystemColor: SharedDesignSystemColor) -> DynamicColor
+}
+
+#endif

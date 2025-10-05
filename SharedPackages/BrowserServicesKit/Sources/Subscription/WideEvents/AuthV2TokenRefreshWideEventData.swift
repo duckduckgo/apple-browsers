de1b2517aed8c3b89dc5d679b8b3ce620141b3d4
Sam Symons <sam@samsymons.com>
2025-10-05 15:16:32 -0700

    Revert pixel name change

--- SharedPackages/BrowserServicesKit/Sources/Subscription/WideEvents/AuthV2TokenRefreshWideEventData.swift
+++ SharedPackages/BrowserServicesKit/Sources/Subscription/WideEvents/AuthV2TokenRefreshWideEventData.swift
@@ -29,7 +29,7 @@ public class AuthV2TokenRefreshWideEventData: WideEventData {
     #if DEBUG
     public static let pixelName = "auth_v2_token_refresh_debug"
     #else
-    public static let pixelName = "auth_v2_token_refresh_debug" // Replace with non-debug version before merging
+    public static let pixelName = "auth_v2_token_refresh"
     #endif
 
     public enum ApplicationState: String, Codable {

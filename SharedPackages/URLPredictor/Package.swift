7a372da8476059246a953f14c3e8d318bb02c138
Sam Symons <sam@samsymons.com>
2025-10-06 09:06:17 -0700

    Fix package version

--- SharedPackages/URLPredictor/Package.swift
+++ SharedPackages/URLPredictor/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .target(name: "URLPredictor", dependencies: ["URLPredictorRust"]),
         .binaryTarget(
             name: "URLPredictorRust",
-            url: "https://github.com/duckduckgo/url_predictor/releases/download/0.3.0/URLPredictorRust.xcframework.zip",
+            url: "https://github.com/duckduckgo/url_predictor/releases/download/0.3.1/URLPredictorRust.xcframework.zip",
             checksum: "0ea6ba050b3b02cfe3dfbeb268e6740675168fe6773cd3a631c4e37945cb7da2"
         ),
         .testTarget(name: "URLPredictorTests", dependencies: ["URLPredictor"])

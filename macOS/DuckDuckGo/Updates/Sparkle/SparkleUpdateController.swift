c76ac13682b0aa2fa22810f0b94bfd211343c69f
Anh Do <ado@duckduckgo.com>
2025-10-06 16:45:25 -0400

    Tighten latestUpdate assignment

--- macOS/DuckDuckGo/Updates/Sparkle/SparkleUpdateController.swift
+++ macOS/DuckDuckGo/Updates/Sparkle/SparkleUpdateController.swift
@@ -61,7 +61,23 @@ final class SparkleUpdateController: NSObject, SparkleUpdateControllerProtocol {
             self.needsLatestReleaseNote = needsLatestReleaseNote
         }
     }
-    private var cachedUpdateResult: UpdateCheckResult?
+
+    private var cachedUpdateResult: UpdateCheckResult? {
+        didSet {
+            if let cachedUpdateResult {
+                refreshUpdateFromCache(cachedUpdateResult)
+            } else {
+                latestUpdate = nil
+                hasPendingUpdate = false
+                needsNotificationDot = false
+            }
+        }
+    }
+
+    private func refreshUpdateFromCache(_ cachedUpdateResult: UpdateCheckResult) {
+        latestUpdate = Update(appcastItem: cachedUpdateResult.item, isInstalled: cachedUpdateResult.isInstalled, needsLatestReleaseNote: cachedUpdateResult.needsLatestReleaseNote)
+        hasPendingUpdate = latestUpdate?.isInstalled == false && updateProgress.isDone && userDriver?.isResumable == true
+    }
 
     // Struct used to persist pending update info across app restarts
     struct PendingUpdateInfo: Codable {
@@ -86,8 +102,7 @@ final class SparkleUpdateController: NSObject, SparkleUpdateControllerProtocol {
     @Published private(set) var updateProgress = UpdateCycleProgress.default {
         didSet {
             if let cachedUpdateResult {
-                latestUpdate = Update(appcastItem: cachedUpdateResult.item, isInstalled: cachedUpdateResult.isInstalled, needsLatestReleaseNote: cachedUpdateResult.needsLatestReleaseNote)
-                hasPendingUpdate = latestUpdate?.isInstalled == false && updateProgress.isDone && userDriver?.isResumable == true
+                refreshUpdateFromCache(cachedUpdateResult)
                 needsNotificationDot = hasPendingUpdate
             }
             showUpdateNotificationIfNeeded()
@@ -392,7 +407,6 @@ final class SparkleUpdateController: NSObject, SparkleUpdateControllerProtocol {
     private func configureUpdater() throws -> SPUUpdater? {
         // Workaround to reset the updater state
         cachedUpdateResult = nil
-        latestUpdate = nil
 
         if !useLegacyAutoRestartLogic, let userDriver {
             userDriver.areAutomaticUpdatesEnabled = areAutomaticUpdatesEnabled

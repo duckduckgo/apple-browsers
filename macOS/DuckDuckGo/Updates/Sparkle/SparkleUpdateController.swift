d8baa84f8aed8d120ab5591bb7020dcd037a23bc
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-10-11 20:55:27 +0200

    Moves some calls to main thread

--- macOS/DuckDuckGo/Updates/Sparkle/SparkleUpdateController.swift
+++ macOS/DuckDuckGo/Updates/Sparkle/SparkleUpdateController.swift
@@ -266,7 +266,9 @@ final class SparkleUpdateController: NSObject, SparkleUpdateControllerProtocol {
     func checkForUpdateRespectingRollout() {
 #if DEBUG
         guard NSApp.delegateTyped.featureFlagger.isFeatureOn(.autoUpdateInDEBUG) else {
-            updater?.checkForUpdateInformation()
+            Task { @MainActor in
+                updater?.checkForUpdateInformation()
+            }
             return
         }
 #endif
@@ -279,9 +281,11 @@ final class SparkleUpdateController: NSObject, SparkleUpdateControllerProtocol {
     private func performUpdateCheck() async {
         // Check if we can start a new check (Sparkle availability + rate limiting)
         let updaterAvailability = SparkleUpdaterAvailabilityChecker(updater: updater)
-        guard await updateCheckState.canStartNewCheck(updater: updaterAvailability) else {
+        guard false else { // await updateCheckState.canStartNewCheck(updater: updaterAvailability) else {
             Logger.updates.debug("Update check skipped - not allowed by Sparkle or rate limited")
-            updater?.checkForUpdateInformation()
+            Task { @MainActor in
+                updater?.checkForUpdateInformation()
+            }
             return
         }
 
@@ -423,7 +427,9 @@ final class SparkleUpdateController: NSObject, SparkleUpdateControllerProtocol {
         }
 
         let updater = SPUUpdater(hostBundle: Bundle.main, applicationBundle: Bundle.main, userDriver: userDriver, delegate: self)
-        updater.checkForUpdateInformation()
+        Task { @MainActor in
+            updater.checkForUpdateInformation()
+        }
 
 #if DEBUG
         if NSApp.delegateTyped.featureFlagger.isFeatureOn(.autoUpdateInDEBUG) {

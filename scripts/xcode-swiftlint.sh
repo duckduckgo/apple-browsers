a6a36b01774cc7ebadee4dd97b4766ef5fd7fe3d
Chris Brind <brindy@duckduckgo.com>
2025-10-17 13:06:08 +0100

    run swiftlint in xcode from re-useable script

---
+++ scripts/xcode-swiftlint.sh
@@ -0,0 +1,12 @@
+if [[ -z "$BUILD_ROOT" ]]; then
+  echo "BUILD_ROOT not specified, please run from Xcode"
+  exit 1
+fi
+
+if [ "$CONFIGURATION" = "Release" ] || [ "$ENABLE_PREVIEWS" = "YES" ]; then exit 0; fi
+
+LINTER_BASE="$BUILD_ROOT/../.."
+LINTER=`find "$LINTER_BASE" | grep "\-macos/bin/swiftlint$"`
+find $SRCROOT ! -name 'Package.swift' -path $SRCROOT/vendor -prune -o -name "*.swift" | xargs "$LINTER"
+
+

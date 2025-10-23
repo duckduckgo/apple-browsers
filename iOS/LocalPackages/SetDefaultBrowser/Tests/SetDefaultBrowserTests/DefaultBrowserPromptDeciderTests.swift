cb091443c939d7a0dfc448a73fec33c8dde0975e
Alessandro Boron <aboron@duckduckgo.com>
2025-10-23 14:49:03 +1100

    Remove same day prompt check from DefaultBrowserPrompt

--- iOS/LocalPackages/SetDefaultBrowser/Tests/SetDefaultBrowserTests/DefaultBrowserPromptDeciderTests.swift
+++ iOS/LocalPackages/SetDefaultBrowser/Tests/SetDefaultBrowserTests/DefaultBrowserPromptDeciderTests.swift
@@ -62,23 +62,6 @@ final class DefaultBrowserPromptDeciderTests {
         #expect(!userTypeProviderMock.didCallCurrentUserType)
     }
 
-    @Test("Check No Modal Is Presented If A modal Has Already Been Shown In The Same Day")
-    func checkNoModalIsNotPresentedWhenAModalHasAlreadyBeenShownInTheSameDay() {
-        // GIVEN
-        defaultBrowserManagerMock.resultToReturn = .successful(isDefaultBrowser: false)
-        let installDate = Date(timeIntervalSince1970: 1750739150) // Tuesday, 24 June 2025 12:00:00 AM (GMT)
-        let today = Date(timeIntervalSince1970: 1750739150) // Wednesday, 6 August 2025 8:01:24 AM
-        storeMock.lastModalShownDate = today.timeIntervalSince1970
-        makeSUT(installDate: installDate)
-        dateProviderMock.setNowDate(today)
-
-        // WHEN
-        let result = sut.promptType()
-
-        // THEN
-        #expect(result == nil)
-    }
-
     @Test("Check Inactive User Modal Has Priority Over Active User Modal")
     func checkInactiveUserModalIsCheckedBeforeActiveUser() {
         // GIVEN
@@ -398,9 +381,6 @@ final class DefaultBrowserPromptDeciderTests {
         #expect(sut.promptType() == .inactive)
         storeMock.lastModalShownDate = dateProviderMock.getDate().timeIntervalSince1970
         storeMock.hasInactiveModalShown = true
-
-        // Active modal should show but a modal has already been presented
-        #expect(sut.promptType() == nil)
 
         // Advance by one day. First modal after should show
         dateProviderMock.advanceBy(.days(1))

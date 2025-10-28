78945409d6d32f288308e9795cd6836a28e10ac6
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2025-10-28 12:29:33 -0300

    Inject key value store on iOS only

--- iOS/DuckDuckGo/TabViewController.swift
+++ iOS/DuckDuckGo/TabViewController.swift
@@ -2870,6 +2870,7 @@ extension TabViewController: UserContentControllerDelegate {
         userScripts.autoconsentUserScript.delegate = self
         userScripts.contentScopeUserScript.delegate = self
         userScripts.serpSettingsUserScript.delegate = self
+        userScripts.serpSettingsUserScript.setStore(keyValueStore)
         userScripts.serpSettingsUserScript.webView = webView
         
         // Setup DaxEasterEgg handler only for DuckDuckGo search pages

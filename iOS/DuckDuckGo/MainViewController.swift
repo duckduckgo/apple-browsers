c5c6dbc49eca417680a1e45ff2ba37144e5f1792
Pete <petes44@icloud.com>
2025-10-31 15:36:34 +0000

    Minor: Fix Swiftlint

--- iOS/DuckDuckGo/MainViewController.swift
+++ iOS/DuckDuckGo/MainViewController.swift
@@ -1465,7 +1465,7 @@ class MainViewController: UIViewController {
     private func refreshOmniBar() {
         updateOmniBarLoadingState()
 
-        guard let tab = currentTab, (tab.link != nil || tab.tabModel.isAITab) else {
+        guard let tab = currentTab, tab.link != nil || tab.tabModel.isAITab else {
             viewCoordinator.omniBar.stopBrowsing()
             // Clear Dax Easter Egg logo when no tab is active
             viewCoordinator.omniBar.setDaxEasterEggLogoURL(nil)

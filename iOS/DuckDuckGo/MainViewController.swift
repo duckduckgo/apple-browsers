dcfbd283b9c4a6fd1dbe65bd8bdd0ee90fe16702
Pete <petes44@icloud.com>
2025-11-01 10:05:28 +0000

    Tab Swiping - Generate ai chat tab swipe preview

--- iOS/DuckDuckGo/MainViewController.swift
+++ iOS/DuckDuckGo/MainViewController.swift
@@ -568,6 +568,16 @@ class MainViewController: UIViewController {
                 completion?()
             }
 
+        } else if let currentTab = self.tabManager.current(), currentTab.tabModel.isAITab {
+            currentTab.prepareAIChatPreview(completion: { image in
+                guard let image else {
+                    completion?()
+                    return
+                }
+                self.previewsSource.update(preview: image, forTab: currentTab.tabModel)
+                completion?()
+            })
+
         } else if let currentTab = self.tabManager.current(), currentTab.link != nil {
             // Web view
             currentTab.preparePreview(completion: { image in

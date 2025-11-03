f36d4b5090b5ca2cd991edb7746433e3ef4987ac
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-11-03 12:13:49 +0100

    Remove AutoconsentManagement.shared

--- macOS/DuckDuckGo/Application/AppDelegate.swift
+++ macOS/DuckDuckGo/Application/AppDelegate.swift
@@ -136,6 +136,8 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     let fireCoordinator: FireCoordinator
     let permissionManager: PermissionManager
 
+    let autoconsentManagement = AutoconsentManagement()
+
     private var updateProgressCancellable: AnyCancellable?
 
     @MainActor
@@ -728,6 +730,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
                 fireproofDomains: fireproofDomains,
                 fireCoordinator: fireCoordinator,
                 tld: tld,
+                autoconsentManagement: autoconsentManagement,
                 contentScopePreferences: contentScopePreferences
             )
             privacyFeatures = AppPrivacyFeatures(contentBlocking: contentBlocking, database: database.db)

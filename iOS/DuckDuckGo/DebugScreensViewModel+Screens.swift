378a5e3c72baeae5aabfb8efe08ae6523e8cc1f9
Alessandro Boron <aboron@duckduckgo.com>
2025-11-05 09:12:57 +1100

    Add simple debug view to inform about cooldown between modal prompts

--- iOS/DuckDuckGo/DebugScreensViewModel+Screens.swift
+++ iOS/DuckDuckGo/DebugScreensViewModel+Screens.swift
@@ -131,6 +131,9 @@ extension DebugScreensViewModel {
             .view(title: "Win-back Offer", { d in
                 WinBackOfferDebugView(keyValueStore: d.keyValueStore)
             }),
+            .view(title: "Modal Prompt Coordination", { d in
+                ModalPromptCoordinationDebugView(keyValueStore: d.keyValueStore)
+            }),
 
             // MARK: Controllers
             .controller(title: "Image Cache", { d in

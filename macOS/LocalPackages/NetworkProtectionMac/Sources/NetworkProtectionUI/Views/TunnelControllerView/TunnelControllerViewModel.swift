7f1c42592ad096da32bf2fa39160008719db5fea
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-11-07 12:05:29 +0100

    Removes an unused paramter

--- macOS/LocalPackages/NetworkProtectionMac/Sources/NetworkProtectionUI/Views/TunnelControllerView/TunnelControllerViewModel.swift
+++ macOS/LocalPackages/NetworkProtectionMac/Sources/NetworkProtectionUI/Views/TunnelControllerView/TunnelControllerViewModel.swift
@@ -244,7 +244,7 @@ public final class TunnelControllerViewModel: ObservableObject {
     }
 
     @MainActor
-    private func updateRefreshTimer(oldStatus: ConnectionStatus) {
+    private func updateRefreshTimer() {
         switch connectionStatus {
         case .connected:
             startTimer()
@@ -321,7 +321,7 @@ public final class TunnelControllerViewModel: ObservableObject {
     private var connectionStatus: VPN.ConnectionStatus {
         didSet {
             previousConnectionStatus = oldValue
-            updateRefreshTimer(oldStatus: oldValue)
+            updateRefreshTimer()
             refreshTimeLapsed()
         }
     }
@@ -514,7 +514,7 @@ public final class TunnelControllerViewModel: ObservableObject {
             if shouldFlipToggle {
                 isToggleDisabled = true
                 toggleTransition = .switchingOn(locallyInitiated: true)
-                updateRefreshTimer(oldStatus: connectionStatus)
+                updateRefreshTimer()
             }
             defer { toggleTransition = .idle }
 
@@ -529,7 +529,7 @@ public final class TunnelControllerViewModel: ObservableObject {
 
         vpnControlTask = Task { @MainActor in
             toggleTransition = .switchingOff(locallyInitiated: true)
-            updateRefreshTimer(oldStatus: connectionStatus)
+            updateRefreshTimer()
             defer { toggleTransition = .idle }
 
             await tunnelController.stop()

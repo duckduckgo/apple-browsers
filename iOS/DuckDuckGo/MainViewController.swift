61053f8b69b876b2bdd37be7d10543309b0aea22
Pete <petes44@icloud.com>
2025-11-20 07:44:01 +0000

    Post rebase-merge fixes

--- iOS/DuckDuckGo/MainViewController.swift
+++ iOS/DuckDuckGo/MainViewController.swift
@@ -2908,12 +2908,12 @@ extension MainViewController: OmniBarDelegate {
 
     /// Delegate method called when the AI Chat left button is tapped
     func onAIChatLeftButtonPressed() {
-        currentAITab?.submitOpenHistoryAction()
+        currentTab?.submitOpenHistoryAction()
     }
 
     /// Delegate method called when the AI Chat right button is tapped
     func onAIChatRightButtonPressed() {
-        currentAITab?.submitStartChatAction()
+        currentTab?.submitStartChatAction()
     }
 
     /// Delegate method called when the omnibar branding area is tapped while in AI Chat mode.

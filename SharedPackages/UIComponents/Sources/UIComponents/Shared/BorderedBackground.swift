392e439fe64369789e944c46d7139936b6498742
James Frost <jfrost@duckduckgo.com>
2025-11-21 23:55:23 +0000

    Fix CI build error

--- SharedPackages/UIComponents/Sources/UIComponents/Shared/BorderedBackground.swift
+++ SharedPackages/UIComponents/Sources/UIComponents/Shared/BorderedBackground.swift
@@ -30,7 +30,7 @@ public struct BorderedBackgroundModifier: ViewModifier {
     let borderColor: Color
     let cornerRadius: CGFloat
 
-    init(backgroundColor: Color = Color(designSystemColor: .surfaceSecondary), borderColor: Color = Color(designSystemColor: .lines), cornerRadius: CGFloat = 8) {
+    init(backgroundColor: Color = Color(designSystemColor: SharedDesignSystemColor.surfaceSecondary), borderColor: Color = Color(designSystemColor: SharedDesignSystemColor.lines), cornerRadius: CGFloat = 8) {
         self.backgroundColor = backgroundColor
         self.borderColor = borderColor
         self.cornerRadius = cornerRadius
@@ -57,7 +57,7 @@ public extension View {
     ///   - borderColor: The border color. Defaults to `.lines` design system color.
     ///   - cornerRadius: The corner radius for the rounded rectangle. Defaults to 8.
     /// - Returns: A view with the bordered background style applied.
-    func borderedBackground(color: Color = Color(designSystemColor: .surfaceSecondary), borderColor: Color = Color(designSystemColor: .lines), cornerRadius: CGFloat = 8) -> some View {
+    func borderedBackground(color: Color = Color(designSystemColor: SharedDesignSystemColor.surfaceSecondary), borderColor: Color = Color(designSystemColor: SharedDesignSystemColor.lines), cornerRadius: CGFloat = 8) -> some View {
         modifier(BorderedBackgroundModifier(backgroundColor: color, borderColor: borderColor, cornerRadius: cornerRadius))
     }
 }

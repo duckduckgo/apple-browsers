e2df1a793f12879404f15be8f6d4e31c02c8d91a
Jacek Łyp <jlyp@duckduckgo.com>
2025-11-21 21:43:06 +0100

    Add metrics to amp contains keyword logic to understand if the large input is the issue

--- macOS/DuckDuckGo/Statistics/GeneralPixel.swift
+++ macOS/DuckDuckGo/Statistics/GeneralPixel.swift
@@ -97,6 +97,7 @@ enum GeneralPixel: PixelKitEvent {
     case bitwardenPasswordSaved
 
     case ampBlockingRulesCompilationFailed
+    case ampKeywordDetectionPerformance
 
     case adClickAttributionDetected
     case adClickAttributionActive
@@ -712,6 +713,8 @@ enum GeneralPixel: PixelKitEvent {
 
         case .ampBlockingRulesCompilationFailed:
             return "m_mac_amp_rules_compilation_failed"
+        case .ampKeywordDetectionPerformance:
+            return "m_mac_amp_keyword_detection_performance"
 
         case .adClickAttributionDetected:
             return "m_mac_ad_click_detected"

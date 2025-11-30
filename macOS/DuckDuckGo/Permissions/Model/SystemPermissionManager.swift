eb020a3ccac55ef39b573f4b1831bcecf9dd2d21
Tomas Strba <tom@duckduckgo.com>
2025-11-30 09:29:21 +0100

    Refactoring SystemPermissionManager to only contain what's necessary

--- macOS/DuckDuckGo/Permissions/Model/SystemPermissionManager.swift
+++ macOS/DuckDuckGo/Permissions/Model/SystemPermissionManager.swift
@@ -36,32 +36,6 @@ enum SystemPermissionAuthorizationState {
 /// Protocol for managing system-level permissions required before website permissions can be granted
 protocol SystemPermissionManagerProtocol: AnyObject {
 
-    // MARK: - Geolocation
-
-    /// Returns the current geolocation authorization state
-    var geolocationAuthorizationState: SystemPermissionAuthorizationState { get }
-
-    /// Publisher that emits the current geolocation authorization state whenever it changes
-    var geolocationAuthorizationStatePublisher: AnyPublisher<SystemPermissionAuthorizationState, Never> { get }
-
-    /// Returns true if geolocation authorization has been granted
-    var isGeolocationAuthorized: Bool { get }
-
-    /// Returns true if geolocation authorization needs to be requested
-    var isGeolocationAuthorizationRequired: Bool { get }
-
-    /// Requests geolocation authorization from the system
-    /// - Parameter completion: Called with the resulting authorization state after the user responds
-    /// - Returns: A cancellable that can be used to cancel the observation
-    @discardableResult
-    func requestGeolocationAuthorization(completion: @escaping (SystemPermissionAuthorizationState) -> Void) -> AnyCancellable
-
-    /// Requests geolocation authorization from the system using async/await
-    /// - Returns: The authorization state after the user responds
-    func requestGeolocationAuthorization() async -> SystemPermissionAuthorizationState
-
-    // MARK: - Generic Permission Methods
-
     /// Returns the current authorization state for the given permission type
     func authorizationState(for permissionType: PermissionType) -> SystemPermissionAuthorizationState
 
@@ -86,106 +60,7 @@ final class SystemPermissionManager: SystemPermissionManagerProtocol {
         self.geolocationService = geolocationService
     }
 
-    // MARK: - Geolocation Authorization
-
-    /// Returns the current geolocation authorization state
-    var geolocationAuthorizationState: SystemPermissionAuthorizationState {
-        guard geolocationService.locationServicesEnabled() else {
-            return .systemDisabled
-        }
-
-        switch geolocationService.authorizationStatus {
-        case .notDetermined:
-            return .notDetermined
-        case .authorized, .authorizedAlways:
-            return .authorized
-        case .denied:
-            return .denied
-        case .restricted:
-            return .restricted
-        @unknown default:
-            return .notDetermined
-        }
-    }
-
-    /// Publisher that emits the current geolocation authorization state whenever it changes
-    var geolocationAuthorizationStatePublisher: AnyPublisher<SystemPermissionAuthorizationState, Never> {
-        geolocationService.authorizationStatusPublisher
-            .map { [weak self] _ in
-                self?.geolocationAuthorizationState ?? .notDetermined
-            }
-            .eraseToAnyPublisher()
-    }
-
-    /// Returns true if geolocation authorization has been granted
-    var isGeolocationAuthorized: Bool {
-        geolocationAuthorizationState == .authorized
-    }
-
-    /// Returns true if geolocation authorization needs to be requested
-    var isGeolocationAuthorizationRequired: Bool {
-        switch geolocationAuthorizationState {
-        case .notDetermined, .systemDisabled:
-            return true
-        case .authorized, .denied, .restricted:
-            return false
-        }
-    }
-
-    /// Requests geolocation authorization from the system
-    /// - Parameter completion: Called with the resulting authorization state after the user responds
-    /// - Returns: A cancellable that can be used to cancel the observation
-    @discardableResult
-    func requestGeolocationAuthorization(completion: @escaping (SystemPermissionAuthorizationState) -> Void) -> AnyCancellable {
-        // If already determined, return current state immediately
-        guard geolocationAuthorizationState == .notDetermined else {
-            completion(geolocationAuthorizationState)
-            return AnyCancellable {}
-        }
-
-        var locationCancellable: AnyCancellable?
-
-        // Subscribe to authorization status publisher to observe changes
-        let authorizationCancellable = geolocationService.authorizationStatusPublisher
-            .dropFirst() // Skip initial value, we want to observe changes
-            .first() // Only need the first change
-            .sink { [weak self] _ in
-                let state = self?.geolocationAuthorizationState ?? .notDetermined
-                // Cancel location subscription once we have the authorization result
-                locationCancellable?.cancel()
-                completion(state)
-            }
-
-        // Subscribe to location publisher to trigger authorization request
-        // The GeolocationService calls requestWhenInUseAuthorization() when first subscribed
-        // We keep this subscription alive until authorization is determined
-        locationCancellable = geolocationService.locationPublisher
-            .sink { _ in }
-
-        return AnyCancellable {
-            authorizationCancellable.cancel()
-            locationCancellable?.cancel()
-        }
-    }
-
-    /// Requests geolocation authorization from the system using async/await
-    /// - Returns: The authorization state after the user responds
-    func requestGeolocationAuthorization() async -> SystemPermissionAuthorizationState {
-        // If already determined, return current state immediately
-        guard geolocationAuthorizationState == .notDetermined else {
-            return geolocationAuthorizationState
-        }
-
-        return await withCheckedContinuation { continuation in
-            var cancellable: AnyCancellable?
-            cancellable = requestGeolocationAuthorization { state in
-                continuation.resume(returning: state)
-                cancellable?.cancel()
-            }
-        }
-    }
-
-    // MARK: - Generic Permission Methods
+    // MARK: - Public Methods
 
     /// Returns the current authorization state for the given permission type
     func authorizationState(for permissionType: PermissionType) -> SystemPermissionAuthorizationState {
@@ -217,6 +92,69 @@ final class SystemPermissionManager: SystemPermissionManagerProtocol {
             // These don't require system permission through our two-step flow
             completion(.authorized)
             return nil
+        }
+    }
+
+    // MARK: - Private Geolocation Implementation
+
+    private var geolocationAuthorizationState: SystemPermissionAuthorizationState {
+        guard geolocationService.locationServicesEnabled() else {
+            return .systemDisabled
+        }
+
+        switch geolocationService.authorizationStatus {
+        case .notDetermined:
+            return .notDetermined
+        case .authorized, .authorizedAlways:
+            return .authorized
+        case .denied:
+            return .denied
+        case .restricted:
+            return .restricted
+        @unknown default:
+            return .notDetermined
+        }
+    }
+
+    private var isGeolocationAuthorizationRequired: Bool {
+        switch geolocationAuthorizationState {
+        case .notDetermined, .systemDisabled:
+            return true
+        case .authorized, .denied, .restricted:
+            return false
+        }
+    }
+
+    @discardableResult
+    private func requestGeolocationAuthorization(completion: @escaping (SystemPermissionAuthorizationState) -> Void) -> AnyCancellable {
+        // If already determined, return current state immediately
+        guard geolocationAuthorizationState == .notDetermined else {
+            completion(geolocationAuthorizationState)
+            return AnyCancellable {}
+        }
+
+        var locationCancellable: AnyCancellable?
+
+        // Subscribe to authorization status publisher to observe changes
+        let authorizationCancellable = geolocationService.authorizationStatusPublisher
+            .dropFirst() // Skip initial value, we want to observe changes
+            .first() // Only need the first change
+            .sink { [weak self] _ in
+                let state = self?.geolocationAuthorizationState ?? .notDetermined
+                // Cancel location subscription once we have the authorization result
+                locationCancellable?.cancel()
+                completion(state)
+            }
+
+        // Subscribe to location publisher to trigger authorization request
+        // The GeolocationService calls requestWhenInUseAuthorization() when first subscribed
+        // We keep this subscription alive until authorization is determined
+        locationCancellable = geolocationService.locationPublisher
+            .sink { _ in }
+
+        return AnyCancellable {
+            authorizationCancellable.cancel()
+            locationCancellable?.cancel()
         }
     }
 }

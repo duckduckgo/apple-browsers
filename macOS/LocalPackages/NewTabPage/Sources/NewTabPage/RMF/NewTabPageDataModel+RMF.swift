85ce4b140997b2bd1cd4c0fceb60fc19dd4dbc46
Anh Do <ado@duckduckgo.com>
2025-12-01 15:17:58 -0500

    Remove Radar

--- macOS/LocalPackages/NewTabPage/Sources/NewTabPage/RMF/NewTabPageDataModel+RMF.swift
+++ macOS/LocalPackages/NewTabPage/Sources/NewTabPage/RMF/NewTabPageDataModel+RMF.swift
@@ -119,7 +119,6 @@ extension NewTabPageDataModel {
         case subscription = "PrivacyPro"
         case duckai = "DuckAi"
         case pir = "PIR"
-        case radar = "Radar"
         case radarNew = "RadarNew"
         case radarCheck = "RadarCheck"
         case subscriptionIcon = "Subscription"
@@ -140,8 +139,6 @@ extension NewTabPageDataModel {
                 self = .duckai
             case .pir:
                 self = .pir
-            case .radar:
-                self = .radar
             case .radarNew:
                 self = .radarNew
             case .radarCheck:

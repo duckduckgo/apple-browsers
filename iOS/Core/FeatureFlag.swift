7e99c99198a231ceea6d517b2b3c4fee1732c744
Hassaan Elgarem <helgarem@duckduckgo.com>
2025-12-02 02:19:44 +0200

    Adds `granularFireButtonOptions` feature flag

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -274,6 +274,9 @@ public enum FeatureFlag: String {
 
     /// https://app.asana.com/1/137249556945/project/1201462886803403/task/1211326076710245?focus=true
     case migrateKeychainAccessibility
+
+    /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1212229431540900
+    case granularFireButtonOptions
 }
 
 extension FeatureFlag: FeatureFlagDescribing {
@@ -356,7 +359,8 @@ extension FeatureFlag: FeatureFlagDescribing {
              .browsingMenuSheetPresentation,
              .autofillExtensionSettings,
              .canPromoteAutofillExtensionInBrowser,
-             .canPromoteAutofillExtensionInPasswordManagement:
+             .canPromoteAutofillExtensionInPasswordManagement,
+             .granularFireButtonOptions:
             return true
         case .showSettingsCompleteSetupSection:
             if #available(iOS 18.2, *) {
@@ -571,6 +575,8 @@ extension FeatureFlag: FeatureFlagDescribing {
             return .remoteReleasable(.subfeature(AutofillSubfeature.canPromoteAutofillExtensionInPasswordManagement))
         case .migrateKeychainAccessibility:
             return .remoteReleasable(.subfeature(AutofillSubfeature.migrateKeychainAccessibility))
+        case .granularFireButtonOptions:
+            return .remoteReleasable(.subfeature(iOSBrowserConfigSubfeature.granularFireButtonOptions))
         }
     }
 }

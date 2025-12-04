3aae72d32a42682946dbf2de7dfabd10bf4ba6f2
Hanyu Tang <htang@duckduckgo.com>
2025-12-04 16:15:38 +0000

    Add tests

--- SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/DataImport/DataImportWideEventData.swift
+++ SharedPackages/BrowserServicesKit/Sources/BrowserServicesKit/DataImport/DataImportWideEventData.swift
@@ -1,14 +1,26 @@
 //
 //  DataImportWideEventData.swift
-//  BrowserServicesKit
 //
-//  Created by Hanyu Tang on 04/12/2025.
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitation
 
 import Foundation
 import PixelKit
 
 public class DataImportWideEventData: WideEventData {
+
+    typealias DataType = DataImport.DataType
 
     #if DEBUG
     public static let pixelName = "data_import_debug"
@@ -30,7 +42,7 @@ public class DataImportWideEventData: WideEventData {
     public var bookmarkImporterDuration: WideEvent.MeasuredInterval?
     public var passwordImporterDuration: WideEvent.MeasuredInterval?
     public var creditCardImporterDuration: WideEvent.MeasuredInterval?
-    
+
     // Per-type status
     public var bookmarkImportStatus: WideEventStatus?
     public var passwordImportStatus: WideEventStatus?
@@ -57,7 +69,7 @@ public class DataImportWideEventData: WideEventData {
                 appData: WideEventAppData = WideEventAppData(),
                 globalData: WideEventGlobalData = WideEventGlobalData()) {
         self.source = source
-        
+
         self.overallDuration = overallDuration
         self.bookmarkImporterDuration = bookmarkImporterDuration
         self.passwordImporterDuration = passwordImporterDuration
@@ -67,12 +79,12 @@ public class DataImportWideEventData: WideEventData {
         self.bookmarkImportStatus = bookmarkImportStatus
         self.passwordImportStatus = passwordImportStatus
         self.creditCardImportStatus = creditCardImportStatus
-        
+
         // Per-type errors
         self.bookmarkImportError = bookmarkImportError
         self.passwordImportError = passwordImportError
         self.creditCardImportError = creditCardImportError
-        
+
         self.errorData = errorData
         self.contextData = contextData
         self.appData = appData
@@ -85,41 +97,10 @@ public class DataImportWideEventData: WideEventData {
 // MARK: - Public
 
 extension DataImportWideEventData {
-    
+
     public enum StatusReason: String, Codable, CaseIterable {
         case partialData = "partial_data"
         case timeout
-    }
-
-    public enum ImportType: String, Codable, CaseIterable {
-        case bookmark
-        case password
-        case creditCard = "credit_card"
-
-        public var statusPath: WritableKeyPath<DataImportWideEventData, WideEventStatus?> {
-            switch self {
-            case .bookmark: return \.bookmarkImportStatus
-            case .password: return \.passwordImportStatus
-            case .creditCard: return \.creditCardImportStatus
-            }
-        }
-        
-        public var importerDurationPath: WritableKeyPath<DataImportWideEventData, WideEvent.MeasuredInterval?> {
-            switch self {
-            case .bookmark: return \.bookmarkImporterDuration
-            case .password: return \.passwordImporterDuration
-            case .creditCard: return \.creditCardImporterDuration
-            }
-        }
-
-
-        public var errorPath: WritableKeyPath<DataImportWideEventData, WideEventErrorData?> {
-            switch self {
-            case .bookmark: return \.bookmarkImportError
-            case .password: return \.passwordImportError
-            case .creditCard: return \.creditCardImportError
-            }
-        }
     }
 
     public func pixelParameters() -> [String: String] {
@@ -128,12 +109,11 @@ extension DataImportWideEventData {
         params[WideEventParameter.Feature.name] = Self.featureName
         params[WideEventParameter.DataImportFeature.source] = source.id
 
-        // Overall latency
         if let overallDuration = overallDuration?.durationMilliseconds {
             params[WideEventParameter.DataImportFeature.latency] = String(Int(overallDuration))
         }
 
-        for type in ImportType.allCases {
+        for type in DataImport.DataType.allCases {
             addTypeStatusAndReason(self[keyPath: type.statusPath], type: type, to: &params)
             addTypeImporterLatency(self[keyPath: type.importerDurationPath], type: type, to: &params)
             addTypeError(self[keyPath: type.errorPath], type: type, to: &params)
@@ -146,8 +126,8 @@ extension DataImportWideEventData {
 // MARK: - Private
 
 private extension DataImportWideEventData {
-    
-    func addTypeStatusAndReason(_ status: WideEventStatus?, type: ImportType, to params: inout [String: String]) {
+
+    func addTypeStatusAndReason(_ status: WideEventStatus?, type: DataType, to params: inout [String: String]) {
         guard let status else { return }
         params[WideEventParameter.DataImportFeature.status(for: type)] = status.description
 
@@ -159,12 +139,12 @@ private extension DataImportWideEventData {
         }
     }
 
-    func addTypeImporterLatency(_ interval: WideEvent.MeasuredInterval?, type: ImportType, to params: inout [String: String]) {
+    func addTypeImporterLatency(_ interval: WideEvent.MeasuredInterval?, type: DataType, to params: inout [String: String]) {
         guard let duration = interval?.durationMilliseconds else { return }
         params[WideEventParameter.DataImportFeature.latency(for: type)] = String(Int(duration))
     }
 
-    func addTypeError(_ error: WideEventErrorData?, type: ImportType, to params: inout [String: String]) {
+    func addTypeError(_ error: WideEventErrorData?, type: DataType, to params: inout [String: String]) {
         guard let error else { return }
         let errorParams = error.pixelParameters()
         for (key, value) in errorParams {
@@ -173,7 +153,7 @@ private extension DataImportWideEventData {
         }
     }
 
-    func transformErrorKey(_ key: String, for type: ImportType) -> String {
+    func transformErrorKey(_ key: String, for type: DataType) -> String {
         switch key {
         case WideEventParameter.Feature.errorDomain:
             return WideEventParameter.DataImportFeature.errorDomain(for: type)
@@ -206,36 +186,62 @@ extension WideEventParameter {
         static let source = "feature.data.ext.source"
         static let latency = "feature.data.ext.latency_ms"
 
-        static func latency(for type: DataImportWideEventData.ImportType) -> String {
-            "feature.data.ext.\(type.rawValue)_importer_latency_ms"
-        }
-        
-        static func status(for type: DataImportWideEventData.ImportType) -> String {
-            "feature.data.ext.\(type.rawValue)_import_status"
-        }
-        
-        static func statusReason(for type: DataImportWideEventData.ImportType) -> String {
-            "feature.data.ext.\(type.rawValue)_import_status_reason"
+        static func latency(for type: DataImportWideEventData.DataType) -> String {
+            "feature.data.ext.\(type.description)_importer_latency_ms"
         }
 
-        static func errorDomain(for type: DataImportWideEventData.ImportType) -> String {
-            "feature.data.ext.\(type.rawValue)_import_error.domain"
+        static func status(for type: DataImportWideEventData.DataType) -> String {
+            "feature.data.ext.\(type.description)_status"
         }
 
-        static func errorCode(for type: DataImportWideEventData.ImportType) -> String {
-            "feature.data.ext.\(type.rawValue)_import_error.code"
+        static func statusReason(for type: DataImportWideEventData.DataType) -> String {
+            "feature.data.ext.\(type.description)_status_reason"
         }
 
-        static func errorDescription(for type: DataImportWideEventData.ImportType) -> String {
-            "feature.data.ext.\(type.rawValue)_import_error.description"
+        static func errorDomain(for type: DataImportWideEventData.DataType) -> String {
+            "feature.data.ext.\(type.description)_error.domain"
         }
 
-        static func errorUnderlyingDomain(for type: DataImportWideEventData.ImportType, suffix: String) -> String {
-            return "feature.data.ext.\(type.rawValue)_import_error.underlying_domain\(suffix)"
+        static func errorCode(for type: DataImportWideEventData.DataType) -> String {
+            "feature.data.ext.\(type.description)_error.code"
         }
 
-        static func errorUnderlyingCode(for type: DataImportWideEventData.ImportType, suffix: String) -> String {
-            return "feature.data.ext.\(type.rawValue)_import_error.underlying_code\(suffix)"
+        static func errorDescription(for type: DataImportWideEventData.DataType) -> String {
+            "feature.data.ext.\(type.description)_error.description"
+        }
+
+        static func errorUnderlyingDomain(for type: DataImportWideEventData.DataType, suffix: String) -> String {
+            return "feature.data.ext.\(type.description)_error.underlying_domain\(suffix)"
+        }
+
+        static func errorUnderlyingCode(for type: DataImportWideEventData.DataType, suffix: String) -> String {
+            return "feature.data.ext.\(type.description)_error.underlying_code\(suffix)"
+        }
+    }
+}
+
+fileprivate extension DataImport.DataType {
+    var statusPath: WritableKeyPath<DataImportWideEventData, WideEventStatus?> {
+        switch self {
+        case .bookmarks: return \.bookmarkImportStatus
+        case .passwords: return \.passwordImportStatus
+        case .creditCards: return \.creditCardImportStatus
+        }
+    }
+
+    var importerDurationPath: WritableKeyPath<DataImportWideEventData, WideEvent.MeasuredInterval?> {
+        switch self {
+        case .bookmarks: return \.bookmarkImporterDuration
+        case .passwords: return \.passwordImporterDuration
+        case .creditCards: return \.creditCardImporterDuration
+        }
+    }
+
+    var errorPath: WritableKeyPath<DataImportWideEventData, WideEventErrorData?> {
+        switch self {
+        case .bookmarks: return \.bookmarkImportError
+        case .passwords: return \.passwordImportError
+        case .creditCards: return \.creditCardImportError
         }
     }
 }

6fe2700a2e13fd70f25d0d00c1fd5677c6b43803
Michal Smaga <msmaga@duckduckgo.com>
2025-12-04 11:45:03 +0100

    Adjust the timing

--- macOS/DuckDuckGo/Autoconsent/AutoconsentStatsPopoverCoordinator.swift
+++ macOS/DuckDuckGo/Autoconsent/AutoconsentStatsPopoverCoordinator.swift
@@ -160,7 +160,7 @@ final class AutoconsentStatsPopoverCoordinator: AutoconsentStatsPopoverCoordinat
     private func openNewTabWithSpecialAction() {
         windowControllersManager.showTab(with: .newtab)
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
             if let newTabPageViewModel = self.windowControllersManager.mainWindowController?.mainViewController.browserTabViewController.newTabPageWebViewModel {
                 NSApp.delegateTyped.newTabPageProtectionsReportModel.scroller.scroll(for: newTabPageViewModel.webView)
             }

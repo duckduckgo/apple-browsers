bbb9c4ce08c8af6f3230137f38cb60a867837e34
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2025-12-08 17:52:53 -0300

    ButtonStyles: Background Color is now parametric

--- macOS/LocalPackages/SwiftUIExtensions/Sources/SwiftUIExtensions/ButtonStyles.swift
+++ macOS/LocalPackages/SwiftUIExtensions/Sources/SwiftUIExtensions/ButtonStyles.swift
@@ -24,17 +24,20 @@ public struct StandardButtonStyle: ButtonStyle {
     public let topPadding: CGFloat
     public let bottomPadding: CGFloat
     public let horizontalPadding: CGFloat
+    public let backgroundColor: Color
+    public let pressedBackgroundColor: Color
 
-    public init(fontSize: CGFloat = 13, topPadding: CGFloat = 2.5, bottomPadding: CGFloat = 3, horizontalPadding: CGFloat = 7.5) {
+    public init(fontSize: CGFloat = 13, topPadding: CGFloat = 2.5, bottomPadding: CGFloat = 3, horizontalPadding: CGFloat = 7.5, backgroundColor: Color? = nil, pressedBackgroundColor: Color? = nil) {
         self.fontSize = fontSize
         self.topPadding = topPadding
         self.bottomPadding = bottomPadding
         self.horizontalPadding = horizontalPadding
+        self.backgroundColor = backgroundColor ?? Color(.pwmButtonBackground)
+        self.pressedBackgroundColor = pressedBackgroundColor ?? Color(.pwmButtonBackgroundPressed)
     }
 
     public func makeBody(configuration: Self.Configuration) -> some View {
-
-        let backgroundColor = configuration.isPressed ? Color(.pwmButtonBackgroundPressed) : Color(.pwmButtonBackground)
+        let backgroundColor = configuration.isPressed ? pressedBackgroundColor : backgroundColor
         let labelColor = Color(.pwmButtonLabel)
 
         configuration.label

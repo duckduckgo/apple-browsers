a249aadeee41ec69a633a25a6d84f2ea4590a604
Sam Symons <sam@samsymons.com>
2025-12-12 13:49:28 -0800

    Update comment

--- SharedPackages/BrowserServicesKit/Sources/RemoteMessaging/Mappers/JsonToRemoteMessageModelMapper.swift
+++ SharedPackages/BrowserServicesKit/Sources/RemoteMessaging/Mappers/JsonToRemoteMessageModelMapper.swift
@@ -175,7 +175,8 @@ struct JsonToRemoteMessageModelMapper {
             }
         }
 
-        // If surface is not defined set to supportedSurfacesForMessage for backward compatibility (e.g. `.small` -> `newTabPage`, `promoList` -> `[.modal, .dedicatedTab]`)
+        // If surface is not defined then set to supportedSurfacesForMessage for backward compatibility (e.g. `.small` -> `newTabPage`, `promoList` -> `[.modal, .dedicatedTab]`)
+        // If the supported surfaces contains `newTabPage` then we return ONLY that, otherwise messages could appear on the tab bar unexpectedly.
         guard let jsonSurfaces else {
             Logger.remoteMessaging.debug("No surfaces declared for message \(messageId, privacy: .public)")
             if supportedSurfacesForMessage.contains(.newTabPage) {

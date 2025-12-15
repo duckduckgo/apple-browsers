eb1de61a4276dd10061b4f656533aa0cf2ba8fd4
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-12-15 19:02:41 +0100

    Fix an issue with a string format

--- macOS/DuckDuckGo/Permissions/View/PermissionContextMenu.swift
+++ macOS/DuckDuckGo/Permissions/View/PermissionContextMenu.swift
@@ -436,7 +436,7 @@ private extension NSMenuItem {
         case .externalScheme(scheme: let scheme):
             title = String(format: UserText.permissionMenuHeaderExternalSchemeFormat, permission.localizedDescription.lowercased(), scheme)
         case .notification:
-            title = UserText.notificationPermissionAuthorizationFormat
+            title = String(format: UserText.notificationPermissionAuthorizationFormat, domain)
         case .popups:
             title = String(format: UserText.permissionMenuHeaderPopupWindowsFormat, domain)
         }

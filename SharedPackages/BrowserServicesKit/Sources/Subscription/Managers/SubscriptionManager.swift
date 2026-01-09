6e81f50884743b5a4a39da1fac793db03db047ab
Federico Cappelli <kap.posta@gmail.com>
2026-01-09 09:51:13 +0000

    lint

--- SharedPackages/BrowserServicesKit/Sources/Subscription/Managers/SubscriptionManager.swift
+++ SharedPackages/BrowserServicesKit/Sources/Subscription/Managers/SubscriptionManager.swift
@@ -216,11 +216,7 @@ extension SubscriptionManager {
     public func signOut(notifyUI: Bool) async {
         await signOut(notifyUI: notifyUI, userInitiated: false)
     }
-
-//    public func currentSubscriptionFeatures() async throws -> [SubscriptionEntitlement] {
-//        try await currentSubscriptionFeatures(forceRefresh: false)
-//    }
-
+    
     /// Checks whether the user is eligible to purchase the subscription, regardless of purchase platform.
     public var isSubscriptionPurchaseEligible: Bool {
         switch currentEnvironment.purchasePlatform {

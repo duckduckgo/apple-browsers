6d4bcac6ab66d96bb35aee1a4be21cef951cf1c5
Chris Brind <brindy@duckduckgo.com>
2026-01-12 11:17:59 +0000

    remove bottom constraint when installing suggestion controller

--- iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/FlowManagers/SuggestionTrayManager.swift
+++ iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/FlowManagers/SuggestionTrayManager.swift
@@ -141,7 +141,7 @@ final class SuggestionTrayManager: NSObject {
             controller.view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             controller.view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
             controller.view.topAnchor.constraint(equalTo: containerView.topAnchor),
-            controller.view.bottomAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.bottomAnchor)
+            // Bottom constraint does not appear to be required and causes a problem with iPad in portrait mode
         ])
 
         controller.autocompleteDelegate = self

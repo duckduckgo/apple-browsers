09f00e991139986577415ac1ece3730b010f58ce
Sabrina Tardio <44158575+SabrinaTardio@users.noreply.github.com>
2026-01-12 14:26:39 +0100

    Update FeatureFlag.swift

--- macOS/LocalPackages/FeatureFlags/Sources/FeatureFlags/FeatureFlag.swift
+++ macOS/LocalPackages/FeatureFlags/Sources/FeatureFlags/FeatureFlag.swift
@@ -19,7 +19,7 @@
 import Foundation
 import PrivacyConfig
 
-public enum FeatureFlag: String, CaseIterable {                                             
+public enum FeatureFlag: String, CaseIterable {                                       
     /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1211866715841970
     case maliciousSiteProtection
 

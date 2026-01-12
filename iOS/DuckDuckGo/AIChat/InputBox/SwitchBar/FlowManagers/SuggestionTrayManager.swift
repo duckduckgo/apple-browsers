50ab9978dd483fc607f87f938dcdd8b950030ad4
Chris Brind <brindy@duckduckgo.com>
2026-01-12 16:46:54 +0000

    use looser bottom constraint

--- iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/FlowManagers/SuggestionTrayManager.swift
+++ iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/FlowManagers/SuggestionTrayManager.swift
@@ -141,7 +141,7 @@ final class SuggestionTrayManager: NSObject {
             controller.view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
             controller.view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
             controller.view.topAnchor.constraint(equalTo: containerView.topAnchor),
-            // Bottom constraint does not appear to be required and causes a problem with iPad in portrait mode
+            controller.view.bottomAnchor.constraint(lessThanOrEqualTo: containerView.safeAreaLayoutGuide.bottomAnchor)
         ])
 
         controller.autocompleteDelegate = self

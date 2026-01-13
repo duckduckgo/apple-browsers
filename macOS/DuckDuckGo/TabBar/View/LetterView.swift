4bef5d1b1679f4e81deb0fa7c0be25da6b5b938d
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-01-13 11:06:32 -0300

    Favicon Placeholder: Inactive State Alpha

--- macOS/DuckDuckGo/TabBar/View/LetterView.swift
+++ macOS/DuckDuckGo/TabBar/View/LetterView.swift
@@ -17,9 +17,12 @@
 //
 
 import Foundation
+import Combine
 import SwiftUIExtensions
 
 final class LetterView: NSView {
+
+    private var applicationStatePublisher: AnyCancellable?
 
     enum BackgroundShape {
         case rectangle
@@ -29,11 +32,13 @@ final class LetterView: NSView {
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         setup()
+        startListeningToNotifications()
     }
 
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setup()
+        startListeningToNotifications()
     }
 
     func displayURL(_ url: URL?) {
@@ -128,5 +133,23 @@ final class LetterView: NSView {
         }
 
         backgroundLayer.cornerRadius = backgroundCornerRadius
+    }
+}
+
+private extension LetterView {
+
+    private func startListeningToNotifications() {
+        applicationStatePublisher = NSApp.isActivePublisher()
+            .dropFirst()
+            .sink { [weak self] isActive in
+                self?.refreshActivationAlpha(isActive: isActive)
+            }
+    }
+
+    func refreshActivationAlpha(isActive: Bool) {
+        let alphaWhenActive: CGFloat = 1
+        let alphaWhenInactive: CGFloat = 0.4
+
+        backgroundView.alphaValue = isActive ? alphaWhenActive : alphaWhenInactive
     }
 }

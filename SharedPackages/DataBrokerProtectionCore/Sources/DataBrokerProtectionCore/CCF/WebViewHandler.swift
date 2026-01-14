d62a536e3d23569b1bc9fd9773d251c0b288b5f1
Anh Do <ado@duckduckgo.com>
2026-01-14 11:12:28 -0500

    One can now close web view

--- SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCore/CCF/WebViewHandler.swift
+++ SharedPackages/DataBrokerProtectionCore/Sources/DataBrokerProtectionCore/CCF/WebViewHandler.swift
@@ -81,7 +81,7 @@ final class DataBrokerProtectionWebViewHandler: NSObject, WebViewHandler {
         if showWebView {
 #if os(macOS)
             window = NSWindow(
-                contentRect: NSRect(x: 0, y: 0, width: 1024, height: 1024), styleMask: [.titled],
+                contentRect: NSRect(x: 0, y: 0, width: 1024, height: 1024), styleMask: [.titled, .closable, .miniaturizable, .resizable],
                 backing: .buffered, defer: false
             )
             window?.title = "Data Broker Protection"

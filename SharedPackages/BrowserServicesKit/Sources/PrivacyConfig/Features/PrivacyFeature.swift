9eb20c98a4788905ac60574a300fc5b37b65cce8
Jacek Łyp <jlyp@duckduckgo.com>
2026-01-21 16:27:27 +0100

    Fix swiftlint

--- SharedPackages/BrowserServicesKit/Sources/PrivacyConfig/Features/PrivacyFeature.swift
+++ SharedPackages/BrowserServicesKit/Sources/PrivacyConfig/Features/PrivacyFeature.swift
@@ -208,7 +208,7 @@ public enum iOSBrowserConfigSubfeature: String, PrivacySubfeature {
 
     /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1212875994217788?focus=true
     case genericBackgroundTask
-    
+
     // https://app.asana.com/1/137249556945/project/392891325557410/task/1211597475706631?focus=true
     case webViewFlashPrevention
 

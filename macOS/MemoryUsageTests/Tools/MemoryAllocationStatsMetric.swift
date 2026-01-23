e2e5b9efab3fea2aabb6963ed435e6dc083e86bc
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-01-23 16:41:01 -0300

    Adds Extra Logs

--- macOS/MemoryUsageTests/Tools/MemoryAllocationStatsMetric.swift
+++ macOS/MemoryUsageTests/Tools/MemoryAllocationStatsMetric.swift
@@ -80,6 +80,9 @@ final class MemoryAllocationStatsMetric: NSObject, XCTMetric {
         initialStatsAttachment = attachment
 
         logger.log("[MemoryAllocationStatsMetric] Initial Memory Used: \(snapshot.totalUsedMB)")
+        if let rawLogsInitial = try? String(contentsOf: memoryStatsURL) {
+            logger.log("[MemoryAllocationStatsMetric] Initial Raw Logs: \(rawLogsInitial, privacy: .public)")
+        }
     }
 
     func didStopMeasuring() {
@@ -91,6 +94,9 @@ final class MemoryAllocationStatsMetric: NSObject, XCTMetric {
         finalStatsAttachment = attachment
 
         logger.log("[MemoryAllocationStatsMetric] Final Memory Used: \(snapshot.totalUsedMB)")
+        if let rawLogsFinal = try? String(contentsOf: memoryStatsURL) {
+            logger.log("[MemoryAllocationStatsMetric] Final Raw Logs: \(rawLogsFinal, privacy: .public)")
+        }
     }
 
     func reportMeasurements(from startTime: XCTPerformanceMeasurementTimestamp, to endTime: XCTPerformanceMeasurementTimestamp) throws -> [XCTPerformanceMeasurement] {

ad2ce48fcf251f7a673af7c8878168a10ead09e6
Hassaan Elgarem <helgarem@duckduckgo.com>
2026-01-26 21:11:56 +0200

    Rename legacy view model tests

--- iOS/DuckDuckGoTests/Fire/GranularFireConfirmationViewModelTests.swift
+++ iOS/DuckDuckGoTests/Fire/GranularFireConfirmationViewModelTests.swift
@@ -1,5 +1,5 @@
 //
-//  FireConfirmationViewModelTests.swift
+//  GranularFireConfirmationViewModelTests.swift
 //  DuckDuckGo
 //
 //  Copyright © 2025 DuckDuckGo. All rights reserved.
@@ -29,13 +29,13 @@ import PersistenceTestingUtils
 // swiftlint:disable force_try
 
 @MainActor
-final class FireConfirmationViewModelTests: XCTestCase {
+final class GranularFireConfirmationViewModelTests: XCTestCase {
     
     private var mockKeyValueStore: MockKeyValueFileStore!
     
     override func setUp() {
         super.setUp()
-        mockKeyValueStore = try! MockKeyValueFileStore()
+        mockKeyValueStore = MockKeyValueFileStore()
     }
     
     override func tearDown() {

16323b42af0fa30684bb3893983fa9c11ec478ef
Alessandro Boron <aboron@duckduckgo.com>
2026-01-27 18:01:07 +1100

    Rename BrowserDaxDialogProvider to ContextualDaxDialogProvider

--- iOS/DuckDuckGo/UICoordination/MainCoordinator.swift
+++ iOS/DuckDuckGo/UICoordination/MainCoordinator.swift
@@ -102,7 +102,7 @@ final class MainCoordinator {
         let tabsModel = try Self.prepareTabsModel(previewsSource: previewsSource, tabsPersistence: tabsPersistence)
         let historyManager = try Self.makeHistoryManager(tabsModel: tabsModel)
         reportingService.subscriptionDataReporter.injectTabsModel(tabsModel)
-        let daxDialogsFactory = BrowserDaxDialogProvider(featureFlagger: featureFlagger,
+        let daxDialogsFactory = ContextualDaxDialogProvider(featureFlagger: featureFlagger,
                                                          contextualOnboardingLogic: daxDialogs,
                                                          contextualOnboardingPixelReporter: reportingService.onboardingPixelReporter)
         let contextualOnboardingPresenter = ContextualOnboardingPresenter(variantManager: variantManager, daxDialogsFactory: daxDialogsFactory)

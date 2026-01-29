16b3a93f177d135db260fbdc0392e0dfd8b290b3
Fernando Bunn <bunn@idevzilla.com>
2026-01-29 16:24:34 +0000

    Respect settings

--- iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/OmniBarEditingStateViewController.swift
+++ iOS/DuckDuckGo/AIChat/InputBox/SwitchBar/OmniBarEditingStateViewController.swift
@@ -147,7 +147,7 @@ final class OmniBarEditingStateViewController: UIViewController, OmniBarEditingS
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        if aiChatHistoryManager == nil && featureFlagger.isFeatureOn(.aiChatSuggestions) {
+        if aiChatHistoryManager == nil && featureFlagger.isFeatureOn(.aiChatSuggestions) && appSettings.autocomplete {
             installChatHistoryList()
         }
 

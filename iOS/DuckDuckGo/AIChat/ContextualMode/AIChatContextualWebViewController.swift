b0cbbc21dbd2c10f87d62c36c3d46dd23da4e895
Pete <petes44@icloud.com>
2026-02-01 10:23:00 +0000

    Remove keyboard debug logging

--- iOS/DuckDuckGo/AIChat/ContextualMode/AIChatContextualWebViewController.swift
+++ iOS/DuckDuckGo/AIChat/ContextualMode/AIChatContextualWebViewController.swift
@@ -156,14 +156,11 @@ final class AIChatContextualWebViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        Logger.aiChat.debug("[Keyboard] viewWillAppear: Setting up keyboard observers")
         setupKeyboardObservers()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
-        Logger.aiChat.debug("[Keyboard] viewWillDisappear: Removing keyboard observers and resetting constraint from \(self.webViewBottomConstraint?.constant ?? 0) to 0")
 
         removeKeyboardObservers()
         webViewBottomConstraint?.constant = 0
@@ -217,13 +214,9 @@ final class AIChatContextualWebViewController: UIViewController {
         let wasInMediumDetent = self.isMediumDetent
         self.isMediumDetent = isMediumDetent
 
-        Logger.aiChat.debug("[Keyboard] setMediumDetent: \(isMediumDetent) (was: \(wasInMediumDetent))")
-
         if isMediumDetent && !wasInMediumDetent {
-            Logger.aiChat.debug("[Keyboard] Transitioning to medium detent, recalculating keyboard overlap")
             recalculateKeyboardOverlapIfNeeded()
         } else if !isMediumDetent && webViewBottomConstraint?.constant != 0 {
-            Logger.aiChat.debug("[Keyboard] Transitioning from medium detent, resetting bottom constraint from \(self.webViewBottomConstraint?.constant ?? 0)")
             UIView.animate(withDuration: 0.25) {
                 self.webViewBottomConstraint?.constant = 0
                 self.view.layoutIfNeeded()
@@ -233,10 +226,8 @@ final class AIChatContextualWebViewController: UIViewController {
 
     private func recalculateKeyboardOverlapIfNeeded() {
         guard let keyboardFrame = lastKnownKeyboardFrame else {
-            Logger.aiChat.debug("[Keyboard] recalculateKeyboardOverlapIfNeeded: No last known keyboard frame")
             return
         }
-        Logger.aiChat.debug("[Keyboard] recalculateKeyboardOverlapIfNeeded: Recalculating with frame \(String(describing: keyboardFrame))")
         adjustForKeyboard(frame: keyboardFrame, duration: 0.25, options: .curveEaseInOut)
     }
 
@@ -293,19 +284,14 @@ final class AIChatContextualWebViewController: UIViewController {
     }
 
     @objc private func keyboardWillChangeFrame(_ notification: Notification) {
-        Logger.aiChat.debug("[Keyboard] keyboardWillChangeFrame called")
-
         guard let userInfo = notification.userInfo,
               let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
-            Logger.aiChat.debug("[Keyboard] No keyboard frame in notification, returning")
             return
         }
 
-        Logger.aiChat.debug("[Keyboard] Keyboard frame: \(String(describing: keyboardFrame))")
         lastKnownKeyboardFrame = keyboardFrame
 
         guard isMediumDetent, view.window != nil else {
-            Logger.aiChat.debug("[Keyboard] Skipping adjustment: isMediumDetent=\(self.isMediumDetent), hasWindow=\(self.view.window != nil)")
             return
         }
 
@@ -313,16 +299,13 @@ final class AIChatContextualWebViewController: UIViewController {
         let animationCurveRaw = (userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber)?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
         let animationCurve = UIView.AnimationOptions(rawValue: animationCurveRaw)
 
-        Logger.aiChat.debug("[Keyboard] Adjusting for keyboard with duration: \(duration)")
         adjustForKeyboard(frame: keyboardFrame, duration: duration, options: animationCurve)
     }
 
     @objc private func keyboardWillHide(_ notification: Notification) {
-        Logger.aiChat.debug("[Keyboard] keyboardWillHide called")
         lastKnownKeyboardFrame = nil
 
         guard view.window != nil else {
-            Logger.aiChat.debug("[Keyboard] No window, skipping hide")
             return
         }
 
@@ -331,7 +314,6 @@ final class AIChatContextualWebViewController: UIViewController {
         let animationCurveRaw = (userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? NSNumber)?.uintValue ?? UIView.AnimationOptions.curveEaseInOut.rawValue
         let animationCurve = UIView.AnimationOptions(rawValue: animationCurveRaw)
 
-        Logger.aiChat.debug("[Keyboard] Resetting bottom constraint to 0 with duration: \(duration)")
         webViewBottomConstraint?.constant = 0
 
         UIView.animate(withDuration: duration, delay: 0, options: animationCurve) {
@@ -343,18 +325,13 @@ final class AIChatContextualWebViewController: UIViewController {
         let keyboardFrameInView = view.convert(keyboardFrame, from: nil)
         let keyboardOverlap = max(0, view.bounds.height - keyboardFrameInView.origin.y)
 
-        Logger.aiChat.debug("[Keyboard] adjustForKeyboard: keyboardFrameInView=\(String(describing: keyboardFrameInView)), viewBounds=\(String(describing: self.view.bounds)), overlap=\(keyboardOverlap)")
-
         // Guard against invalid keyboard positions during sheet animation transitions
         // If keyboard is above the view (negative Y) or overlap exceeds view height, skip adjustment
         if keyboardFrameInView.origin.y < 0 || keyboardOverlap > view.bounds.height {
-            Logger.aiChat.debug("[Keyboard] Invalid keyboard position detected (likely during transition), skipping adjustment")
             return
         }
 
         webViewBottomConstraint?.constant = -keyboardOverlap
-
-        Logger.aiChat.debug("[Keyboard] Setting bottom constraint to \(-keyboardOverlap)")
 
         UIView.animate(withDuration: duration, delay: 0, options: options) {
             self.view.layoutIfNeeded()

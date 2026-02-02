3ed3ea924984dff918d47f1add81ed3dfe30a362
Michal Smaga <msmaga@duckduckgo.com>
2026-02-02 09:36:44 +0100

    Disable loading installed extension on startup on iOS

--- iOS/DuckDuckGo/UICoordination/MainCoordinator.swift
+++ iOS/DuckDuckGo/UICoordination/MainCoordinator.swift
@@ -222,7 +222,9 @@ final class MainCoordinator {
             controller.setWebExtensionEventsCoordinator(webExtensionEventsCoordinator)
             controller.setWebExtensionManager(webExtensionManager)
             Task { @MainActor in
-                await webExtensionManager.loadInstalledExtensions()
+                // FIXME: Currently loading installed extensions does not work on iOS do to absolute paths being unstable
+                webExtensionManager.uninstallAllExtensions()
+//                await webExtensionManager.loadInstalledExtensions()
             }
         } else {
             self.webExtensionManager = nil

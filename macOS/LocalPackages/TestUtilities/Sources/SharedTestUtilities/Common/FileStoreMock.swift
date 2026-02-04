cdd471c0ed1d406bee538996ae9e1be2de6950ce
DDG_HanyuTang <htang@duckduckgo.com>
2026-02-04 11:11:39 +0000

    Fix tests

--- macOS/LocalPackages/TestUtilities/Sources/SharedTestUtilities/Common/FileStoreMock.swift
+++ macOS/LocalPackages/TestUtilities/Sources/SharedTestUtilities/Common/FileStoreMock.swift
@@ -97,7 +97,12 @@ public final class FileStoreMock: NSObject {
         storage[url.lastPathComponent] = nil
     }
 
-    public func removeOrThrow(fileAtURL url: URL) throws {}
+    public func removeOrThrow(fileAtURL url: URL) throws {
+        if let failWithError {
+            throw failWithError
+        }
+        storage[url.lastPathComponent] = nil
+    }
 
     public func move(fileAt from: URL, to: URL) {
         guard storage[to.lastPathComponent] == nil,

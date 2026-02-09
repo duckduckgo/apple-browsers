95fe1ee77fef817fe46b505edd5bfdac5a10fc3f
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-02-09 15:25:56 +0100

    Add can_update parameter to daily os version pixel

--- macOS/DuckDuckGo/Statistics/ATB/StatisticsLoader.swift
+++ macOS/DuckDuckGo/Statistics/ATB/StatisticsLoader.swift
@@ -327,7 +327,8 @@ final class StatisticsLoader {
 
         DispatchQueue.global().asyncAfter(deadline: .now() + randomDelay) {
             PixelKit.fire(GeneralPixel.dailyOsVersionCounter,
-                          frequency: .legacyDailyNoSuffix)
+                          frequency: .legacyDailyNoSuffix,
+                          withAdditionalParameters: ["can_update": String(SupportedOSChecker().hardwareSupportsNewerOS)])
         }
     }
 

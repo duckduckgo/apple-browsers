9de9754589cdbbee531891a995bb5e3f15da8f6b
Alessandro Boron <aboron@duckduckgo.com>
2026-02-13 16:21:37 +1100

    Fix secondary button fill color

--- SharedPackages/Onboarding/Sources/Onboarding/Rebranding/iOS/Theme/OnboardingTheme-iOS.swift
+++ SharedPackages/Onboarding/Sources/Onboarding/Rebranding/iOS/Theme/OnboardingTheme-iOS.swift
@@ -53,7 +53,7 @@ public extension OnboardingTheme {
             optionsListTextColor: Color(singleUseColor: .rebranding(.textLink)),
             primaryButtonBackgroundColor: Color(singleUseColor: .rebranding(.buttonsPrimaryDefault)),
             primaryButtonTextColor: Color(singleUseColor: .rebranding(.buttonsPrimaryText)),
-            secondaryButtonBackgroundColor: Color(singleUseColor: .rebranding(.buttonsSecondaryDefault)),
+            secondaryButtonBackgroundColor: Color(singleUseColor: .rebranding(.controlsFillPrimary)),
             secondaryButtonTextColor: Color(singleUseColor: .rebranding(.buttonsSecondaryText))
         )
         let bubbleMetrics = BubbleMetrics(

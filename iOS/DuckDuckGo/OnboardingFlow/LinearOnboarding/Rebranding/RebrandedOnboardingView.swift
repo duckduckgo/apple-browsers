e5571c79d9f78aa42d3e5ae89aebf17fbe39f910
Alessandro Boron <aboron@duckduckgo.com>
2026-02-15 17:33:43 +1100

    Rasterize the whole background to improve animation performace

--- iOS/DuckDuckGo/OnboardingFlow/LinearOnboarding/Rebranding/RebrandedOnboardingView.swift
+++ iOS/DuckDuckGo/OnboardingFlow/LinearOnboarding/Rebranding/RebrandedOnboardingView.swift
@@ -636,6 +636,7 @@ struct ScrollableOnboardingBackground: View {
                 .frame(maxHeight: 410)
         }
         .ignoresSafeArea()
+        .drawingGroup()  // Rasterizes layer for better animation performance
     }
 
 }

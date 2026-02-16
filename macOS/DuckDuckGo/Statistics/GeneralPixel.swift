507490f275c1d2820dcd23de7ef897a68890f91d
amddg44 <amallon@duckduckgo.com>
2026-02-16 16:39:36 +0100

    Add syncAiChatTokenRequestedDaily to macOS standardParams

--- macOS/DuckDuckGo/Statistics/GeneralPixel.swift
+++ macOS/DuckDuckGo/Statistics/GeneralPixel.swift
@@ -1668,6 +1668,7 @@ enum GeneralPixel: PixelKitEvent {
                 .syncDuckAddressOverride,
                 .syncSuccessRateDaily,
                 .syncLocalTimestampResolutionTriggered,
+                .syncAiChatTokenRequestedDaily,
                 .syncBookmarksObjectLimitExceededDaily,
                 .syncCredentialsObjectLimitExceededDaily,
                 .syncCreditCardsObjectLimitExceededDaily,

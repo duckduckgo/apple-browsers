e75cb92e79ecca92adc5e8332e294e2863b52119
Fernando Bunn <bunn@idevzilla.com>
2026-02-17 11:14:36 +0000

    Fix toggle popover

--- macOS/DuckDuckGo/SecureVault/StatusBar/PasswordsStatusBarMenu.swift
+++ macOS/DuckDuckGo/SecureVault/StatusBar/PasswordsStatusBarMenu.swift
@@ -83,7 +83,7 @@ final class PasswordsStatusBarMenu: NSObject {
     // MARK: - Popover
 
     private func togglePopover() {
-        if let popover {
+        if let popover, popover.isShown {
             popover.close()
             self.popover = nil
         } else {

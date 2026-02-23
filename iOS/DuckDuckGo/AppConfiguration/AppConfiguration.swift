564cbbd1542ba9e69e9c27a68fdf44daf1ba0354
Federico Cappelli <kap.posta@gmail.com>
2026-02-23 09:39:29 +0000

    cleanup

--- iOS/DuckDuckGo/AppConfiguration/AppConfiguration.swift
+++ iOS/DuckDuckGo/AppConfiguration/AppConfiguration.swift
@@ -27,8 +27,6 @@ import WebKit
 
 struct AppConfiguration {
 
-    private let featureFlagger = AppDependencyProvider.shared.featureFlagger
-
     let atbAndVariantConfiguration = ATBAndVariantConfiguration()
     let persistentStoresConfiguration = PersistentStoresConfiguration()
     let onboardingConfiguration = OnboardingConfiguration()

e9b97903f9130ef69f3ee3bfbd983cebdd978dc1
Rachel McRoberts <rmcroberts@duckduckgo.com>
2026-03-05 19:59:57 +0000

    Migrate Next Steps to PromoService

--- macOS/DuckDuckGo/Promotions/PromoServiceFactory.swift
+++ macOS/DuckDuckGo/Promotions/PromoServiceFactory.swift
@@ -44,8 +44,8 @@ struct PromoServiceFactory {
     private static func makeAllPromos(dependencies: PromoDependencies) -> [Promo] {
         var promos: [Promo] = [
             remoteMessageNewTabPage(model: dependencies.activeRemoteMessageModel),
-            remoteMessageTabBar(model: dependencies.activeRemoteMessageModel)
-        ]
+            remoteMessageTabBar(model: dependencies.activeRemoteMessageModel),
+        ] + makeNextStepsPromos()
 
         if includeTestPromos {
             promos.append(contentsOf: testPromos)

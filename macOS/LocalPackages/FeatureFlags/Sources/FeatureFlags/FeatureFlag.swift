c12ae2ac511129a318026adb53b5c219cc5d93c7
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-03-05 17:00:29 -0300

    Enables startupMetrics Feature Flag

--- macOS/LocalPackages/FeatureFlags/Sources/FeatureFlags/FeatureFlag.swift
+++ macOS/LocalPackages/FeatureFlags/Sources/FeatureFlags/FeatureFlag.swift
@@ -307,7 +307,8 @@ extension FeatureFlag: FeatureFlagDescribing {
                 .aiChatSidebarResizable,
                 .aiChatSidebarFloating,
                 .nextStepsListWidget,
-                .webViewLookUpAction:
+                .webViewLookUpAction,
+                .startupMetrics:
             true
         default:
             false
@@ -567,7 +568,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .aiChatSidebarFloating:
             return .internalOnly()
         case .startupMetrics:
-            return .internalOnly()
+            return .remoteReleasable(.subfeature(MacOSBrowserConfigSubfeature.startupMetrics))
         case .privateProcessName:
             return .disabled
         case .webViewLookUpAction:

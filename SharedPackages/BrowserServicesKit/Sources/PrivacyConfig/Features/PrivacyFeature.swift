6b83d00aa37ade3fda269593b0060dcfc58aa497
Fernando Bunn <bunn@idevzilla.com>
2026-03-05 17:37:03 +0000

    add comment

--- SharedPackages/BrowserServicesKit/Sources/PrivacyConfig/Features/PrivacyFeature.swift
+++ SharedPackages/BrowserServicesKit/Sources/PrivacyConfig/Features/PrivacyFeature.swift
@@ -376,6 +376,7 @@ public enum AIChatSubfeature: String, Equatable, PrivacySubfeature {
     /// Signals that the iPad app should display duck.ai chats in a tab instead of a sheet
     case iPadDuckaiOnTab
 
+    /// Signals that the iPad app should display the duck.ai toggle
     case iPadAIChatToggle
 
     /// Controls deletion of Synced chats

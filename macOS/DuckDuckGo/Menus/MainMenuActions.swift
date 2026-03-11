391d4bdbc2e7fe00929ca93a75fac4c532d60162
Jacek Łyp <jlyp@duckduckgo.com>
2026-03-11 11:50:30 +0100

    Reuse Duck.ai chrome visibility manager in menu actions

--- macOS/DuckDuckGo/Menus/MainMenuActions.swift
+++ macOS/DuckDuckGo/Menus/MainMenuActions.swift
@@ -1274,13 +1274,11 @@ extension MainViewController {
 
     @objc func toggleDuckAIChromeButtonVisibility(_ sender: Any?) {
         guard featureFlagger.isFeatureOn(.chromeSidebar) else { return }
-        let duckAIChromeButtonsVisibilityManager = LocalDuckAIChromeButtonsVisibilityManager()
         duckAIChromeButtonsVisibilityManager.toggleVisibility(for: .duckAI)
     }
 
     @objc func toggleDuckAIChromeSidebarButtonVisibility(_ sender: Any?) {
         guard featureFlagger.isFeatureOn(.chromeSidebar) else { return }
-        let duckAIChromeButtonsVisibilityManager = LocalDuckAIChromeButtonsVisibilityManager()
         duckAIChromeButtonsVisibilityManager.toggleVisibility(for: .sidebar)
     }
 

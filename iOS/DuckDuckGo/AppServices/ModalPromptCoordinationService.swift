5077b321f58a38ecea37ca593855af1d2cb3a04a
Sabrina Tardio <stardio@duckduckgo.com>
2026-03-23 10:04:10 +0000

    fix logginh

--- iOS/DuckDuckGo/AppServices/ModalPromptCoordinationService.swift
+++ iOS/DuckDuckGo/AppServices/ModalPromptCoordinationService.swift
@@ -115,13 +115,15 @@ final class ModalPromptCoordinationService {
 
         Logger.modalPrompt.info("[Modal Prompt Coordination] - ✓ App Launched from standard source.")
         Logger.modalPrompt.info("[Modal Prompt Coordination] - ✓ Onboarding has been seen.")
+        let presentationStatusMessage: String
         if isOmniBarEditing {
-            Logger.modalPrompt.info("[Modal Prompt Coordination] - ✓ OmniBar editing sheet is presented; evaluating modal prompts (may present on top).")
+            presentationStatusMessage = "OmniBar editing sheet is presented; evaluating modal prompts."
         } else if presented?.isBeingDismissed == true {
-            Logger.modalPrompt.info("[Modal Prompt Coordination] - ✓ A modal is being dismissed; evaluating modal prompts.")
+            presentationStatusMessage = "A modal is being dismissed; evaluating modal prompts."
         } else {
-            Logger.modalPrompt.info("[Modal Prompt Coordination] - ✓ No modal is currently presented.")
+            presentationStatusMessage = "No Modal is currently presented."
         }
+        Logger.modalPrompt.info("[Modal Prompt Coordination] - ✓ \(presentationStatusMessage, privacy: .public)")
         modalPromptCoordinationManager.presentModalPromptIfNeeded(from: viewController)
     }
 

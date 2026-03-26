e15722fc9143391449bd1ddb9b6bb145751eecf2
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2026-03-26 18:00:48 +0100

    Use verbatim initializer for non-translated debug strings

--- iOS/DuckDuckGo/DebugScreensViewController.swift
+++ iOS/DuckDuckGo/DebugScreensViewController.swift
@@ -50,7 +50,7 @@ struct DebugScreensView: View {
                 Section {
                     VStack(alignment: .leading, spacing: 8) {
                         Label("Shake your device or press \u{2303}\u{2318}Z in the Simulator to open this screen quickly.", systemImage: "info.circle")
-                        Text("On App Store builds, this screen will only show if you're signed in as an internal user via **use-login.duckduckgo.com**.")
+                        Text(verbatim: "On App Store builds, this screen will only show if you're signed in as an internal user via **use-login.duckduckgo.com**.")
                     }
                     .font(.subheadline)
                     .foregroundStyle(.secondary)

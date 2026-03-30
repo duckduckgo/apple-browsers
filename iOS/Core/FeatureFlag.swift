b696fa58b0f095fe9c3a8bf67cb0043e31d0137c
Anh Do <ado@duckduckgo.com>
2026-03-30 09:30:27 -0400

    Defaults continued processing to false

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -375,7 +375,6 @@ extension FeatureFlag: FeatureFlagDescribing {
              .newDeviceSyncPrompt,
              .dbpForegroundRunningOnAppActive,
              .dbpForegroundRunningWhenDashboardOpen,
-             .dbpContinuedProcessing,
              .syncCreditCards,
              .unifiedURLPredictor,
              .migrateKeychainAccessibility,

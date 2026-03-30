0bdf050b685190c5d4f0d074b097660e58bb5d68
pikorddg <lpikor@duckduckgo.com>
2026-03-30 13:11:24 +0200

    Update copy

--- iOS/DuckDuckGo/AppIntents/AIVoiceChatIntent.swift
+++ iOS/DuckDuckGo/AppIntents/AIVoiceChatIntent.swift
@@ -23,7 +23,7 @@ import Core
 
 @available(iOS 17.0, *)
 struct AIVoiceChatIntent: AppIntent {
-    static var title: LocalizedStringResource = "Start a voice chat with Duck.ai on DuckDuckGo"
+    static var title: LocalizedStringResource = "Start a voice chat with Duck.ai in DuckDuckGo"
     static let description: LocalizedStringResource = "Start a private voice chat with Duck.ai"
     static let openAppWhenRun: Bool = true
     static let isDiscoverable: Bool = true

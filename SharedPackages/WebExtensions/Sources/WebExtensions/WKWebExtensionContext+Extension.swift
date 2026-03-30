c9fe48603a79dca990cd5ad1467d76fd1b996f23
Michal Smaga <msmaga@duckduckgo.com>
2026-03-30 13:41:29 +0200

    Retain proper extension id

--- SharedPackages/WebExtensions/Sources/WebExtensions/WKWebExtensionContext+Extension.swift
+++ SharedPackages/WebExtensions/Sources/WebExtensions/WKWebExtensionContext+Extension.swift
@@ -30,7 +30,7 @@ public enum DuckDuckGoWebExtensionType: String, Codable, CaseIterable {
     /// Embedded web extension (e.g. autoconsent/CPM).
     case embedded = "com.duckduckgo.web-extension.embedded"
     case darkReader = "org.duckduckgo.web-extension.darkreader"
-    case adBlockingExtension = "com.duckduckgo.web-extension.substitution" // TODO: "com.duckduckgo.content-blocker-extension"
+    case adBlockingExtension = "com.duckduckgo.content-blocker-extension"
 }
 
 /// Metadata extracted from a web extension without loading it into a controller.

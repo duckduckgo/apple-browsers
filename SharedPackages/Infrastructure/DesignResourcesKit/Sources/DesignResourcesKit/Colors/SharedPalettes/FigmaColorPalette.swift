36c283d17ca2f04487f85463d00e1bc303530913
Sabrina Tardio <stardio@duckduckgo.com>
2026-03-31 15:31:41 +0200

    Use RebrandingColor.Lilypad for shieldPrivacy color

--- SharedPackages/Infrastructure/DesignResourcesKit/Sources/DesignResourcesKit/Colors/SharedPalettes/FigmaColorPalette.swift
+++ SharedPackages/Infrastructure/DesignResourcesKit/Sources/DesignResourcesKit/Colors/SharedPalettes/FigmaColorPalette.swift
@@ -99,7 +99,7 @@ struct FigmaColorPalette: SharedColorPaletteDefinition {
     static let alertGreen = DynamicColor(lightColor: .alertGreen, darkColor: .alertGreen)
     static let alertYellow = DynamicColor(lightColor: .alertYellow, darkColor: .alertYellow)
 
-    static let shieldPrivacy = DynamicColor(lightColor: .lilypad70, darkColor: .lilypad50)
+    static let shieldPrivacy = DynamicColor(lightColor: RebrandingColor.Lilypad.lilypad70, darkColor: RebrandingColor.Lilypad.lilypad50)
 
     static let buttonsWhite = DynamicColor(lightColor: .white, darkColor: .black)
 

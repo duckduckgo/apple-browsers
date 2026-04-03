ade7b56facdac3bac57e2089b93ada7ab22ee119
Sam Symons <sam@samsymons.com>
2026-04-03 12:50:33 -0700

    Disable flaky test.

--- macOS/UITests/AutoconsentUITests.swift
+++ macOS/UITests/AutoconsentUITests.swift
@@ -105,6 +105,8 @@ class AutoconsentUITests: UITestCase {
     }
 
     func testAutoconsent_CookieBannerHiding_BannersAreHidden() throws {
+        throw XCTSkip("Flaky test - uses cosmetic copy on CI and in release builds, whereas Debug builds don't")
+
         // Navigate to test page with cookie banner
         let bannerTestURL = URL(string: "http://privacy-test-pages.site/features/autoconsent/banner.html")!
         addressBarTextField.pasteURL(bannerTestURL, pressingEnter: true)

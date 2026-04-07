a6a27ed3f8117361f74550f14c26568751ad8058
Hassaan Elgarem <helgarem@duckduckgo.com>
2026-04-07 17:53:35 +0200

    Use proper order for creating and resolving fire mode capability

--- iOS/DuckDuckGo/UICoordination/MainCoordinator.swift
+++ iOS/DuckDuckGo/UICoordination/MainCoordinator.swift
@@ -126,6 +126,7 @@ final class MainCoordinator {
         self.wideEvent = wideEvent
         self.voiceSessionStateManager = VoiceSessionStateManager()
         self.voiceShortcutFeature = DuckAIVoiceShortcutFeature(featureFlagger: featureFlagger)
+        FireModeCapability.resolve(using: featureFlagger)
         let fireModeCapability = FireModeCapability.create()
         let fireModePromotionsCoordinator = FireModePromotionsCoordinator(fireModeCapability: fireModeCapability)
         let homePageConfiguration = HomePageConfiguration(variantManager: AppDependencyProvider.shared.variantManager,
@@ -133,7 +134,6 @@ final class MainCoordinator {
                                                           subscriptionDataReporter: reportingService.subscriptionDataReporter,
                                                           fireModePromotionEligibility: fireModePromotionsCoordinator,
                                                           isStillOnboarding: { daxDialogsManager.isStillOnboarding() })
-        FireModeCapability.resolve(using: featureFlagger)
         let previewsSource = DefaultTabPreviewsSource()
         let tabsPersistence = try TabsModelPersistence()
         let tabsModelProvider = try Self.prepareTabsModel(previewsSource: previewsSource, tabsPersistence: tabsPersistence)

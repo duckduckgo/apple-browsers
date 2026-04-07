33081d2d7965bec935c744ed4b006a27bc35a846
Pete <petes44@icloud.com>
2026-04-07 14:26:34 +0100

    fix: resolve UTI status background ordering

--- iOS/DuckDuckGo/MainViewCoordinator.swift
+++ iOS/DuckDuckGo/MainViewCoordinator.swift
@@ -152,9 +152,7 @@ class MainViewCoordinator {
     }
 
     func hideNavigationBarWithBottomPosition() {
-        guard addressBarPosition.isBottom else {
-            return
-        }
+        guard addressBarPosition.isBottom else { return }
 
         navigationBarContainer.isHidden = true
 
@@ -162,9 +160,7 @@ class MainViewCoordinator {
     }
 
     func showNavigationBarWithBottomPosition() {
-        guard addressBarPosition.isBottom else {
-            return
-        }
+        guard addressBarPosition.isBottom else { return }
 
         navigationBarContainer.isHidden = false
 
@@ -425,7 +421,7 @@ class MainViewCoordinator {
     private func resolvedStatusBackgroundColor() -> UIColor {
         switch statusBackgroundPresentation {
         case .standard:
-            standardStatusBackgroundColor ?? statusBackground.backgroundColor ?? UIColor(designSystemColor: .background)
+            standardStatusBackgroundColor ?? UIColor(designSystemColor: .background)
         case .omnibarEditing, .aiTabSearchChromeHidden:
             UIColor(designSystemColor: .panel)
         case .aiTabChatChromeHidden:

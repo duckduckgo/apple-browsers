a6db9b9f1da26e4cdd73b9f7a9c4b6be5d9cac3b
Eduardo Pinto <edulpn@gmail.com>
2026-04-08 16:31:49 -0400

    Fix progress bar bug

--- iOS/DuckDuckGo/WebProgressWorker.swift
+++ iOS/DuckDuckGo/WebProgressWorker.swift
@@ -20,26 +20,28 @@
 import UIKit
 
 class WebProgressWorker {
-    
+
     private struct Constants {
         static let initialProgress: CGFloat = 0.1
     }
-    
+
     weak var progressBar: ProgressView? {
         didSet(oldValue) {
             oldValue?.hide()
-            
+
             if isLoading {
-                self.progressBar?.show(initialProgress: currentProgress)
+                // Cap initial progress below 1.0 so finishAndHide can always animate to completion
+                let cappedProgress = min(currentProgress, 0.9)
+                self.progressBar?.show(initialProgress: cappedProgress)
             } else {
                 self.progressBar?.hide()
             }
         }
     }
-    
+
     private var isLoading = false
     private var currentProgress: CGFloat = 0.0
-    
+
     func didStartLoading() {
         guard isLoading == false else { return }
 
@@ -47,17 +49,17 @@ class WebProgressWorker {
         progressBar?.show()
         progressBar?.increaseProgress(to: Constants.initialProgress, animated: true)
     }
-    
+
     func progressDidChange(_ progress: Double) {
         guard isLoading else { return }
-        
+
         let progress = CGFloat(progress)
         guard progress > currentProgress else { return }
         currentProgress = progress
-        
+
         progressBar?.increaseProgress(to: progress, animated: true)
     }
-    
+
     func didFinishLoading() {
         guard isLoading else { return }
         isLoading = false

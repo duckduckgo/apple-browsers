edc71ac28d490664eedb824b08a3521e96e5beee
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-04-09 14:10:34 -0300

    Updates Autoplay strings

--- macOS/DuckDuckGo/Preferences/View/PreferencesGeneralView.swift
+++ macOS/DuckDuckGo/Preferences/View/PreferencesGeneralView.swift
@@ -335,6 +335,7 @@ extension Preferences {
                                     }
                                 }
                             }
+                            TextMenuItemCaption(UserText.autoplayCaption)
                         }
                     }
                 }

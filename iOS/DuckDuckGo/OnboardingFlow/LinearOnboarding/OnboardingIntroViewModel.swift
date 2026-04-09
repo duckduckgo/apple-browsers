a8dab7dce00da1f15b3b57240d419697dd76752d
Alexey Martemyanov <mallexxx@gmail.com>
2026-04-09 23:38:34 +0600

    set control treatment by default

--- iOS/DuckDuckGo/OnboardingFlow/LinearOnboarding/OnboardingIntroViewModel.swift
+++ iOS/DuckDuckGo/OnboardingFlow/LinearOnboarding/OnboardingIntroViewModel.swift
@@ -441,7 +441,7 @@ private extension OnboardingIntroViewModel {
         guard featureFlagger.isFeatureOn(.onboardingDuckAIQueryExperiment) else { return nil }
         // TODO: Temporary override for dev validation; remove once remote cohort mapping is finalized.
         let isRunningTests = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
-        if !isRunningTests { return .treatmentA }
+        if !isRunningTests { return .control }
         return featureFlagger.resolveCohort(for: FeatureFlag.onboardingDuckAIQueryExperiment) as? FeatureFlag.DuckAIQueryExperimentCohort
     }
 

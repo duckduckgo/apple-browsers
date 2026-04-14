db45b34606457637c7ac535684805bb10f55b94b
Michal Smaga <msmaga@duckduckgo.com>
2026-04-14 17:30:19 +0100

    Attempt fixing UI test

--- macOS/UITests/ContentScopeExperimentsEndToEndTests.swift
+++ macOS/UITests/ContentScopeExperimentsEndToEndTests.swift
@@ -23,6 +23,7 @@ final class ContentScopeExperimentsEndToEndTests: UITestCase {
     func testContentScopeExperiments() throws {
         let app = XCUIApplication.setUp()
         app.openNewTab()
+        skipOnboarding(app)
 
         // Step 1: Load custom remote config
         let menuBarsQuery = app.menuBars
@@ -62,4 +63,12 @@ final class ContentScopeExperimentsEndToEndTests: UITestCase {
         XCTAssertTrue(suiteStatusValue.waitForExistence(timeout: UITests.Timeouts.elementExistence), "Test Suite Status Value not pass")
     }
 
+    private func skipOnboarding(_ app: XCUIApplication) {
+        let skipOnboardingMenuItem = app.menuItems["MainMenu.skipOnboarding"]
+        XCTAssertTrue(
+            skipOnboardingMenuItem.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "Skip onboarding menu item didn't become available in a reasonable timeframe."
+        )
+        skipOnboardingMenuItem.click()
+    }
 }

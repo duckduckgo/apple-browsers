85a33a224343ae7e3655c504fba3b1b7b0fd334f
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-04-17 07:56:59 +0100

    Default to nil channel in DBP Agent if user defaults fail to initialize

--- macOS/DuckDuckGoDBPBackgroundAgent/DuckDuckGoDBPBackgroundAgentAppDelegate.swift
+++ macOS/DuckDuckGoDBPBackgroundAgent/DuckDuckGoDBPBackgroundAgentAppDelegate.swift
@@ -37,9 +37,13 @@ final class DuckDuckGoDBPBackgroundAgentApplication: NSApplication {
 
     override init() {
         Logger.dbpBackgroundAgent.log("🟢 Starting: \(NSRunningApplication.current.processIdentifier, privacy: .public)")
-        let appConfigDefaults = UserDefaults(suiteName: Bundle.main.appGroup(bundle: .appConfiguration))
-        let internalUserDecider = DefaultInternalUserDecider(store: appConfigDefaults ?? .standard)
-        let channel = StandardApplicationBuildType().channelName(isInternalUser: internalUserDecider.isInternalUser)
+        let channel: String? = {
+            guard let appConfigDefaults = UserDefaults(suiteName: Bundle.main.appGroup(bundle: .appConfiguration)) else {
+                return nil
+            }
+            let internalUserDecider = DefaultInternalUserDecider(store: appConfigDefaults)
+            return StandardApplicationBuildType().channelName(isInternalUser: internalUserDecider.isInternalUser)
+        }()
         let userAgent = UserAgent.duckDuckGoUserAgent()
 
         PixelKit.setUp(dryRun: PixelKitConfig.isDryRun(isProductionBuild: BuildFlags.isProductionBuild),

68c5140f6d8b512abb8263b90178d2c8cca49e73
Alessandro Boron <aboron@duckduckgo.com>
2026-04-20 14:41:52 +1000

    Make tests compile

--- iOS/SharedTestUtils/Mocks/DuckDuckGo/OmniBar/MockOmniBar.swift
+++ iOS/SharedTestUtils/Mocks/DuckDuckGo/OmniBar/MockOmniBar.swift
@@ -141,6 +141,8 @@ final class MockOmniBar: OmniBar {
         var onCustomizableButtonPressed: (() -> Void)?
         var onAIChatLeftButtonPressed: (() -> Void)?
         var onAIChatBrandingPressed: (() -> Void)?
+        var onMenuButtonLongPressed: (() -> Void)?
+        var onSettingsButtonLongPressed: (() -> Void)?
 
         static func create() -> Self {
             Self.init()

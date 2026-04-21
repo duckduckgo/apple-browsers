6a80381094cdb237333c699081fd065ab3733647
Sabrina Tardio <stardio@duckduckgo.com>
2026-04-21 16:49:43 +0200

    Default aiChatContextualSheetImprovements feature flag to enabled

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -611,7 +611,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .appRatingPrompt:
             Config(defaultValue: .enabled, source: .remoteReleasable(.subfeature(iOSBrowserConfigSubfeature.appRatingPrompt)))
         case .contextualDuckAIMode:
-            Config(defaultValue: .enabled, source: .remoteReleasable(.subfeature(AIChatSubfeature.contextualDuckAIMode)))
+            Config(source: .remoteReleasable(.subfeature(AIChatSubfeature.contextualDuckAIMode)))
         case .pageContextFeature:
             Config(source: .remoteReleasable(.feature(.pageContext)))
         case .aiChatAutoAttachContextByDefault:
@@ -625,7 +625,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .aiChatSuggestions:
             Config(source: .remoteReleasable(.feature(.duckAiChatHistory)))
         case .aiChatContextualSheetImprovements:
-            Config(source: .remoteReleasable(.subfeature(AIChatSubfeature.contextualSheetImprovements)))
+            Config(defaultValue: .enabled, source: .remoteReleasable(.subfeature(AIChatSubfeature.contextualSheetImprovements)))
         case .showWhatsNewPromptOnDemand:
             Config(defaultValue: .enabled, source: .remoteReleasable(.subfeature(iOSBrowserConfigSubfeature.showWhatsNewPromptOnDemand)))
         case .unifiedToggleInput:

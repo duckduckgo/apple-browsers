a9223e0f992e015eed16458258b7472c2ce225d2
Alexey Martemyanov <mallexxx@gmail.com>
2026-04-28 09:24:15 +0600

    Fix unit test regressions: gate chat-path logic on isChatFirstPath flag, remove TODO override

--- iOS/DuckDuckGo/OnboardingFlow/LinearOnboarding/OnboardingIntroViewModel.swift
+++ iOS/DuckDuckGo/OnboardingFlow/LinearOnboarding/OnboardingIntroViewModel.swift
@@ -430,8 +430,6 @@ private extension OnboardingIntroViewModel {
     }
 
     func resolveDuckAIQueryExperimentCohortID() -> FeatureFlag.DuckAIQueryExperimentCohort? {
-        // TODO: Remove this
-        return .treatmentA
         guard featureFlagger.isFeatureOn(.onboardingDuckAIQueryExperiment) else { return nil }
         return featureFlagger.resolveCohort(for: FeatureFlag.onboardingDuckAIQueryExperiment) as? FeatureFlag.DuckAIQueryExperimentCohort
     }

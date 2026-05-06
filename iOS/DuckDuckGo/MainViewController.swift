50528699c364b28f2844319148fc69ab317b2d9d
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2026-05-06 17:06:53 +0200

    [iPad perf] Skip save + tabs bar refresh for non-current tabs
    Asana: https://app.asana.com/1/137249556945/project/1206226850447395/task/1214543872030689

--- iOS/DuckDuckGo/MainViewController.swift
+++ iOS/DuckDuckGo/MainViewController.swift
@@ -4783,10 +4783,9 @@ extension MainViewController: TabDelegate {
             }
         }
 
-        if currentTab == tab {
-            refreshControls()
-            themeColorManager.updateThemeColor()
-        }
+        guard currentTab == tab else { return }
+        refreshControls()
+        themeColorManager.updateThemeColor()
         _ = tabManager.save()
         tabsBarController?.refresh(tabsModel: tabManager.currentTabsModel)
         // note: model in swipeTabsCoordinator doesn't need to be updated here

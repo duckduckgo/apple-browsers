4b249cd09f40949bb370dbc96747490b1922e26d
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-05-18 23:46:15 +0200

    Fix broken build

--- macOS/DuckDuckGo/Sharing/QRSharingService.swift
+++ macOS/DuckDuckGo/Sharing/QRSharingService.swift
@@ -30,7 +30,7 @@ final class QRSharingService: NSSharingService {
     fileprivate var imageUrl: URL?
 
     fileprivate init() {
-        super.init(title: UserText.shareViaQRCodeMenuItem, image: .qrIcon), alternateImage: nil) {}
+        super.init(title: UserText.shareViaQRCodeMenuItem, image: .qrIcon, alternateImage: nil) {}
     }
 
     /// Get ASCII `Data` for an array of items to share that can be represented as strings (e.g., URLs or Strings).

1f923269b30d399f526501bda49178b294a5f675
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-05-19 08:19:30 +0200

    Add icons to menu actions (#4927)
    Task/Issue URL: https://app.asana.com/1/137249556945/project/1201899738287924/task/1212767937549553?focus=true
    
    Description:
    This change adds icons to various actions in main menu, application (more options) menu and context menus.

--- macOS/DuckDuckGo/NavigationBar/View/MoreOptionsMenu.swift
+++ macOS/DuckDuckGo/NavigationBar/View/MoreOptionsMenu.swift
@@ -181,7 +181,7 @@ final class MoreOptionsMenu: NSMenu, NSMenuDelegate {
         setupMenuItems()
     }
 
-    let zoomMenuItem = NSMenuItem(title: UserText.zoom, action: nil, keyEquivalent: "").withImage(.zoomIn)
+    let zoomMenuItem = NSMenuItem(title: UserText.zoom, action: nil, keyEquivalent: "").withImage(DesignSystemImages.Glyphs.Size12.zoomIn)
 
     @MainActor
     private func setupMenuItems() {
@@ -282,7 +282,7 @@ final class MoreOptionsMenu: NSMenu, NSMenuDelegate {
         let menuItem = MenuItemWithNotificationDot(leftImage: image, title: title, onTapMenuItem: onTap)
 
         let hostingView = NSHostingView(rootView: menuItem)
-        hostingView.frame = NSRect(x: 0, y: 0, width: size.width, height: 22)
+        hostingView.frame = NSRect(x: 0, y: 0, width: size.width, height: AppVersion.isLiquidGlassSupported ? 24 : 22)
         hostingView.autoresizingMask = [.width, .height]
 
         return hostingView
@@ -574,7 +574,7 @@ final class MoreOptionsMenu: NSMenu, NSMenuDelegate {
         if aiChatMenuConfiguration.shouldDisplayMoreOptionsMenuShortcut {
             let aiChatItem = NSMenuItem(title: "Duck.ai", action: #selector(newAiChat), keyEquivalent: "n")
             aiChatItem.keyEquivalentModifierMask = [.command, .option]
-            aiChatItem.image = DesignSystemImages.Glyphs.Size16.duckAi
+            aiChatItem.image = DesignSystemImages.Glyphs.Size12.duckAi
             aiChatItem.target = self
             aiChatItem.submenu = makeAIChatMenu()
             addItem(aiChatItem)
@@ -977,13 +977,13 @@ final class FeedbackSubMenu: NSMenu, NSMenuDelegate {
         let reportABrowserProblemItem = NSMenuItem(title: UserText.reportBrowserProblem,
                                                    action: #selector(AppDelegate.openReportABrowserProblem(_:)),
                                                    keyEquivalent: "")
-            .withImage(DesignSystemImages.Glyphs.Size16.feedbackAlert)
+            .withImage(DesignSystemImages.Glyphs.Size12.feedbackAlert)
         addItem(reportABrowserProblemItem)
 
         let requestANewFeatureItem = NSMenuItem(title: UserText.requestNewFeature,
                                                 action: #selector(AppDelegate.openRequestANewFeature(_:)),
                                                 keyEquivalent: "")
-            .withImage(DesignSystemImages.Glyphs.Size16.windowNew)
+            .withImage(DesignSystemImages.Glyphs.Size12.windowNew)
         addItem(requestANewFeatureItem)
     }
 
@@ -1148,6 +1148,7 @@ final class BookmarksSubMenu: NSMenu, NSMenuDelegate {
             .withModifierMask([.command])
             .targetting(target)
             .withAccessibilityIdentifier("MoreOptionsMenu.bookmarkPage")
+            .withImage(DesignSystemImages.Glyphs.Size12.bookmarkAdd)
 
         bookmarkPageItem.isEnabled = tabCollectionViewModel.selectedTabViewModel?.canBeBookmarked == true
 
@@ -1167,6 +1168,7 @@ final class BookmarksSubMenu: NSMenu, NSMenuDelegate {
         addItem(withTitle: UserText.bookmarksManageBookmarks, action: #selector(MoreOptionsMenu.openBookmarksManagementInterface), keyEquivalent: "b")
             .withModifierMask([.command, .option])
             .targetting(target)
+            .withImage(DesignSystemImages.Glyphs.Size12.bookmarks)
 
         addItem(NSMenuItem.separator())
 
@@ -1199,8 +1201,10 @@ final class BookmarksSubMenu: NSMenu, NSMenuDelegate {
 
         addItem(withTitle: UserText.importBookmarks, action: #selector(MoreOptionsMenu.openBookmarkImportInterface(_:)), keyEquivalent: "")
             .targetting(target)
+            .withImage(DesignSystemImages.Glyphs.Size12.import)
 
         let exportBookmarItem = NSMenuItem(title: UserText.exportBookmarks, action: #selector(MoreOptionsMenu.openBookmarkExportInterface(_:)), keyEquivalent: "").targetting(target)
+            .withImage(DesignSystemImages.Glyphs.Size12.export)
         exportBookmarItem.isEnabled = bookmarkManager.list?.totalBookmarks != 0
         addItem(exportBookmarItem)
 

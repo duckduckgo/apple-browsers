d3a9ca41831b5ec8d6b7408b8df53c11291e055f
Eduardo Pinto <edulpn@gmail.com>
2026-05-23 00:33:10 -0400

    Remove fireproofingETLDPlus1 feature flag and related pixels

--- SharedPackages/BrowserServicesKit/Sources/PrivacyConfig/Features/PrivacyFeature.swift
+++ SharedPackages/BrowserServicesKit/Sources/PrivacyConfig/Features/PrivacyFeature.swift
@@ -248,8 +248,6 @@ public enum iOSBrowserConfigSubfeature: String, PrivacySubfeature {
 
     case crashReportOptInStatusResetting
 
-    case fireproofingETLDPlus1
-
     case screenTimeCleaning
 
     case minimalChromeInLandscape

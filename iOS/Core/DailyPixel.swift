284ac6fa2c1df2642ef4702a59a19c32d76aa84e
Diego Rey Mendez <dreymendez@duckduckgo.com>
2026-05-25 17:10:28 +0200

    Revert "iOS: attach error params to _d in DailyPixel.fireDailyAndCount"
    This reverts commit 0ff278a59d4e4aad6ebe1f188e0455107823deca.

--- iOS/Core/DailyPixel.swift
+++ iOS/Core/DailyPixel.swift
@@ -103,17 +103,12 @@ public final class DailyPixel {
                                          onCountComplete: @escaping (Swift.Error?) -> Void = { _ in }) {
         let key: String = pixel.name
 
-        var newParams = params
-        if let error {
-            newParams.appendErrorPixelParams(error: error)
-        }
-
         if !hasBeenFiredToday(forKey: key, dailyPixelStore: dailyPixelStore) {
             do {
                 try updatePixelLastFireDate(forKey: key, dailyPixelStore: dailyPixelStore)
                 pixelFiring.fire(
                     pixelNamed: pixel.name + pixelNameSuffixes.dailySuffix,
-                    withAdditionalParameters: newParams,
+                    withAdditionalParameters: params,
                     includedParameters: includedParameters,
                     onComplete: onDailyComplete
                 )
@@ -125,6 +120,10 @@ public final class DailyPixel {
             }
         } else {
             onDailyComplete(Error.alreadyFired)
+        }
+        var newParams = params
+        if let error {
+            newParams.appendErrorPixelParams(error: error)
         }
         pixelFiring.fire(
             pixelNamed: pixel.name + pixelNameSuffixes.countSuffix,

62bd82b329b4af5095b99854b3e8fc79fe72d7b4
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-05-29 15:44:59 -0300

    Drops unused variable

--- macOS/DuckDuckGo/MainWindow/MainWindowController.swift
+++ macOS/DuckDuckGo/MainWindow/MainWindowController.swift
@@ -226,7 +226,7 @@ final class MainWindowController: NSWindowController {
 
     private var trafficLightsAlphaCancellables = [AnyCancellable]()
     private func subscribeToTrafficLightsAlpha() {
-        guard let window, let featureFlagger else {
+        guard let window else {
             return
         }
 

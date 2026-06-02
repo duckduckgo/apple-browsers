43b8be9b658fbd29ca8fa4604c1cc9df2278025c
Elle Sullivan <ellesailenesullivan@gmail.com>
2026-06-02 09:54:50 -0400

    Restore loading performance pixels

--- iOS/DuckDuckGo/TabViewController.swift
+++ iOS/DuckDuckGo/TabViewController.swift
@@ -195,6 +195,7 @@ class TabViewController: UIViewController {
     public var inferredOpenerContext: BrokenSiteReport.OpenerContext?
     private var refreshCountSinceLoad: Int = 0
     private var breakageReportingSubfeature: BreakageReportingSubfeature?
+    private var siteLoadingPerformanceSubfeature: SiteLoadingPerformanceSubfeature?
 
     private var detectedLoginURL: URL?
     private var fireproofingWorker: FireproofingWorking?
@@ -3478,6 +3479,9 @@ extension TabViewController: UserContentControllerDelegate {
         
         breakageReportingSubfeature = BreakageReportingSubfeature(targetWebview: webView)
         userScripts.contentScopeUserScriptIsolated.registerSubfeature(delegate: breakageReportingSubfeature!)
+
+        siteLoadingPerformanceSubfeature = SiteLoadingPerformanceSubfeature(targetWebview: webView)
+        userScripts.contentScopeUserScriptIsolated.registerSubfeature(delegate: siteLoadingPerformanceSubfeature!)
 
         adClickAttributionLogic.onRulesChanged(latestRules: ContentBlocking.shared.contentBlockingManager.currentRules)
         

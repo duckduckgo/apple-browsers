08aafa0d3a575c89aee5d2745ef7db3e385b6fb8
Hassaan Elgarem <helgarem@duckduckgo.com>
2026-06-05 13:40:02 +0300

    Defensive code for out of bounds crash in TabsBarViewController

--- iOS/Core/PixelEvent.swift
+++ iOS/Core/PixelEvent.swift
@@ -923,6 +923,7 @@ extension Pixel {
 
         case debugTabSwitcherDidChangeInvalidState
         case debugTabsModelCrossModeMismatch
+        case debugTabsBarCellIndexOutOfRange
 
         case debugBookmarksInitialStructureQueryFailed
         case debugBookmarksStructureLost
@@ -2727,6 +2728,7 @@ extension Pixel.Event {
 
         case .debugTabSwitcherDidChangeInvalidState: return "m_debug_tabswitcher_didchange_invalidstate"
         case .debugTabsModelCrossModeMismatch: return "m_debug_tabs-model_cross-mode-mismatch"
+        case .debugTabsBarCellIndexOutOfRange: return "m_debug_tabs-bar_cell-index-out-of-range"
 
         case .debugBookmarksMigratedMoreThanOnce: return "m_debug_bookmarks_migrated-more-than-once"
             

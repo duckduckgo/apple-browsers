db181ea9b8b9ef0c0c12e0debda54fd1329eb63e
Federico Cappelli <kap.posta@gmail.com>
2026-06-10 16:42:12 +0100

    PixelRetryQueueStorageError as DDGError

--- SharedPackages/BrowserServicesKit/Sources/PixelKit/RetryQueue/PixelRetryQueueStoring.swift
+++ SharedPackages/BrowserServicesKit/Sources/PixelKit/RetryQueue/PixelRetryQueueStoring.swift
@@ -17,6 +17,7 @@
 //
 
 import Foundation
+import Common
 
 /// Persists pixels awaiting retry. Implementations must be safe to call from multiple threads.
 protocol PixelRetryQueueStoring {
@@ -25,9 +26,48 @@ protocol PixelRetryQueueStoring {
     func storedItems() throws -> [PixelRetryQueueItem]
 }
 
-enum PixelRetryQueueStorageError: Error {
+enum PixelRetryQueueStorageError: DDGError {
     case readError(Error)
     case writeError(Error)
     case encodingError(Error)
     case decodingError(Error)
+
+    static func == (lhs: PixelRetryQueueStorageError, rhs: PixelRetryQueueStorageError) -> Bool {
+        switch (lhs, rhs) {
+        case (.readError(let lhsError), .readError(let rhsError)),
+            (.writeError(let lhsError), .writeError(let rhsError)),
+            (.encodingError(let lhsError), .encodingError(let rhsError)),
+            (.decodingError(let lhsError), .decodingError(let rhsError)):
+            return String(describing: lhsError) == String(describing: rhsError)
+        default:
+            return false
+        }
+    }
+
+    static var errorDomain: String { "com.duckduckgo.pixelkit.PixelRetryQueueStorageError" }
+
+    var errorCode: Int {
+        switch self {
+        case .readError: 13000
+        case .writeError: 13001
+        case .encodingError: 13002
+        case .decodingError: 13003
+        }
+    }
+
+    var underlyingError: Error? {
+        switch self {
+        case .readError(let error), .writeError(let error), .encodingError(let error), .decodingError(let error):
+            return error
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .readError: "Failed to read the pixel retry queue from disk"
+        case .writeError: "Failed to write the pixel retry queue to disk"
+        case .encodingError: "Failed to encode the pixel retry queue"
+        case .decodingError: "Failed to decode the pixel retry queue"
+        }
+    }
 }

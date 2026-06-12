18fb1ddfce624b5530aa477e34f9eef546c8cb33
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2026-06-12 15:33:18 +0200

    Fix navigation bar stuck hidden on error page

--- iOS/DuckDuckGo/MainViewController.swift
+++ iOS/DuckDuckGo/MainViewController.swift
@@ -3651,6 +3651,10 @@ extension MainViewController: BrowserChromeDelegate {
 
         setBarsVisibility(hidden ? 0 : 1.0, animated: animated, animationDuration: customAnimationDuration)
     }
+
+    func resetBars(animated: Bool) {
+        chromeManager.reset(animated: animated)
+    }
     
     func setBarsVisibility(_ percent: CGFloat, animated: Bool, animationDuration: CGFloat?) {
         if percent < 1 {
@@ -3724,6 +3728,8 @@ extension MainViewController: BrowserChromeDelegate {
     }
 
     var canHideBars: Bool {
+        // Keep bars shown on the error page: the webView is hidden, so scroll can't self-heal a stuck-hidden bar.
+        if currentTab?.isError == true { return false }
         return !daxDialogsManager.shouldShowFireButtonPulse
     }
 

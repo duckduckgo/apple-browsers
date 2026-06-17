2b30a76534912e4e53571028a67c7faa23259ae8
Sam Symons <sam@samsymons.com>
2026-06-17 14:19:17 -0700

    Add failsafe feature flag.

--- iOS/DuckDuckGo/TabManager.swift
+++ iOS/DuckDuckGo/TabManager.swift
@@ -755,6 +755,7 @@ class TabManager: TabManaging, TrackerAnimationSuppressing {
     }
 
     func cleanupTabsFaviconCache() {
+        guard featureFlagger.isFeatureOn(.staleFaviconCleanup) else { return }
         guard tabsCacheNeedsCleanup else { return }
 
         DispatchQueue.global(qos: .background).async { [weak self] in

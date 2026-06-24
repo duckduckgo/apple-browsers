d8c66a16a3398e52fd29439fc49256e6ab28a229
Chris Brind <brindy@duckduckgo.com>
2026-06-24 12:55:08 +0100

    fix bug in duck player that forced chrome to reappear

--- iOS/DuckDuckGo/DuckPlayer/DuckPlayer.swift
+++ iOS/DuckDuckGo/DuckPlayer/DuckPlayer.swift
@@ -845,7 +845,8 @@ final class DuckPlayer: NSObject, DuckPlayerControlling {
     }
 
     @objc private func handleChromeVisibilityChange(_ notification: Notification) {
-        if let url = hostView?.webView.url,
+        if hostView?.isTabCurrentlyPresented() == true,
+            let url = hostView?.webView.url,
             url.isYoutubeWatch,
             let isHidden = notification.userInfo?["isHidden"] as? Bool {
 

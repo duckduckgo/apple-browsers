d378214800cc6db96614a4404c841918ca07a98a
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-06-30 12:59:40 -0300

    AddressBar: Adjusts constraints

--- macOS/DuckDuckGo/VisualRefresh/AddressBarStyleProviding.swift
+++ macOS/DuckDuckGo/VisualRefresh/AddressBarStyleProviding.swift
@@ -231,7 +231,7 @@ final class RefreshAddressBarStyleProvider: AddressBarStyleProviding {
     private let addressBarBottomPaddingForDefault: CGFloat = 7
     private let addressBarBottomPaddingForDefaultFocused: CGFloat = 3
     private let addressBarBottomPaddingForPopUpWindow: CGFloat = 7
-    private let addressBarTrailingStackViewOmnibarPadding: CGFloat = -2
+    private let addressBarTrailingStackViewOmnibarPadding: CGFloat = 1
     private let addressBarTrailingStackViewFocusedPadding: CGFloat = 4
     private let addressBarTrailingStackViewDefaultPadding: CGFloat = 3
 
@@ -258,8 +258,8 @@ final class RefreshAddressBarStyleProvider: AddressBarStyleProviding {
     let addressBarHorizontalPaddingIDLE: CGFloat? = 4           // OK
     let addressBarInactiveBackgroundViewLeadingPadding: CGFloat = 6
     let addressBarInactiveBackgroundViewTrailingPadding: CGFloat = 6
-    let addressBarButtonsContainerViewLeadingPadding: CGFloat = 9
-    let addressBarButtonsContainerViewTrailingPadding: CGFloat = 9
+    let addressBarButtonsContainerViewLeadingPadding: CGFloat = 6
+    let addressBarButtonsContainerViewTrailingPadding: CGFloat = 6
     let addressBarInnerBorderViewRadius: CGFloat = 19           // OK - Matches addressBarActiveBackgroundViewRadius
     let addressBarTextFieldLeadingPadding: CGFloat = 26
     let addTabButtonPadding: CGFloat = 32                       // Takes into account the extra 24pts (12pts for each inset on s-shaped tabs)

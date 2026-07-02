d3a92afa7387186fd0e982c00707bc999d2e7ede
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-07-02 20:36:30 -0300

    AICHat: Adjusting bottom padding

--- macOS/DuckDuckGo/AIChat/Suggestions/AIChatSuggestionsView.swift
+++ macOS/DuckDuckGo/AIChat/Suggestions/AIChatSuggestionsView.swift
@@ -35,7 +35,8 @@ final class AIChatSuggestionsView: NSView {
         static let legacySeparatorHorizontalInset: CGFloat = 12
         static let rowsHorizontalPadding: CGFloat = 6
         static let legacyRowsHorizontalPadding: CGFloat = 4
-        static let bottomPadding: CGFloat = 4
+        static let bottomPadding: CGFloat = 1
+        static let legacyBottomPadding: CGFloat = 4
         static let viewAllChatsSeparatorBottomPadding: CGFloat = 0
         static let legacyViewAllChatsSeparatorBottomPadding: CGFloat = 8
     }
@@ -155,10 +156,11 @@ final class AIChatSuggestionsView: NSView {
         let isAppRebranded = NSApp.delegateTyped.themeManager.isAppRebranded
         let rowHeight = isAppRebranded ? Constants.rowHeight : Constants.legacyRowHeight
         let viewAllChatsSeparatorBottomPadding = isAppRebranded ? Constants.viewAllChatsSeparatorBottomPadding : Constants.legacyViewAllChatsSeparatorBottomPadding
+        let bottomPadding = isAppRebranded ? Constants.bottomPadding : Constants.legacyBottomPadding
         let separatorTotalHeight = Constants.separatorHeight + Constants.separatorTopPadding + Constants.separatorBottomPadding
         let rowsHeight = CGFloat(count) * rowHeight
         let viewAllChatsHeight = showViewAllChats ? Constants.separatorHeight + viewAllChatsSeparatorBottomPadding + rowHeight : 0
-        return separatorTotalHeight + rowsHeight + viewAllChatsHeight + Constants.bottomPadding
+        return separatorTotalHeight + rowsHeight + viewAllChatsHeight + bottomPadding
     }
 
     // MARK: - Public Methods

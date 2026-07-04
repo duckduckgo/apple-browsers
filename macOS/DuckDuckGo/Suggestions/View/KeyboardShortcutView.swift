87b562dfc68f3fd26ec42f591c6782d6f0b4c9d8
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-07-03 21:58:25 -0300

    KeyboardShortcutView: Wiring Theme colors

--- macOS/DuckDuckGo/Suggestions/View/KeyboardShortcutView.swift
+++ macOS/DuckDuckGo/Suggestions/View/KeyboardShortcutView.swift
@@ -42,6 +42,7 @@ final class KeyboardShortcutView: NSView {
         return stack
     }()
 
+    private var themeManager: ThemeManaging = NSApp.delegateTyped.themeManager
     private var keyCapViews: [KeyCapView] = []
 
     var isHighlighted: Bool = false {
@@ -87,8 +88,17 @@ final class KeyboardShortcutView: NSView {
     }
 
     private func updateAppearance() {
-        let bgColor = isHighlighted ? Constants.selectedBackgroundColor : Constants.normalBackgroundColor
-        let textColor = isHighlighted ? Constants.selectedTextColor : Constants.normalTextColor
+        let palette = themeManager.theme.palette
+        let bgColor: NSColor
+        let textColor: NSColor
+
+        if themeManager.isAppRebranded {
+            bgColor = isHighlighted ? palette.controlsFillSecondary : palette.controlsFillPrimary
+            textColor = isHighlighted ? palette.accentTextPrimary : palette.textPrimary
+        } else {
+            bgColor = isHighlighted ? Constants.selectedBackgroundColor : Constants.normalBackgroundColor
+            textColor = isHighlighted ? Constants.selectedTextColor : Constants.normalTextColor
+        }
 
         keyCapViews.forEach { keyCapView in
             keyCapView.backgroundColor = bgColor

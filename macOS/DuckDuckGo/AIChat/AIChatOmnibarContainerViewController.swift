d5f61c94ac9c9e3f257e0680043931df5c7a19b5
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-07-06 18:12:25 -0300

    Omnibar: Updates Shadow Overlap Constants

--- macOS/DuckDuckGo/AIChat/AIChatOmnibarContainerViewController.swift
+++ macOS/DuckDuckGo/AIChat/AIChatOmnibarContainerViewController.swift
@@ -51,7 +51,7 @@ final class AIChatOmnibarContainerViewController: NSViewController {
 
     private enum Constants {
         static let clipMaskBottomOffset: CGFloat = 14
-        static let shadowOverlapHeight: CGFloat = -4
+        static let shadowOverlapHeight: CGFloat = 26
         static let legacyShadowOverlapHeight: CGFloat = 11
         static let submitButtonSize: CGFloat = 28
         static let submitButtonCornerRadius: CGFloat = 14

9077c0538417be985b8f46b0cdebbd69cabdc4f6
Michal Smaga <msmaga@duckduckgo.com>
2026-07-06 15:08:58 +0200

    [macOS] Isolate PromoServiceFactoryTests setup to the main actor for the window controllers manager mock

--- macOS/UnitTests/Promotions/PromoServiceFactoryTests.swift
+++ macOS/UnitTests/Promotions/PromoServiceFactoryTests.swift
@@ -27,6 +27,7 @@ final class PromoServiceFactoryTests: XCTestCase {
 
     private var dependencies: PromoDependencies!
 
+    @MainActor
     override func setUp() {
         super.setUp()
         dependencies = makeDependencies()
@@ -134,6 +135,7 @@ final class PromoServiceFactoryTests: XCTestCase {
 }
 
 extension PromoServiceFactoryTests {
+    @MainActor
     private func makeDependencies() -> PromoDependencies {
         let activeRemoteMessageModel = ActiveRemoteMessageModel(
             remoteMessagingStore: MockRemoteMessagingStore(),

07f12ecc65e9d48f184e04d21f72c237dfb6c281
pikorddg <lpikor@duckduckgo.com>
2026-07-08 10:13:43 +0200

    2026 > 2025

--- iOS/DuckDuckGo/AIChat/ContextualMode/ContextualSuggestedPromptsProviding.swift
+++ iOS/DuckDuckGo/AIChat/ContextualMode/ContextualSuggestedPromptsProviding.swift
@@ -2,7 +2,7 @@
 //  ContextualSuggestedPromptsProviding.swift
 //  DuckDuckGo
 //
-//  Copyright © 2025 DuckDuckGo. All rights reserved.
+//  Copyright © 2026 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.

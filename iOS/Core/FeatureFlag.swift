1aaef61a45ef8e2fab49729087a3d3be58e73525
Alexey Martemyanov <mallexxx@gmail.com>
2026-07-15 20:46:24 +0600

    mark subscriptionPromoForExistingUsers feature as remote-releasable

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -632,7 +632,7 @@ extension FeatureFlag: FeatureFlagDescribing {
         case .subscriptionExpirationReminderNotification:
             Config(source: .remoteReleasable(PrivacyProSubfeature.subscriptionExpirationReminderNotification))
         case .subscriptionPromoForExistingUsers:
-            Config(defaultValue: .enabled, source: .remoteReleasable(PrivacyProSubfeature.subscriptionPromoForExistingUsers))
+            Config(source: .remoteReleasable(PrivacyProSubfeature.subscriptionPromoForExistingUsers))
         case .syncSetupBarcodeIsUrlBased:
             Config(source: .remoteReleasable(SyncSubfeature.syncSetupBarcodeIsUrlBased))
         case .canScanUrlBasedSyncSetupBarcodes:

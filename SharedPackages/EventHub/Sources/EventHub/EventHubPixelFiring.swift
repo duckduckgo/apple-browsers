0239fc0a58dfe7a682381eacd5a03ee4803c7fdb
Michal Smaga <msmaga@duckduckgo.com>
2026-07-22 13:05:28 +0200

    EventHub: add the project-mandated copyright header to all new files

--- SharedPackages/EventHub/Sources/EventHub/EventHubPixelFiring.swift
+++ SharedPackages/EventHub/Sources/EventHub/EventHubPixelFiring.swift
@@ -1,3 +1,21 @@
+//
+//  EventHubPixelFiring.swift
+//
+//  Copyright © 2026 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
 import Foundation
 
 /// A fired telemetry pixel: the bare governed config name (e.g. `webTelemetry_adwalls_day`) with no

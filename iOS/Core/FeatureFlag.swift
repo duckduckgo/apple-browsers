c16933ae455c8bd42562520e8fd1e12d47add67c
Bartosz Kunat <bkunat@duckduckgo.com>
2026-07-23 17:30:20 +0200

    Add iOS promo queue feature flag

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -369,6 +369,9 @@ public enum FeatureFlag: String {
     /// https://app.asana.com/1/137249556945/project/72649045549333/task/1213076120133808?focus=true
     case showNTPAfterIdleReturn
 
+    /// Coordinates launch modal prompts and Remote Messaging Framework cards.
+    case promoQueue
+
     /// Test-only feature flag for verifying UI test override mechanism.
     /// Used in Debug > UI Test Overrides screen.
     case uiTestFeatureFlag
@@ -794,6 +797,8 @@ extension FeatureFlag: FeatureFlagDescribing {
             Config(defaultValue: .enabled, source: .remoteReleasable(TabSwitcherTrackerCountSubfeature.featureEnabled))
         case .showNTPAfterIdleReturn:
             Config(source: .remoteReleasable(iOSBrowserConfigSubfeature.showNTPAfterIdleReturn))
+        case .promoQueue:
+            Config(defaultValue: .disabled, source: .remoteReleasable(PromoQueueSubfeature.featureEnabled))
         case .uiTestFeatureFlag:
             Config(source: .disabled)
         case .uiTestExperiment:

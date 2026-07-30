7340777c9168878c9de1488dac17b0b14012166c
Chris Brind <brindy@duckduckgo.com>
2026-07-30 15:41:26 +0100

    feature flag link

--- iOS/Core/FeatureFlag.swift
+++ iOS/Core/FeatureFlag.swift
@@ -466,7 +466,7 @@ public enum FeatureFlag: String {
     /// https://app.asana.com/1/137249556945/project/1204186595873227/task/1214830562427843
     case defaultExistingIPhoneUsersToNewTabAfterIdle
 
-    #warning("Add the feature flag URL for customizeNTPIcons")
+    /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1217024926605808
     case customizeNTPIcons
 
     /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1215169783702336

7b36b466153e2e51acd4fd9a206a22d24613709d
Bartosz Kunat <bkunat@duckduckgo.com>
2026-08-06 01:29:25 +0200

    Fix MockModalPromptProvider ignoring configuration

--- iOS/DuckDuckGo/NewTabPageMessagesModel.swift
+++ iOS/DuckDuckGo/NewTabPageMessagesModel.swift
@@ -308,15 +308,14 @@ final class NewTabPageMessagesModel: ObservableObject {
         let newMessageID = newRemoteMessageCandidate?.remoteMessageID
 
         messagesSnapshot = newMessages
+        remoteMessageCandidate = newRemoteMessageCandidate
 
         if previousMessageID != newMessageID {
-            withdrawAdmittedRemoteMessage()
             visibleRemoteMessageGateID = nil
             visibleRemoteMessageGateMountIDs.removeAll()
             remoteMessageGateIdentity = nil
+            withdrawAdmittedRemoteMessage()
         }
-
-        remoteMessageCandidate = newRemoteMessageCandidate
 
         if let admittedRemoteMessageSession,
            admittedRemoteMessageSession.identity.promoID == newMessageID,

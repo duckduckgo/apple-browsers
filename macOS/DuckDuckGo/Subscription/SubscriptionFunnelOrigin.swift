e0acb6c55ea0080b08c6e2dfad59f5070fa6ab25
József Vesza <jvesza@duckduckgo.com>
2026-08-09 14:59:55 +0200

    macOS: Update onboarding upsell task link

--- macOS/DuckDuckGo/Subscription/SubscriptionFunnelOrigin.swift
+++ macOS/DuckDuckGo/Subscription/SubscriptionFunnelOrigin.swift
@@ -55,7 +55,7 @@ enum SubscriptionFunnelOrigin: String {
     case newTabPageNextStepsCard = "funnel_onboarding_macOS__nextstepscard"
 
     /// User entered the funnel via the subscription upsell at the end of contextual onboarding.
-    /// https://app.asana.com/1/137249556945/project/1205044477659400/task/1216627186305529
+    /// https://app.asana.com/1/137249556945/project/1211834678943996/task/1217302662203337
     case onboardingSubscriptionUpsell = "funnel_onboarding_macos__subscriptionupsell"
 
     /// User entered the funnel via the subscription promo on the Fire Window home page.

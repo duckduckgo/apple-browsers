a90312bddd992dddf4d77006cc814870252862de
Dominik Kapusta <dkapusta@duckduckgo.com>
2026-08-10 17:03:33 +0200

    [macOS] Address Fire Dialog design feedback (#6262)
    Task/Issue URL: https://app.asana.com/1/137249556945/project/72649045549333/task/1216831005511464?focus=true
    
    Description:
    A few changes to colors and animations in the new Fire Dialog.

--- macOS/DuckDuckGo/Fire/ViewModel/FireDialogViewModel.swift
+++ macOS/DuckDuckGo/Fire/ViewModel/FireDialogViewModel.swift
@@ -212,9 +212,12 @@ final class FireDialogViewModel: ObservableObject {
 
     var shouldShowChatHistoryToggle: Bool {
         let isPresentedOnAIChatTab = tabCollectionViewModel?.selectedTab?.url?.isDuckAIURL ?? false
+        // Only hide chats toggle when no chats in the new dialog, so default this to `true` when the flag is off.
+        let hasChats = featureFlagger.isFeatureOn(.fireDialogSimplified) ? chats.count > 0 : true
         return aiChatHistoryCleaner.shouldDisplayCleanAIChatHistoryOption
             && mode.shouldShowChatHistoryToggle
             && (clearingOption.shouldShowChatHistoryToggle || isPresentedOnAIChatTab)
+            && hasChats
     }
 
     let fireViewModel: FireViewModel

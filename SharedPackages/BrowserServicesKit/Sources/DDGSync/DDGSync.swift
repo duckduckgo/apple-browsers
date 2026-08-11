04aea344615c8f47efe3c371ff50239e7f79d1e4
amddg44 <amallon@duckduckgo.com>
2026-08-11 14:59:10 +0200

    Rename devices through unified device API

--- SharedPackages/BrowserServicesKit/Sources/DDGSync/DDGSync.swift
+++ SharedPackages/BrowserServicesKit/Sources/DDGSync/DDGSync.swift
@@ -268,6 +268,10 @@ public class DDGSync: DDGSyncing {
         }
 
         do {
+            if dependencies.syncFeatureFlags.canWriteUnifiedDeviceList() {
+                return try await deviceInfoMigrationCoordinator.renameCurrentDevice(to: name, for: account)
+            }
+
             let result = try await dependencies.account.refreshToken(account, deviceName: name)
             try dependencies.secureStore.persistAccount(result.account)
             persistRecoveredThirdPartyScopedPasswordIfAvailable(from: result.accessCredentials, account: result.account)

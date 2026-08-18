0c9c3c416a59aaeae26000f8f3c3660ee439fc4b
Jorge Leandro Perez <jleandroperez@duckduckgo.com>
2026-08-18 13:24:15 -0300

    Implements DataImportDebugMenu

--- macOS/DuckDuckGo/Menus/MainMenu.swift
+++ macOS/DuckDuckGo/Menus/MainMenu.swift
@@ -940,6 +940,8 @@ final class MainMenu: NSMenu {
 
             // All items below will be automatically sorted alphabetically
             NSMenuItem(title: "Clear WebKit Cache", action: #selector(AppDelegate.debugClearWebViewCache)).withAccessibilityIdentifier("MainMenu.clearWebKitCache")
+            NSMenuItem(title: "Data Import")
+                .submenu(DataImportDebugMenu(title: "Data Import"))
             NSMenuItem(title: "Favicons") {
                 NSMenuItem(title: "Clear In-Memory Cache", action: #selector(AppDelegate.debugClearFaviconsCache)).withAccessibilityIdentifier("MainMenu.clearFaviconsCache")
                 NSMenuItem(title: "Inspect", action: #selector(MainViewController.inspectFavicons(_:))).withAccessibilityIdentifier("MainMenu.inspectFavicons")

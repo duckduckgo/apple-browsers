f980237deda0b0721e28a635d6a61ff77c554088
Pete <petes44@icloud.com>
2026-08-18 11:43:28 +0100

    Revert "Temporarily report iOS 7.235.0 for CI"
    This reverts commit b3c16681d32ed9053016385e8431f6b86d0ae5a6.

--- iOS/DuckDuckGo/AppConfigurationURLProvider.swift
+++ iOS/DuckDuckGo/AppConfigurationURLProvider.swift
@@ -42,7 +42,7 @@ struct AppConfigurationURLProvider: ConfigurationURLProviding {
         case .bloomFilterSpec: return URL.bloomFilterSpec
         case .bloomFilterBinary: return URL.bloomFilter
         case .bloomFilterExcludedDomains: return URL.bloomFilterExcludedDomains
-        case .privacyConfiguration: return URL(string: "https://duckduckgo.github.io/privacy-configuration/pr-5782/v4/ios-config.json")!
+        case .privacyConfiguration: return URL.privacyConfig
         case .trackerDataSet: return trackerDataUrlProvider.trackerDataURL ?? URL.trackerDataSet
         case .surrogates: return URL.surrogates
         case .remoteMessagingConfig: return RemoteMessagingClient.Constants.endpoint

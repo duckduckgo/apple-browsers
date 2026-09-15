c89ebf0667f5b1c819711e0ff8e260a3b8c8a861
Andre Martingo <amartingo@duckduckgo.com>
2026-09-14 23:01:38 -0400

    Fix macOS 27 debug menu icons with Xcode 26.6

--- macOS/LocalPackages/AppKitExtensions/Sources/AppKitExtensions/NSMenuItemExtension.swift
+++ macOS/LocalPackages/AppKitExtensions/Sources/AppKitExtensions/NSMenuItemExtension.swift
@@ -120,8 +120,16 @@ public extension NSMenuItem {
             preferredImageVisibility = visibleOnMacOS27 ? .visible : .hidden
         }
 #else
-        if #available(macOS 27.0, *), visibleOnMacOS27 == false {
-            effectiveImage = nil
+        if #available(macOS 27.0, *) {
+            if visibleOnMacOS27 {
+                // Xcode 26 cannot reference this macOS 27 API directly.
+                // NSMenuItem.ImageVisibility.visible has a raw value of 1.
+                if responds(to: NSSelectorFromString("setPreferredImageVisibility:")) {
+                    setValue(1, forKey: "preferredImageVisibility")
+                }
+            } else {
+                effectiveImage = nil
+            }
         }
 #endif
 
